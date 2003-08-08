a69b69683180a305160c89984354768debcf34fa
kiniry <kiniry@c6399e9c-662f-4285-9817-23cccad57800>
2003-08-08 12:58:52 +0000

    Code, spec, and comment cleanup; no semantic changes.

--- src/escjava/trunk/ESCTools/Javafe/java/javafe/tc/CheckCompilationUnit.java
+++ src/escjava/trunk/ESCTools/Javafe/java/javafe/tc/CheckCompilationUnit.java
@@ -2,52 +2,45 @@
 
 package javafe.tc;
 
-
 import javafe.ast.*;
 import javafe.parser.*;
 import javafe.util.*;
 
-
 /**
+ * This module is responsible for handling <code>CompilationUnit</code>-level type
+ * checks.
+ *
+ * <p> In practice, these are mostly checks that the set of import declarations is
+ * legal. </p>
+ */
 
-This module is responsible for handling
-<code>CompilationUnit</code>-level type checks.
-
-<p> In practice, these are mostly checks that the set of import
-declarations is legal.
-
-*/
-
-public class CheckCompilationUnit {
-
+public class CheckCompilationUnit
+{
     /**
-     * A new field for CompilationUnits: iff it is non-null then we
-     * have already checked that CompilationUnit.
+     * A new field for CompilationUnits: iff it is non-null then we have already
+     * checked that CompilationUnit.
      */
-    //@ invariant checkedField!=null
+    //@ invariant checkedField != null
     //@ invariant checkedField.decorationType == \type(Boolean)
     private static ASTDecoration checkedField =
 	new ASTDecoration("javafe.tc.CheckCompilationUnit.checked");
 
-
     /**
-     * Check a <code>CompilationUnit</code>. <p>
+     * Check a <code>CompilationUnit</code>.
      *
-     * If this method is called multiple times on the same
-     * <code>CompilationUnit</code>, it has no effect the second and
-     * later times.<p>
+     * <p> If this method is called multiple times on the same
+     * <code>CompilationUnit</code>, it has no effect the second and later
+     * times. </p>
      *
-     * Precondition: <code>cu</code> must have already been loaded by
-     * <code>OutsideEnv</code>.<p>
+     * <p> Precondition: <code>cu</code> must have already been loaded by
+     * <code>OutsideEnv</code>. </p>
      *
-     * Any resulting errors or warnings are reported via
-     * <code>ErrorSet</code>.<p>
+     * <p> Any resulting errors or warnings are reported via
+     * <code>ErrorSet</code>. </p>
      */
-    //@ requires cu!=null
+    //@ requires cu != null
     public static void checkCompilationUnit(CompilationUnit cu) {
-	/*
-	 * Check any given CompilationUnit at most once:
-	 */
+	// Check any given CompilationUnit at most once:
 	if (checkedField.get(cu) != null)
 	    return;
 	checkedField.set(cu, new Boolean(true));
@@ -59,53 +52,43 @@ public class CheckCompilationUnit {
 	// Warn about "file local" types declared public:
 	checkPublic(cu);
 
-	/*
-	 * Check to make sure that each import is individually well
-	 * formed:
-	 */
+	// Check to make sure that each import is individually well formed:
 	checkImports(cu);
 	
-
 	// Don't declare two decls with same name in same package 
 	//   This is implemented by OutsideEnv -- mdl.
 	// Don't declare two types with the same name in the same file
 	//   This is implemented in OutsideEnv -- mdl.
 
-
 	ImportDeclVec imports = cu.imports;
 	TypeDeclVec elems = cu.elems;
 
+	// Check for "import P.T" and "import Q.T" pairs.  Such a pair * is legal iff
+        // P=Q.
 
-	/*
-	 * Check for "import P.T" and "import Q.T" pairs.  Such a pair
-         * is legal iff P=Q.
-         *
-	 * Moreover, check for "import P.T" where T is defined in this
-	 * CompilationUnit.  Such imports are legal iff P is the name of
-	 * this package.
-	 */
+	// Moreover, check for "import P.T" where T is defined in this
+	// CompilationUnit.  Such imports are legal iff P is the name of this
+	// package.
+
 	for (int i = 0; i < imports.size(); i++) {
 	    // Skip to the next single-type import:
 	    ImportDecl idecl = imports.elementAt(i);
 	    if (!(idecl instanceof SingleTypeImportDecl))
 		continue;
 
-	    // Get the full name and unqualified name of the type it's
-	    // importing:
+	    // Get the full name and unqualified name of the type it's importing:
 	    Name N1 = ((SingleTypeImportDecl)idecl).typeName.name;
 	    Identifier T1 = N1.identifierAt(N1.size()-1);
 
-	    /*
-	     * Check for import N1, import Q.T1 pairs:
-	     */
+	    // Check for import N1, import Q.T1 pairs:
+
 	    for (int j = i+1; j < imports.size(); j++) {
 		// Skip to the next single-type import:
 		ImportDecl jdecl = imports.elementAt(j);
 		if (!(jdecl instanceof SingleTypeImportDecl))
 		    continue;
 
-		// Get the full name and unqualified name of the type it's
-		// importing:
+		// Get the full name and unqualified name of the type it's importing:
 		Name N2 = ((SingleTypeImportDecl)jdecl).typeName.name;
 		Identifier T2 = N2.identifierAt(N2.size()-1);
 	  
@@ -117,11 +100,8 @@ public class CheckCompilationUnit {
 			    + " using single-type-import declarations");
 	    }
 
-	    /*
-	     * Check to see if N1 is of the form P.T where T is declared
-	     * in this CompilationUnit and P is not this
-	     * CompilationUnit's package:
-	     */
+	    // Check to see if N1 is of the form P.T where T is declared in this
+	    // CompilationUnit and P is not this CompilationUnit's package:
 	    for (int j=0; j<elems.size(); j++) {
 		if (T1 != elems.elementAt(j).id)
 		    continue;
@@ -145,15 +125,15 @@ public class CheckCompilationUnit {
 	}
     }
 
+    // Private methods
 
-  // Private methods
+    /**
+     * Check a <code>CompilationUnit<code> to make sure that the only type that may
+     * be declared public in it is the one with the same name as the file it occurs
+     * in.  Violations result in warnings only.
+     */
 
-    /** Check a <code>CompilationUnit<code> to make sure that the only
-	type that may be declared public in it is the one with the same
-	name as the file it occurs in.  Violations result in warnings
-	only. */
-
-    //@ requires cu!=null
+    //@ requires cu != null
     private static void checkPublic(CompilationUnit cu) {
 	// Get the basename of the file we loaded cu from:
 	String localname = Location.toFile(cu.loc).getLocalName();
@@ -179,23 +159,21 @@ public class CheckCompilationUnit {
 
 
     /**
-     * Check a <code>CompilationUnit<code> to make sure that each
-     * import is individually well formed. <p>
+     * Check a <code>CompilationUnit<code> to make sure that each import is
+     * individually well formed.
      *
-     * In particular,
-     *
-     *   - Type in single import must exist
-     *
-     *   - Single imports from other packages must be public (not
-     *     implemented)
-     *
-     *   - Packages in all imports must be "accessible" (disabled)
-     *
-     * This routine does not check legality of pairs of import
-     * statements or import statements and the types declared in the
-     * rest of the CompilationUnit.<p>
+     * <p> In particular,
+     * <ul>
+     *   <li> Type in single import must exist </li>
+     *   <li> Single imports from other packages must be public (not implemented)
+     *   </li>
+     *   <li> Packages in all imports must be "accessible" (disabled) </li>
+     * </ul>
+     * <p> This routine does not check legality of pairs of import statements or
+     * import statements and the types declared in the rest of the
+     * CompilationUnit. </p>
      */
-    //@ requires cu!=null
+    //@ requires cu != null
     private static void checkImports(CompilationUnit cu) {
 	ImportDeclVec imports = cu.imports;
 
@@ -235,4 +213,11 @@ public class CheckCompilationUnit {
 	    }
 	}
     }
-}
+} // end of class CheckCompilationUnit
+
+/*
+ * Local Variables:
+ * Mode: Java
+ * fill-column: 85
+ * End:
+ */
