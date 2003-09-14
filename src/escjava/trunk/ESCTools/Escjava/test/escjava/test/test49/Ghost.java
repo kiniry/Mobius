28ea11d6b97e97f636fa2c99d201607534550173
cok <cok@c6399e9c-662f-4285-9817-23cccad57800>
2003-09-14 01:17:21 +0000

    Changes to avoid problems with modifies everything

--- src/escjava/trunk/ESCTools/Escjava/test/escjava/test/test49/Ghost.java
+++ src/escjava/trunk/ESCTools/Escjava/test/escjava/test/test49/Ghost.java
@@ -6,7 +6,7 @@
 // First test writable_if on ghost fields...
 class WritableIf {
     //@ ghost public Object mono //@ writable_if mono==null
-
+    //@ pure
     WritableIf() {
 	Object newthing = new Object();
 
@@ -14,7 +14,7 @@ class WritableIf {
 	//@ set mono = newthing;       // ok, 1st time
 	//@ set mono = newthing;       // error, 2nd time
     }
-
+    //@ pure
     WritableIf(int x) {
 	Object newthing = new Object();
 
@@ -28,14 +28,14 @@ class WritableIf {
 class NonNull {
     //@ ghost public /*@non_null*/ Object foo
     //@ ghost public static /*@non_null*/ Object s
-
+    //@ pure
     void foo(/*@non_null*/ Object x) {
 	//@ assert foo!=null
 	//@ set foo = x        // ok since x!=null
 	//@ set foo = null     // error
 	//@ set foo = foo      // ok since foo is known to be non-null
     }
-
+    //@ pure
     void foo2(/*@non_null*/ Object x) {
 	//@ assert s!=null
 	//@ set s = x        // ok since x!=null
@@ -50,7 +50,7 @@ class MonitoredBy {
     public Object mutex;
 
     //@ ghost public Object x //@ monitored_by mutex
-
+    //@ pure
     void foo(Object foo) {
 	//@ assert x==x        // ok: annotations don't "read" variables...
 	//@ set x = foo         // error
--- src/escjava/trunk/ESCTools/Escjava/test/escjava/test/test56/FormalModifiers.java
+++ src/escjava/trunk/ESCTools/Escjava/test/escjava/test/test56/FormalModifiers.java
@@ -3,13 +3,14 @@
  **/
 
 class FormalModifiers {
-
+    //@ pure
     void foo (String x /*@non_null*/,
 	      Object z[] /*@non_null*/,
 	      int[] q /*@non_null*/ /*@non_null*/) {}
-
+    //@ pure
     void bar (/*@non_null*/ String a /*@non_null*/) {}
-
+    //@ pure
     void b1 (int b) {}
+    //@ pure
     void b2 (/*@non_null*/ String b) {}
 }
