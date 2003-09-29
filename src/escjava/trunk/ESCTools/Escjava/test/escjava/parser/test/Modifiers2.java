31ccb6608f51b2ea6efab718ea19aeb4aa49e38c
cok <cok@c6399e9c-662f-4285-9817-23cccad57800>
2003-09-29 11:12:17 +0000

    Test update

--- src/escjava/trunk/ESCTools/Escjava/test/escjava/parser/test/Modifiers2.java
+++ src/escjava/trunk/ESCTools/Escjava/test/escjava/parser/test/Modifiers2.java
@@ -5,7 +5,7 @@ public class Modifiers2 extends Modifiers1 {
   int v3;
 
   public int update(Modifiers1 v1)
-    /*@ also_modifies v3; also_ensures v3 == this.v1 */
+    /*@ modifies v3; ensures v3 == this.v1 */
   {
     v3 = this.v1;
     return super.update(v1);
