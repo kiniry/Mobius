68d76eb001409b3f699952f13344777a5703ffe3
cok <cok@c6399e9c-662f-4285-9817-23cccad57800>
2003-10-23 01:57:25 +0000

    Some files missed in the last commit

---
+++ src/escjava/trunk/ESCTools/Escjava/test/junittests/Instance2.java
@@ -0,0 +1,39 @@
+// Tests static default for ghost and model fields
+
+public class Instance2 implements INSTI {
+
+	public int in;
+
+	static void m() {
+		int i = N;
+		// N = 7;
+		//@ ghost int ii;
+/*@
+		set ii = g;
+		set g = 9; // is ghost final ?
+		set ii = mmm;
+*/
+	}
+
+	void mm() {
+
+		//@ ghost int ii;
+		//@ set ii = gi;
+		//@ set gi = 10; // is ghost final?
+		//@ set ii = g; 
+		//@ set ii = mmm;
+		//@ set ii = mmmi;
+	}
+
+}
+
+interface INSTI {
+
+	int N = 0;
+
+	//@ ghost int g = 0;
+	//@ instance ghost int gi = 0;
+
+	//@ model int mmm;
+	//@ model instance int mmmi;
+}
