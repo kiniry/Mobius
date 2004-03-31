3913faf5474ce907c5fa772115d5fcc9dfac840c
cok <cok@c6399e9c-662f-4285-9817-23cccad57800>
2004-03-31 12:03:16 +0000

    Fixing the annotations of the example

--- src/escjava/trunk/ESCTools/papers/SortedSequence.java
+++ src/escjava/trunk/ESCTools/papers/SortedSequence.java
@@ -1,15 +1,13 @@
 public class SortedSequence  {
-
 	private /*@ spec_public */ int[] seq = new int[9];
 	private /*@ spec_public */ int size = 0;
 	//@ invariant seq != null;
 	//@ invariant size >= 0 && size <= seq.length;
 
-	//@ requires true;
-	//@ assignable seq, seq[*];
+	//@ assignable seq, seq[*],size;
 	public void push(int i) {
 	    if (size == seq.length) {
-	        int[] newseq = new int[2 * seq.length];
+	        int[] newseq = new int[2 * seq.length + 1];
 	        System.arraycopy(seq,0,newseq,0,size);
 	        seq = newseq;
 	    }
@@ -17,14 +15,21 @@ public class SortedSequence  {
 	}
 
 	//@ requires size > 0;
-	//@ assignable seq[*];
-	//@ ensures \result == sort()[size-1];
+	//@ assignable seq,seq[*],size;
+	//@ ensures size == \old(size-1);
+	//@ ensures \result == sort()[size];
 	public int pop() {
 	    seq = sort();
 	    return seq[--size];
 	}
 
-	//@ pure
-	private int[] sort() { ... }
-
+	//@ ensures \result != null;
+	//@ ensures \result.length >= size;
+	//@ ensures (* result is sorted *);
+	//@ ensures (* result has same elements as seq *);
+	/*@ pure */ private int[] sort() { 
+	    int[] result = new int[seq.length];
+	    /*...*/ 
+	    return result;
+	}
 }
