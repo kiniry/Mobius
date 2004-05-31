a33b6d743b3c90dc1bcbcf31120487af72c500fc
kiniry <kiniry@c6399e9c-662f-4285-9817-23cccad57800>
2004-05-31 20:47:04 +0000

    Added preliminary text to first section and started outlining code for first problems.  Added infrastructure to Makefile to support generation of Texinfo files from Java and JML source.

---
+++ src/escjava/trunk/ESCTools/papers/problems/Assertions.java
@@ -0,0 +1,22 @@
+class Assertions
+{
+  int i;
+  Object o;
+  String s;
+
+  void m() {
+    
+  }
+
+  void n(int j, Object p, String t) {
+    
+  }
+
+  void o(int i, Object o, String s) {
+    
+  }
+
+  void p(int i, Object o, String s) throws AssertionError {
+    
+  }
+}
