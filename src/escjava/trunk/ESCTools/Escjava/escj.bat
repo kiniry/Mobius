93c443226bfcca5f8e15e34e08ac6cccfaf7f3d7
cok <cok@c6399e9c-662f-4285-9817-23cccad57800>
2004-12-15 03:12:15 +0000

    Fixes to the escj.bat file

--- src/escjava/trunk/ESCTools/Escjava/escj.bat
+++ src/escjava/trunk/ESCTools/Escjava/escj.bat
@@ -1,16 +1,13 @@
 @echo off
 
-rem To install ESC/Java2 on a new machine, change the following two
-rem lines appropriately:
-set ESCJAVA_ROOT=F:\ESCJava-2.0a5
-rem set JAVA=F:\jsdk\1.4.2\bin\Java.exe
+rem To install ESC/Java2 on a new machine, change the following 
+rem line to point to the directory containing the installation:
+set ESCJAVA_ROOT=C:\cygwin\ESC\ESCTools
 
-set ESCJAVA_ROOT=C:/cygwin/ESC/ESCTools
 
 
 
 rem Some arguments to Simplify, passed via environment variables
-rem set ESCJ_SIMPLIFY=%ESCJAVA_ROOT%\bin\Simplify-1.5.4.exe
 set ESCJ_SIMPLIFY=%ESCJAVA_ROOT%\Simplify-1.5.4.exe
 set ESCJ_SIMPLIFY_ARGS=-noprune -noplunge
 set PROVER_KILL_TIME=300
@@ -20,16 +17,10 @@ if "%ESCJ_STDARGS%"=="" set ESCJ_STDARGS=-nowarn Deadlock
 
 rem ESCJ_ARGS is a variable local to this batch file
 set ESCJ_ARGS=%*
-rem :getargs
-rem shift
-rem if "%1"=="" goto endGetargs
-rem set ESCJ_ARGS=%ESCJ_ARGS% %1
-rem goto getargs
-rem :endGetargs
 
 rem USERPATH is a variable local to this batch file
-if "%CLASSPATH%"=="" set USERPATH=.
-if not "%CLASSPATH%"=="" set USERPATH=%CLASSPATH%
+if '%CLASSPATH%'=='' set USERPATH=.
+if not '%CLASSPATH%'=='' set USERPATH=%CLASSPATH%
 rem @echo on
 rem echo %ESCJ_ARGS%
-java  -Dsimplify=%ESCJ_SIMPLIFY% -classpath "%ESCJAVA_ROOT%\esctools2.jar" escjava.Main -classpath  "%ESCJAVA_ROOT%\jmlspecs.jar;." %ESCJ_STDARGS% %ESCJ_ARGS%
+java  -Dsimplify="%ESCJ_SIMPLIFY%" -classpath "%ESCJAVA_ROOT%\esctools2.jar" escjava.Main -specs "%ESCJAVA_ROOT%\jmlspecs.jar" -classpath "%USERPATH%" %ESCJ_STDARGS% %ESCJ_ARGS%
