19f78b31681de3a2819141adb506939ad7ed91f2
kiniry <kiniry@c6399e9c-662f-4285-9817-23cccad57800>
2005-01-04 12:35:23 +0000

    Revised support for all command-line switches.  All switches now have consistent, case-insensitive long and short forms.  Public switches are shown when help is requested; private switches are shown when verbose help is requested.  Several switches are still undocumented; please review.  Standardized formatting of class as well.

--- src/escjava/trunk/ESCTools/Javafe/java/javafe/SrcToolOptions.java
+++ src/escjava/trunk/ESCTools/Javafe/java/javafe/SrcToolOptions.java
@@ -8,95 +8,88 @@ import javafe.util.UsageError;
  
 public class SrcToolOptions extends Options
 {
-    //**************************************************************************
-    //  Hard-coded behavior
-    //**************************************************************************
+  /**
+   * Do we allow the <code>-avoidSpec</code> option?  Defaults to
+   * yes.
+   */
+  public static boolean allowAvoidSpec = true;
+
+  /**
+   * Do we allow the -depend option?  Defaults to yes.
+   */
+  public static boolean allowDepend = true;
+
+  /**
+   * Should we avoid specs for all types loaded after the initial
+   * set of source files?
+   *
+   * <p> Defaults to false.  Set by using the <tt>-avoidSpec</tt>
+   * option.
+   *
+   * <p> Note: if <code>processRecursively</code> is set, then we
+   * always avoid specs.
+   */
+  public boolean avoidSpec = false;
+
+  /**
+   * Should we process files recursively?  Defaults to no, can be set
+   * by a sub-class, or the <tt>-depend</tt> option.
+   *
+   * <p> Warning: this needs to be set before option processing is
+   * finished!
+   */
+  public boolean processRecursively = false;
+
+  /**
+   * The list of filenames on the command line; this {@link Vector} is
+   * aliased with a variable in {@link SrcTool}.
+   */
     
-    /**
-     * Do we allow the <code>-avoidSpec</code> option?  Defaults to
-     * yes.
-     */
-    public static boolean allowAvoidSpec = true;
+  // @overrides Options.processOption(String, String[], int)
 
-    /**
-     * Do we allow the -depend option?  Defaults to yes.
-     */
-    public static boolean allowDepend = true;
-
-    //****************************************************************************
-    // Options
-    //****************************************************************************
-    
-    /**
-     * Should we avoid specs for all types loaded after the initial
-     * set of source files?
-     *
-     * <p> Defaults to false.  Set by -avoidSpec option.
-     *
-     * <p> Note: if <code>processRecursively</code> is set, then we
-     * always avoid specs.
-     */
-    public boolean avoidSpec = false;
-
-    /**
-     * Should we process files recursively?  Defaults to no, 
-     * can be set by a sub-class, or the -depend option.
-     *
-     * <p> Warning: this needs to be set before option processing is
-     * finished!
-     */
-    public boolean processRecursively = false;
-
-    /**
-     * The list of filenames on the command line; this Vector is aliased
-     * with a variable in SrcTool.
-     */
-    
-    //************************************************************************
-    // Support routines
-    //************************************************************************
-
-    // @overrides Options.processOption(String, String[], int)
-
-    public int processOption(String option, String[] args, int offset) 
-        throws UsageError {
- 	if (option.equals("-depend") && allowDepend) {
-	    processRecursively = true;
-	    return offset;
-	} else if (option.equals("-avoidSpec") && allowAvoidSpec) {
-	    avoidSpec = true;
-	    return offset;
-	} else
-	    // Pass on unrecognized options:
-	    return super.processOption(option, args, offset);
-    }
+  public int processOption(String option, String[] args, int offset) 
+    throws UsageError {
+    if (option.equals("-depend") && allowDepend) {
+      processRecursively = true;
+      return offset;
+    } else if (option.equals("-avoidSpec") && allowAvoidSpec) {
+      avoidSpec = true;
+      return offset;
+    } else
+      // Pass on unrecognized options:
+      return super.processOption(option, args, offset);
+  }
         
-    /**
-     * Print non-option usage info to <code>System.err</code>.  Output
-     * must include at least one newline.
-     */
-    public String showNonOptions() {
-		return ("<source files>");
-    }
+  /**
+   * Print non-option usage info to <code>System.err</code>.  Output
+   * must include at least one newline.
+   */
+  public String showNonOptions() {
+    return ("<source files>");
+  }
 
-    /**
-     * Print option information to <code>System.err</code>.  Each
-     * printed line should be preceeded by two blank spaces.
-     *
-     * <p> Each overriding method should first call
-     * <code>super.showOptions()</code>.
-     */
-    public String showOptions(boolean all) {
-	String[][] data1 = {{"-avoidSpec",""}};
-	String[][] data2 = {{"-depend",""}};
+  /**
+   * Print option information to <code>System.err</code>.  Each
+   * printed line should be preceeded by two blank spaces.
+   *
+   * <p> Each overriding method should first call
+   * <code>super.showOptions()</code>.
+   */
+  public String showOptions(boolean all) {
+    String[][] data1 = {{"-AvoidSpec",
+                         "Avoid specification files if possible and read type and spec information\n" +
+                         "\tfrom source and class files instead"}};
+    String[][] data2 = {{"-Depend",
+                         "Recursively process all specification, source, and binary files on which\n" +
+                         "\tthe input files depend"}};
 
-        StringBuffer sb = new StringBuffer(super.showOptions(all));
+    StringBuffer sb = new StringBuffer(super.showOptions(all));
 	
-	if (allowAvoidSpec) sb.append(showOptionArray(data1));
+    if (allowAvoidSpec) sb.append(showOptionArray(data1));
 	
-	if (allowDepend) sb.append(showOptionArray(data2));
+    if (allowDepend) sb.append(showOptionArray(data2));
 		
-	return sb.toString();
-    }
+    return sb.toString();
+  }
 
 }
