87eddb4defaf2679cdbc8a6e2b84a5c8a60aafcb
kiniry <kiniry@c6399e9c-662f-4285-9817-23cccad57800>
2005-02-28 17:08:40 +0000

    Syntactic reformatting and some new comments.

--- src/escjava/trunk/ESCTools/Escjava/java/escjava/parser/EscPragmaLex.java
+++ src/escjava/trunk/ESCTools/Escjava/java/escjava/parser/EscPragmaLex.java
@@ -42,97 +42,99 @@ import escjava.ast.TagConstants;
 
 public final class EscPragmaLex extends Lex
 {
-    public EscPragmaLex(PragmaParser pragmaParser, boolean isJava) {
-        super(pragmaParser, isJava);
-    }
+  // Documented in parent class.
+  public EscPragmaLex(PragmaParser pragmaParser, boolean isJava) {
+    super(pragmaParser, isJava);
+  }
 
-    protected int scanJavaExtensions(int nextchr) {
-        try { 
-            if (nextchr == '\\') {
-                int h = 0;
-                do {
-                    try {
-                        text[textlen] = (char)nextchr;
-                        textlen++;
-                    } catch (ArrayIndexOutOfBoundsException e) { 
-                        this.append(nextchr);
-                    }
-                    h = _SpecialParserInterface.HC * h + nextchr;
-                    nextchr = m_in.read();
-                } while (Character.isJavaIdentifierPart((char)nextchr));
-                m_nextchr = nextchr;
-                auxVal = null;
-                identifierVal = _SpecialParserInterface.intern(text, textlen, h);
-                if (keywords != null) {
-                    Object val = keywords.get(identifierVal);
-                    if (val != null) {
-                        ttype = ((Integer)val).intValue();
-                    } else {
-                        int esckeytag = TagConstants.fromIdentifier(identifierVal);
-			if (TagConstants.isKeywordTag(esckeytag)) {
-                            ttype = _SpecialParserInterface.getTokenType(identifierVal);
-                        } else {
-                            ErrorSet.fatal(startingLoc, "Unrecognized special keyword");
-                        }
-                    }
-                } else {
-                    ttype = TagConstants.NULL;
-                }
-                return ttype;
-            } else if (nextchr == '(') {
-                // Try to lex an informal predicate
-                m_in.mark();
-                nextchr = m_in.read();
-                if (nextchr == '*') {
-                    // now look for the closing "*)"
-                    while (true) {
-                        nextchr = m_in.read();
-                        try {
-                            text[textlen] = (char)nextchr;
-                            textlen++;
-                        } catch (ArrayIndexOutOfBoundsException e) { 
-                            this.append(nextchr);
-                        }
-                        if (nextchr == '*') {
-                            // read all the '*'s there are
-                            do {
-                                nextchr = m_in.read();
-                                try {
-                                    text[textlen] = (char)nextchr;
-                                    textlen++;
-                                } catch (ArrayIndexOutOfBoundsException e) { 
-                                    this.append(nextchr);
-                                }
-                            } while (nextchr == '*');
-                            if (nextchr == ')') {
-                                // we've found the end of the informal predicate
-                                ttype = TagConstants.INFORMALPRED_TOKEN;
-                                m_nextchr = nextchr;
-                                auxVal = new String(text, 0, textlen-2);
-                                endingLoc = m_in.getLocation();
-                                m_nextchr = m_in.read();
-                                return ttype;
-                            }
-                        }
-                        if (nextchr == -1) {
-                            // error in input
-                            ErrorSet.fatal(startingLoc, 
-                                           "Unterminated informal predicate");
-                        }
-                    }
-                } else {
-                    // it wasn't an informal predicate after all
-                    m_in.reset();
-                }
+  // Documented in parent class.
+  protected int scanJavaExtensions(int nextchr) {
+    try { 
+      if (nextchr == '\\') {
+        int h = 0;
+        do {
+          try {
+            text[textlen] = (char)nextchr;
+            textlen++;
+          } catch (ArrayIndexOutOfBoundsException e) { 
+            this.append(nextchr);
+          }
+          h = _SpecialParserInterface.HC * h + nextchr;
+          nextchr = m_in.read();
+        } while (Character.isJavaIdentifierPart((char)nextchr));
+        m_nextchr = nextchr;
+        auxVal = null;
+        identifierVal = _SpecialParserInterface.intern(text, textlen, h);
+        if (keywords != null) {
+          Object val = keywords.get(identifierVal);
+          if (val != null) {
+            ttype = ((Integer)val).intValue();
+          } else {
+            int esckeytag = TagConstants.fromIdentifier(identifierVal);
+            if (TagConstants.isKeywordTag(esckeytag)) {
+              ttype = _SpecialParserInterface.getTokenType(identifierVal);
+            } else {
+              ErrorSet.fatal(startingLoc, "Unrecognized special keyword");
             }
-
-            ttype = TagConstants.NULL;
-            return ttype;
-        } catch (IOException e) {
-            ErrorSet.fatal(m_in.getLocation(), e.toString());
-            return TagConstants.NULL; // Dummy
+          }
+        } else {
+          ttype = TagConstants.NULL;
         }
+        return ttype;
+      } else if (nextchr == '(') {
+        // Try to lex an informal predicate
+        m_in.mark();
+        nextchr = m_in.read();
+        if (nextchr == '*') {
+          // now look for the closing "*)"
+          while (true) {
+            nextchr = m_in.read();
+            try {
+              text[textlen] = (char)nextchr;
+              textlen++;
+            } catch (ArrayIndexOutOfBoundsException e) { 
+              this.append(nextchr);
+            }
+            if (nextchr == '*') {
+              // read all the '*'s there are
+              do {
+                nextchr = m_in.read();
+                try {
+                  text[textlen] = (char)nextchr;
+                  textlen++;
+                } catch (ArrayIndexOutOfBoundsException e) { 
+                  this.append(nextchr);
+                }
+              } while (nextchr == '*');
+              if (nextchr == ')') {
+                // we've found the end of the informal predicate
+                ttype = TagConstants.INFORMALPRED_TOKEN;
+                m_nextchr = nextchr;
+                auxVal = new String(text, 0, textlen-2);
+                endingLoc = m_in.getLocation();
+                m_nextchr = m_in.read();
+                return ttype;
+              }
+            }
+            if (nextchr == -1) {
+              // error in input
+              ErrorSet.fatal(startingLoc, 
+                             "Unterminated informal predicate");
+            }
+          }
+        } else {
+          // it wasn't an informal predicate after all
+          m_in.reset();
+        }
+      }
+
+      ttype = TagConstants.NULL;
+      return ttype;
+    } catch (IOException e) {
+      ErrorSet.fatal(m_in.getLocation(), e.toString());
+      return TagConstants.NULL; // Dummy
     }
+  }
 } // end of class EscPragmaLex
 
 /*
