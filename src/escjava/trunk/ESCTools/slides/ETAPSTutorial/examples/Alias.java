4018e94692cd4a7615c1a88302d77c3e6439592e
erikpoll <erikpoll@c6399e9c-662f-4285-9817-23cccad57800>
2005-04-13 13:55:49 +0000

    a couple of typos in Java/JML code fixed

--- src/escjava/trunk/ESCTools/slides/ETAPSTutorial/examples/Alias.java
+++ src/escjava/trunk/ESCTools/slides/ETAPSTutorial/examples/Alias.java
@@ -2,8 +2,8 @@ public class Alias {
   /*@ non_null */ int[] a = new int[10];
   boolean noneg = true;
 
-  /*@ invariant noneg ==> (\forall int i; 0<=i && i < a.length; 
-                        (a[i]>=0)); */
+  /*@ invariant noneg ==> 
+                 (\forall int i; 0<=i && i < a.length;  a[i]>=0); */
 
   //@ requires 0<=i && i < a.length;
   public void insert(int i, int v) {
