09de0f6839fdb7f4c5dedaeb7b8084e0030972c6
erikpoll <erikpoll@c6399e9c-662f-4285-9817-23cccad57800>
2005-04-13 13:41:09 +0000

    *** empty log message ***

--- src/escjava/trunk/ESCTools/slides/ECOOPTutorial-cok/examples/LoopInvWarning.java
+++ src/escjava/trunk/ESCTools/slides/ECOOPTutorial-cok/examples/LoopInvWarning.java
@@ -1,6 +1,6 @@
 public class LoopInvWarning {
   public int max(/*@ non_null */ int[] a) {
-    int m;
+    int m=Integer.MAX_VALUE;
     //@ loop_invariant (\forall int j; 0<=j && j<i; a[j] <= m);
     //@ decreases a.length - i - 1;
     for (int i=0; i<a.length; ++i) {
