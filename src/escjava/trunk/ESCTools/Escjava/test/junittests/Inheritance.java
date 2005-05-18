534b843431b8b3a4c0e1f4ea0a6fa1d10814905e
davidcok <davidcok@c6399e9c-662f-4285-9817-23cccad57800>
2005-05-18 00:32:52 +0000

    Partially fixed a test

--- src/escjava/trunk/ESCTools/Escjava/test/junittests/Inheritance.java
+++ src/escjava/trunk/ESCTools/Escjava/test/junittests/Inheritance.java
@@ -9,13 +9,25 @@ class Sub extends Super {
 }
 
 public class Inheritance {
+    Inheritance();
+
     public void mm(/*@ non_null */ Super s) {
         //@ assert s.m() >= 0;              // Line A
         if (s instanceof Sub) {
             Sub ss = (Sub)s;
             //@ assert ss.m() == 0;         // Line B
             //@ assert ((Sub)s).m() == 0;   // Line C
-            //@ assert s.m() == 0;          // Line D // FIXME - can't prove this yet
+            //@ assert s.m() == 0;          // Line D 
         }
+    }
+
+    public void m3(/*@ non_null */ Super s) {
+        if (s instanceof Sub) {
+            //@ assert s.m() == 0;         // FIXME should be ok
+        }
+    }
+
+    public void m2(/*@ non_null*/ Super s) {
+        //@ assert s.m() == 0; // ERROR
     }
 }
