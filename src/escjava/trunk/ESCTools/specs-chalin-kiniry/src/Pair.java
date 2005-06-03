f168a0e6f8c582d49fe15561dd418198e023f179
jkiniry <jkiniry@c6399e9c-662f-4285-9817-23cccad57800>
2005-06-03 17:36:55 +0000

    Beefed up the (private) specs for make.  Make first, second spec_public. Added natural language comments to explain some of the invariants.

--- src/escjava/trunk/ESCTools/specs-chalin-kiniry/src/Pair.java
+++ src/escjava/trunk/ESCTools/specs-chalin-kiniry/src/Pair.java
@@ -7,10 +7,15 @@
 public final /*@ pure @*/ class Pair
 {
   /*@ private static invariant
-    @   (\forall Pair p;; Cons.isMember(chain, p));
+    @   (* chain contains all Pairs and only Pairs, that is ... *);
+    @ private static invariant
+    @   (\forall Object o;; Cons.isMember(chain, o) <==> (o instanceof Pair));
     @*/
 
   /*@ public static invariant
+    @   (* two pairs are referentially equal iff their firsts/seconds are 
+    @      referentially equal, that is ... *);
+    @ public static invariant
     @   (\forall Pair p, q;; p == q <==> p.first() == q.first() && 
     @                                    p.second() == q.second());
     @*/
@@ -19,8 +24,8 @@ public final /*@ pure @*/ class Pair
   //@ private static invariant (* every first is a non-null Pair *);
   private static /* null */ Cons chain = null;
 
-  private final /* null */ Object first;
-  private final /* null */ Object second;
+  /*@ spec_public @*/ private final /* null */ Object first;
+  /*@ spec_public @*/ private final /* null */ Object second;
 
   /*@ private normal_behavior
     @  modifies this.first, this.second;
@@ -32,35 +37,43 @@ public final /*@ pure @*/ class Pair
     this.second = second;
   }
 
+
   /*@ public normal_behavior
     @   modifies \nothing;
     @   ensures \result.first() == first;
     @   ensures \result.second() == second;
     @ also
     @ private normal_behavior
+    @   requires (\exists Pair p;; p.first() == first && p.second() == second);
+    @   requires_redundantly inChain(first, second);
+    @   modifies \nothing;
+    @   ensures_redundantly Cons.isMember(chain, \result);
+    @ also
+    @ private normal_behavior
+    @   requires !(\exists Pair p;; p.first() == first && p.second() == second);
+    @   requires_redundantly !inChain(first, second);
     @   modifies chain;
+    @   ensures \fresh(\result);
+    @   ensures chain.first() == \result && chain.second() == \old(chain);
     @   ensures_redundantly Cons.isMember(chain, \result);
     @*/
-  public static /*@ non_null @*/ Pair make(Object first, Object second) {
-    for (Cons c = chain; c != null; c = (Cons)(c.second())) {
-      Pair p = (Pair)(c.first());
-      if (p.first == first && p.second == second)
-        return p;
-    }
-    Pair result = new Pair(first, second);
+  public static /*@ non_null @*/ /* non_pure */ Pair make(Object first, Object second) {
+    Pair result = getPairFromChain(first, second);
+    if (result == null)
+      result = new Pair(first, second);
     chain = new Cons(result, chain);
     return result;
   }
 
-  /*@ normal_behavior
-    @   modifies \nothing;
+  /*@ public normal_behavior
+    @   ensures \result == first;
     @*/
   public Object first() {
     return first;
   }
 
-  /*@ normal_behavior
-    @   modifies \nothing;
+  /*@ public normal_behavior
+    @   ensures \result == second;
     @*/
   public Object second() {
     return second;
@@ -68,10 +81,67 @@ public final /*@ pure @*/ class Pair
   
   /*@ also 
     @ normal_behavior
-    @   modifies \nothing;
     @   ensures \result == (this == other);
     @*/
   public boolean equals(Object other) {
     return this == other;
+  }
+
+  // -----------------------------------------------------------------
+  // Helpers
+
+  /*@ private normal_behavior
+    @   ensures \result == (p != null && 
+    @			    p.first() == first && p.second() == second);
+    @*/
+  private static /*@ pure @*/ boolean same(/* null */ Pair p, 
+                                           /* null */ Object first, 
+                                           /* null */ Object second)
+  {
+    return p != null && p.first() == first && p.second() == second;
+  }
+
+  /*@ private normal_behavior
+    @   ensures \result == (chain == null
+    @	  ? null
+    @	  : same((Pair)chain.first(), first, second)
+    @		? (Pair)chain.first()
+    @		: getPairFromChainHelper((Cons)(chain.second()), first, second));
+    @*/
+  private static /*@ pure @*/ /* null */ 
+    Pair getPairFromChainHelper(/* null */ Cons chain,
+                                /* null */ Object first, 
+                                /* null */ Object second)
+  {
+    if(chain == null)
+      return null;
+
+    Pair p = (Pair)chain.first();
+    return same(p, first, second)
+      ? p
+      : getPairFromChainHelper((Cons)chain.second(), first, second);
+  }
+
+  /**
+   * Checks to see if a Pair with 'first' and 'second' is already in our chain.
+   */
+  /*@ private normal_behavior
+    @   ensures (* returns the Pair [first, second] in chain, otherwise returns null *);
+    @   ensures \result == getPairFromChainHelper(chain, first, second);
+    @*/
+  private static /*@ pure @*/ /* null */ Pair getPairFromChain(Object first, Object second) {
+    for (Cons c = chain; c != null; c = (Cons)(c.second())) {
+      Pair p = (Pair)(c.first());
+      if (p.first == first && p.second == second)
+        return p;
+    }
+    return null;
+  }
+
+  /*@ private normal_behavior
+    @   ensures \result <==> getPairFromChain(first, second) != null;
+    @*/
+  private static /*@ pure @*/ boolean inChain(Object first, Object second) {
+    return getPairFromChain(first, second) != null;
   }
 }
