c6118f790123f6178f67b85263e47e86b00c2a67
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2005-11-14 12:12:48 +0000

    Added some javadoc.

--- src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/TMethodCall.java
+++ src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/TMethodCall.java
@@ -2,17 +2,44 @@ package escjava.vcGeneration;
 
 import java.util.Vector;
 
+
+/**
+ * The class used to represent method calls for the new Vc gen tree.
+ * @version 14/11/2005
+ */
 public class TMethodCall extends TFunction {
 	private VariableInfo name;
 	private Vector argTypes = new Vector();
-	public static Vector methNames = new Vector();
-	public static Vector methDefs = new Vector();
+	
+	
 	/**
+	 * The vector containing all the method names (in forms of String).
+	 */
+	public static Vector methNames = new Vector();
+	/**
+	 * The vector containing the definition of the methods. The
+	 * definition is fetched following this pattern, for a method
+	 * called strMethName (a String).
+	 * <code>
+	 * methDefs.get({@link Vector#indexOf(java.lang.Object) methName.indexOf(strMethName)})
+	 * </code>
+	 */
+	public static Vector methDefs = new Vector();
+	
+	
+	/**
+	 * @see TNode#accept(TVisitor)
 	 * @param args
 	 */ 
 	public void accept(/*@ non_null @*/ TVisitor v){
 			v.visitTMethodCall(this);
 	}
+	
+	
+	/**
+	 * Construct a method call.
+	 * @param name The name of the method called.
+	 */
 	public TMethodCall(/*@ non_null @*/ String name) {
 		this.name = new VariableInfo(name, new TypeInfo("pure method"));
 		if(!methNames.contains(this.name)) {
@@ -20,9 +47,20 @@ public class TMethodCall extends TFunction {
 			methDefs.add(this);
 		}
 	}
+	
+	
+	/**
+	 * return the VariableInfo associated with the method name,
+	 * of interest for the return type of the method.
+	 * @return The variable info object associated with the method.
+	 */
 	public VariableInfo getName() {
 		return name;
 	}
+	
+	/**
+	 * @see TFunction#typeTree()
+	 */
     protected void typeTree(){
 
     	/*
@@ -36,6 +74,11 @@ public class TMethodCall extends TFunction {
     	}
 
     }
+    /**
+     * Returns a vector containing the types from the different arguments of
+     * the MethodCall. The pure method has the following signature: <code>elem(1) -> elem(1) -> ... -> {@link TNode#type this.type}</code>
+     * @return A vector containing elements from the TypeInfo class.
+     */
     public Vector getArgType() {
     	return argTypes;
     }
