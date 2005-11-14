3fbdacba4f12894d076a68c5bd43045b8ff73be1
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2005-11-14 14:57:51 +0000

    fixed some spaces in the prelude generator

--- src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/coq/PreludeGenerator.sh
+++ src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/coq/PreludeGenerator.sh
@@ -4,7 +4,7 @@ f=../../../../../docs/Escjava2-Logics/coq/defs.v
 cat Prelude.head > Prelude.java
 cat $f | sed 's/\\/\\\\/g' | \
 sed 's/"/\\"/g' | \
-sed 's/^/	out.println("/' |\
+sed 's/^/	   out.println("/' |\
  sed 's/$/");/' >> Prelude.java
 cat Prelude.tail >> Prelude.java
 
