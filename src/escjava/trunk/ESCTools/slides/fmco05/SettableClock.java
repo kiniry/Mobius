93552f04145d599d70dedc2e4152604e68c91c14
chalin <chalin@c6399e9c-662f-4285-9817-23cccad57800>
2006-03-24 16:45:57 +0000

    Didn't compile (parentheses missing).  Formatted code more nicely (on multiple lines) since the figure shows up on a page by itself anyways.

--- src/escjava/trunk/ESCTools/slides/fmco05/SettableClock.java
+++ src/escjava/trunk/ESCTools/slides/fmco05/SettableClock.java
@@ -17,8 +17,11 @@ class SettableClock extends TickTockClock {
       @   signals_only IllegalArgumentException;
       @*/
     public void setTime(int hour, int minute) {
-        if !(0 <= hour && hour <= 23 && 0 <= minute && minute <= 59)
+        if(!(0 <= hour && hour <= 23 && 0 <= minute && minute <= 59)){
              throw new IllegalArgumentException();
-        this.hour = hour; this.minute = minute; this.second = 0;
+	}
+        this.hour = hour; 
+	this.minute = minute; 
+	this.second = 0;
     }
 }
