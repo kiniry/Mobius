71175c8c6634481b42035c2af374493d13b15901
kiniry <kiniry@c6399e9c-662f-4285-9817-23cccad57800>
2006-04-28 17:42:15 +0000

    A new initial general exception for the PVS plugin.

---
+++ src/provereditor_plugin/provers/pvs/PvsEditor/src/ie/ucd/csi/srg/kindsoft/pvs/editor/PvsProverException.java
@@ -0,0 +1,26 @@
+/*
+ * $Id$ 
+ */
+
+package ie.ucd.csi.srg.kindsoft.pvs.editor;
+
+import prover.exec.AProverException;
+
+/**
+ * Description
+ * @author Joe Kiniry (kiniry@ucd.ie)
+ * @version Apr 28, 2006
+ *
+ */
+// @explanation Description.
+// @module 
+// @organisation School of Computer Science and Informatics
+// @copyright University College Dublin
+
+public class PvsProverException extends AProverException {
+
+  public PvsProverException(String description) {
+    super(description);
+  }
+
+}
