31c6be4336ed3a75c6188dd84cb7e9a6cb263829
alx <alx@c6399e9c-662f-4285-9817-23cccad57800>
2006-06-23 20:25:13 +0000

    added TODOs for JavaDoc

--- src/Umbra/source/umbra/BytecodeContribution.java
+++ src/Umbra/source/umbra/BytecodeContribution.java
@@ -21,20 +21,61 @@ import org.eclipse.ui.IEditorPart;
 import umbra.instructions.BytecodeController;
 
 /**
- * @author Wojtek W¹s
+ * @author Wojtek Wï¿½s
  */
 public class BytecodeContribution extends ControlContribution { 
 	
-	private int num = 0;
-	private boolean needNew = true;
+	/**
+     * TODO write description
+     */
+    private int num = 0;
+	/**
+     * TODO write description
+     */
+    private boolean needNew = true;
+    /**
+     * TODO write description
+     */    
 	private IEditorPart activeEditor;
-	private Label labelNum, labelOff, labelText;
+    /**
+     * TODO write description
+     */    
+	private Label labelNum;
+    /**
+     * TODO write description
+     */    
+    private Label labelOff;
+    /**
+     * TODO write description
+     */    
+    private Label labelText;
+    /**
+     * TODO write description
+     */    
 	private static BytecodeContribution inUse;
+    /**
+     * TODO write description
+     */    
 	private BytecodeController bcc;	
+    /**
+     * TODO write description
+     */    
 	private boolean ready = false;
+    /**
+     * TODO write description
+     */    
 	private boolean modTable = false;
+    /**
+     * TODO write description
+     */    
 	private boolean[] modified;
 	
+    /**
+     * TODO write description
+     * 
+     * @param doc TODO write description
+     * @throws TODO write description
+     */    
 	private void init(IDocument doc) throws BadLocationException
 	{
 		bcc = new BytecodeController();
@@ -48,10 +89,23 @@ public class BytecodeContribution extends ControlContribution {
 		return;
 	}
 	
+    /**
+     * TODO write description
+     */    
 	public class BytecodeListener implements IDocumentListener {
 		
-		int startRem = -1, stopRem = -1;
+        /**
+         * TODO write description
+         */    
+		int startRem = -1;
+        /**
+         * TODO write description
+         */    
+        int stopRem = -1;
 		
+        /**
+         * TODO write description
+         */    
 		public BytecodeListener() {
 		}
 
@@ -101,15 +155,28 @@ public class BytecodeContribution extends ControlContribution {
 		
 	}
 	
+    /**
+     * TODO write description
+     */    
 	protected BytecodeContribution() {
 		super("Bytecode");
 		inUse = this;
 	}
 	
+    /**
+     * TODO write description
+     * 
+     * @return TODO write description
+     */    
 	public static BytecodeContribution inUse() {
 		return inUse;
 	}
 	
+    /**
+     * TODO write description
+     * 
+     * @return TODO write description
+     */    
 	public static BytecodeContribution newItem() {
 		if (inUse != null) {
 			if (!inUse.needNew) {
@@ -120,10 +187,19 @@ public class BytecodeContribution extends ControlContribution {
 		return new BytecodeContribution();
 	}
 	
+    /**
+     * TODO write description
+     */    
 	public void survive() {
 		needNew = false;
 	}
 	
+    /**
+     * TODO write description
+     * 
+     * @param parent TODO write description
+     * @return TODO write description
+     */    
 	protected Control createControl(Composite parent) {
 		Composite composite = new Composite(parent, SWT.BORDER);
 		composite.setData(this);
@@ -135,46 +211,84 @@ public class BytecodeContribution extends ControlContribution {
 		return composite;
 	}
 	
+    /**
+     * TODO write description
+     */    
 	private void displayCorrect() {
 		labelText.setBackground(new Color(null, new RGB(0, 128, 0)));
 		labelText.setText("Correct");
 	}
 	
+    /**
+     * TODO write description
+     * 
+     * @param line TODO write description
+     */    
 	private void displayError(int line) {
 		labelText.setBackground(new Color(null, new RGB(255, 128, 0)));
 		labelText.setText("Error detected: " + line);
 	}
 
+    /**
+     * TODO write description
+     * 
+     * @param document TODO write description
+     */    
 	public void addListener(IDocument document) {
 		BytecodeListener listener = new BytecodeListener();
 		document.addDocumentListener(listener);
 	}
 
 	/**
-	 * @param editor
+     * TODO write description
+     * 
+	 * @param editor TODO write description
 	 */
 	public void setActiveEditor(IEditorPart editor) {
 		activeEditor = editor;
 	}
 	
+    /**
+     * TODO write description
+     */    
 	public void reinit() {
 		ready = false;
 	}
 	
+    /**
+     * TODO write description
+     * 
+     * @return TODO write description
+     */    
 	public boolean[] getModified() {
 		return bcc.getModified();
 	}
 	
+    /**
+     * TODO write description
+     * 
+     * @param TODO write description
+     */    
 	public void setModTable(boolean[] modified) {
 		this.modified = modified;
 		modTable = true;
 	}
 
+    /**
+     * TODO write description
+     * 
+     * @return TODO write description
+     */    
 	public String[] getCommentTab() {
 		return bcc.getComments();
 	}
 	
-	public String[] getInterlineTab() {
+	/**
+     * TODO write description
+     * 
+     * @return TODO write description
+     */
+     public String[] getInterlineTab() {
 		return bcc.getInterline();
 	}
 }
