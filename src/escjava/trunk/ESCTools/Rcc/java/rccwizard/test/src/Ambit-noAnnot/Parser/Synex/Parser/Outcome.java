56fb7749998fb4af7cdcc51dbd5238199b7cf45d
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2006-08-23 13:27:01 +0000

    none

--- src/escjava/trunk/ESCTools/Rcc/java/rccwizard/test/src/Ambit-noAnnot/Parser/Synex/Parser/Outcome.java
+++
@@ -1,9 +0,0 @@
-// Copyright 1996 Digital Equipment Corporation. Distributed only by permission.
-
-package Parser.Synex.Parser;
-
-/** The outcome of parsing. Any outcome must be a subclass of this.
-    Client outcomes should probably be subclasses of OutcomeLocated. */
-
-public class Outcome {
-}
