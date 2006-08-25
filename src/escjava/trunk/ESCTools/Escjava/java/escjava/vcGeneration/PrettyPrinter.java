e4f546e16dbd4732399b45a4f04d00d10d395e57
ghagen <ghagen@c6399e9c-662f-4285-9817-23cccad57800>
2006-08-25 16:58:46 +0000

    Added support for newline-terminated commenting to PrettyPrinter via beginC() and endC() methods.

--- src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/PrettyPrinter.java
+++ src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/PrettyPrinter.java
@@ -8,6 +8,9 @@ public class PrettyPrinter {
     private final String LBR;
     private final String RBR;
     private final String NL;
+
+
+    String commentMark = "";
     
     /*@ non_null @*/Writer out = null;
 
@@ -26,8 +29,9 @@ public class PrettyPrinter {
      * just append the parameter, N stands for normal
      */
     public/*@ non_null @*/Writer appendN(/*@ non_null @*/String s) throws IOException {
+        if (!commentMark.equals(""))
+          s.replaceAll(NL,NL+commentMark);
         out.write(s);
-
         return out;
     }
 
@@ -35,6 +39,8 @@ public class PrettyPrinter {
      * append indentation and parameter
      */
     public/*@ non_null @*/Writer append(/*@ non_null @*/String s) throws IOException {
+        if (!commentMark.equals(""))
+          s.replaceAll(NL,NL+commentMark);
         out.write(indentation.toString());
         out.write(s);
 
@@ -47,10 +53,14 @@ public class PrettyPrinter {
      * If you want to change the indentation style do it here.
      */
     public/*@ non_null @*/Writer appendI(/*@ non_null @*/String s) throws IOException {
+        if (!commentMark.equals(""))
+          s.replaceAll(NL,NL+commentMark);
         if (indentation.length() != 0) { // not first call
-            out.write(NL);
+            out.write(NL + commentMark);
             out.write(indentation.toString());
         }
+        else
+          out.write(commentMark);
 
         indentation.append(TAB);
 
@@ -64,6 +74,8 @@ public class PrettyPrinter {
      * If you want to change the indentation style do it here.
      */
     public/*@ non_null @*/Writer appendIwNl(/*@ non_null @*/String s) throws IOException {
+        if (!commentMark.equals(""))
+          s.replaceAll(NL,NL+commentMark);
         indentation.append(TAB);
         out.write(indentation.toString());
         out.write(LBR + s);
@@ -77,7 +89,7 @@ public class PrettyPrinter {
      * If you want to change the indentation style do it here.
      */
     public/*@ non_null @*/Writer reduceI() throws IOException {
-        out.write(NL);
+        out.write(NL + commentMark);
         indentation = indentation.delete(0, TAB.length());
         out.write(indentation + RBR);
 
@@ -99,5 +111,28 @@ public class PrettyPrinter {
     public/*@ non_null @*/String toString() {
         return out.toString();
     }
+
+    /**
+     * This adds a user-specified comment mark string to the beginning of each
+     * line.  It can be removed by wither recalling setC with the empty
+     * string or calling removeC.  It immediately adds one instance of the
+     * comment marker.
+     */
+    public /*@ non_null @*/Writer beginC(/*@ non_null @*/String s) throws IOException {
+      commentMark = s;
+      out.write(s);
+      return out;
+    }
+
+    /**
+     * This function removes the comment mark set by setC() and appends a
+     * newline;
+     */
+    public/*@ non_null @*/Writer endC() throws IOException {
+      commentMark = "";
+      out.write(NL);
+      return out;
+    }
+    
 
 }
