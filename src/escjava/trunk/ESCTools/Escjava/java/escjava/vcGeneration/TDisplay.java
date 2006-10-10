da3f91ab5c85fe20084cb282a4f0475a2e7b2fb0
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2006-10-10 15:56:50 +0000

    Added some methods to simplify the use of the TDisplay class: instead of taking 3 arguments the methods take only one and write the same informations. The method which take more arguments are marked deprecated since the new ones infer the two missing arguments. I added also some Javadoc

--- src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/TDisplay.java
+++ src/escjava/trunk/ESCTools/Escjava/java/escjava/vcGeneration/TDisplay.java
@@ -1,27 +1,42 @@
 package escjava.vcGeneration;
 
+import java.io.PrintStream;
+
+
+/**
+ * The class to print info message, warning messages and error messages.
+ */
 public class TDisplay {
-
-    private static boolean err;
-
-    private static boolean warn;
-
-    private static boolean info;
-
-    private static boolean colors;
-
+	/** tell whether or not errors should be printed */
+    private static boolean bErr;
+	/** tell whether or not warnings should be printed */
+    private static boolean bWarn;
+	/** tell whether or not infos should be printed */
+    private static boolean bInfo;
+    /** the stream to use to print errors */
+    private static PrintStream sErr = System.err;
+    /** the stream to use to print warnings */
+    private static PrintStream sWrn = System.err;
+    /** the stream to use to print infos */
+    private static PrintStream sNfo = System.err;
+    /** the error prompt */
     static/*@ non_null @*/String errPrompt;
-
+    /** the warning prompt */
     static/*@ non_null @*/String warnPrompt;
-
-    static/*@ non_null @*/String infoPrompt;
-
+    
+    /**
+     * Initialize the TDisplay class with it options, especially to
+     * tell if, how and what should be printed.
+     * @param err to tell whether or not to print the error messages
+     * @param warn to tell whether or not to print the warning messages
+     * @param info to tell whether or not to print the info messages
+     * @param colors to trigger the color mode
+     */
     static public void init(boolean err, boolean warn, boolean info, boolean colors) {
 
-        TDisplay.err = err;
-        TDisplay.warn = warn;
-        TDisplay.info = info;
-        TDisplay.colors = colors;
+        TDisplay.bErr = err;
+        TDisplay.bWarn = warn;
+        TDisplay.bInfo = info;
 
         if (!colors) {
             TDisplay.errPrompt = "Err ";
@@ -33,57 +48,115 @@ public class TDisplay {
 
     }
 
+    /**
+     * Print an error message, with the program point where the
+     * method was called.
+     * @param err the error message
+     */
+    public static void err(String err) {
+        if (TDisplay.bErr) {
+        	StackTraceElement [] ste = new Exception().getStackTrace();	
+            sErr.println(errPrompt + ste[1]);
+            sErr.println("  " + err);
+        }
+    }    
+    
+    /**
+     * Print an warning message, with the program point where the
+     * method was called.
+     * @param err the warning message
+     */
+    public static void warn(String warn) {
+
+        if (TDisplay.bWarn) {
+        	StackTraceElement [] ste = new Exception().getStackTrace();	
+        	sWrn.println(warnPrompt + ste[1]);
+            sWrn.println("  " + warn);
+        }
+    }
+    
+    /**
+     * Print an info message, with the program point where the
+     * method was called.
+     * @param info the info message
+     */
+    public static void info(String info) {
+        if (TDisplay.bInfo) {
+        	StackTraceElement [] ste = new Exception().getStackTrace();	
+            sNfo.println("[" + ste[1] + "]");
+            sNfo.println("[" + info + "]");
+        }
+    }
+    
+    /**
+     * @deprecated use {@link #err(String)} instead
+     */
     public static void err(/*@ non_null @*/Object o, /*@ non_null @*/String method, String err) {
 
-        if (TDisplay.err) {
-            System.err.println(errPrompt + o.getClass().getName() + "."
+        if (TDisplay.bErr) {
+            sErr.println(errPrompt + o.getClass().getName() + "."
                     + method);
-            System.err.println("  " + err);
+            sErr.println("  " + err);
         }
     }
 
+    /**
+     * @deprecated use {@link #warn(String)} instead
+     */
     public static void warn(/*@ non_null @*/Object o, /*@ non_null @*/String method, String warn) {
 
-        if (TDisplay.warn) {
-            System.err.println(warnPrompt + o.getClass().getName() + "."
+        if (TDisplay.bWarn) {
+        	sWrn.println(warnPrompt + o.getClass().getName() + "."
                     + method);
-            System.err.println("  " + warn);
+            sWrn.println("  " + warn);
         }
     }
 
+    /**
+     * @deprecated use {@link #info(String)} instead
+     */
     public static void info(/*@ non_null @*/Object o, /*@ non_null @*/String method, String info) {
 
-        if (TDisplay.info) {
-            System.err.println("[" + o.getClass().getName() + "." + method
+        if (TDisplay.bInfo) {
+            sNfo.println("[" + o.getClass().getName() + "." + method
                     + "]");
-            System.err.println("[" + info + "]");
+            sNfo.println("[" + info + "]");
         }
 
     }
 
+    /**
+     * @deprecated use {@link #err(String)} instead
+     */
     public static void err(/*@ non_null @*/String o, /*@ non_null @*/String method, String err) {
 
-        if (TDisplay.err) {
-            System.err.println(errPrompt + o + "." + method);
-            System.err.println("  " + err);
+        if (TDisplay.bErr) {
+            sErr.println(errPrompt + o + "." + method);
+            sErr.println("  " + err);
         }
 
     }
 
+    /**
+     * @deprecated use {@link #warn(String)} instead
+     */
     public static void warn(/*@ non_null @*/String o, /*@ non_null @*/String method, String warn) {
 
-        if (TDisplay.warn) {
-            System.err.println(warnPrompt + o + "." + method);
-            System.err.println("  " + warn);
+        if (TDisplay.bWarn) {
+            sWrn.println(warnPrompt + o + "." + method);
+            sWrn.println("  " + warn);
         }
 
     }
 
+    /**
+     * @deprecated use {@link #info(String)} instead
+     */
     public static void info(/*@ non_null @*/String o, /*@ non_null @*/String method, String info) {
 
-        if (TDisplay.info) {
-            System.err.println("[" + o + "." + method + "]");
-            System.err.println("[" + info + "]");
+        if (TDisplay.bInfo) {
+            sNfo.println("[" + o + "." + method + "]");
+            sNfo.println("[" + info + "]");
         }
 
     }
