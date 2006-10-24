bfa7884ea73c57bc6133f57299873aa304c36958
davidcok <davidcok@c6399e9c-662f-4285-9817-23cccad57800>
2006-10-24 10:39:21 +0000

    Adding modifies clauses

--- src/escjava/trunk/ESCTools/Escjava/test/junittests/Helper3.java
+++ src/escjava/trunk/ESCTools/Escjava/test/junittests/Helper3.java
@@ -13,23 +13,33 @@
 public class Helper3 {
   //@ non_null
   Object o;
-  
+
   // this method should not check as it calls a non-helper which violates the
   // object invariant in the precondition.
+  //@ modifies o;
   public Helper3() {
     non_helper();
   }
   
   // all other constructors and methods should check.
+  //@ modifies o;
   public Helper3(boolean b) {
     helper();
   }
+
+  // trying the nonhelper in a method
+  //@ modifies o;
+  public void m() {
+    //@ assert o != null;
+    non_helper();
+  }
   
-  //@ ensures o != null;
+  //@ modifies o;
   private void non_helper() {
     	o = new Object();
   }
   
+  //@ modifies o;
   //@ ensures o != null;
   private /*@ helper @*/ void helper() {
     o = new Object();
