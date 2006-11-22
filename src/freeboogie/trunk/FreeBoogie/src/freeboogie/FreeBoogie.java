5c11192ad6d7a14f4eb11ae07a5e70e321ccf656
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2006-11-22 08:58:07 +0000

    Hello world.

---
+++ src/freeboogie/trunk/FreeBoogie/src/freeboogie/FreeBoogie.java
@@ -0,0 +1,26 @@
+/** Public domain */
+package freeboogie;
+
+/**
+ * @author rgrig
+ * @reviewer TODO
+ */
+public class FreeBoogie {
+
+  /**
+   * 
+   */
+  public FreeBoogie() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello world!");
+  }
+
+}
