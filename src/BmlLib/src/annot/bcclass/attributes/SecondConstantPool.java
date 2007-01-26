a9be4f276c7e065035648691c2b675c6feec4daf
tomekb <tomekb@c6399e9c-662f-4285-9817-23cccad57800>
2007-01-26 09:04:41 +0000

    2nd cp

--- src/BmlLib/src/annot/bcclass/attributes/SecondConstantPool.java
+++ src/BmlLib/src/annot/bcclass/attributes/SecondConstantPool.java
@@ -29,68 +29,68 @@ public class SecondConstantPool implements BCAttribute {
 	int constant_pool_count;
 	Constant[] constant_pool;
 
-//	 /**
-//	   * Initialize instance from file data.
-//	   *
-//	   * @param file Input stream
-//	   * @throws IOException
-//	   */
-//	static ConstantClass ConstantClass(DataInputStream file) throws IOException
-//	  {
-//	    return new ConstantClass(file.readUnsignedShort());
-//	  }
-//
-//
-//	 static ConstantFieldref ConstantFieldref(DataInputStream file) throws IOException
-//	  {
-//		 return new  ConstantFieldref(file.readUnsignedShort(), file.readUnsignedShort());
-//		  }
-//	
-//	 static ConstantMethodref ConstantMethodref(DataInputStream file) throws IOException
-//	  {
-//		 return new  ConstantMethodref(file.readUnsignedShort(), file.readUnsignedShort());
-//		  }
-//
-//	 static ConstantInterfaceMethodref ConstantInterfaceMethodref(DataInputStream file) throws IOException
-//	  {
-//		 return new  ConstantInterfaceMethodref(file.readUnsignedShort(), file.readUnsignedShort());
-//		  }
-//	 
-//	 static ConstantString ConstantString(DataInputStream file) throws IOException
-//	  {
-//		 return new ConstantString((int)file.readUnsignedShort());
-//	  }
-//
-//	 
-//	 static ConstantInteger ConstantInteger(DataInputStream file) throws IOException
-//	  {
-//		 return new ConstantInteger(file.readInt());
-//	  }
-//
-//	 static ConstantFloat ConstantFloat(DataInputStream file) throws IOException
-//	  {
-//		 return new ConstantFloat(file.readFloat());
-//	  }
-//
-//	 static ConstantLong ConstantLong(DataInputStream file) throws IOException
-//	  {
-//		 return new ConstantLong(file.readLong());
-//	  }
-//
-//	 static ConstantDouble ConstantDouble(DataInputStream file) throws IOException
-//	  {
-//		 return new ConstantDouble(file.readDouble());
-//	  }
-//	 
-//	 static ConstantNameAndType ConstantNameAndType(DataInputStream file) throws IOException
-//	  {
-//		return new  ConstantNameAndType((int)file.readUnsignedShort(), (int)file.readUnsignedShort());
-//	  }
-//
-//	 static ConstantUtf8 ConstantUtf8(DataInputStream file) throws IOException
-//	  {
-//	    return new ConstantUtf8(file.readUTF());
-//	  }
+	 /**
+	   * Initialize instance from file data.
+	   *
+	   * @param file Input stream
+	   * @throws IOException
+	   */
+	static ConstantClass ConstantClass(DataInputStream file) throws IOException
+	  {
+	    return new ConstantClass(file.readUnsignedShort());
+	  }
+
+
+	 static ConstantFieldref ConstantFieldref(DataInputStream file) throws IOException
+	  {
+		 return new  ConstantFieldref(file.readUnsignedShort(), file.readUnsignedShort());
+		  }
+	
+	 static ConstantMethodref ConstantMethodref(DataInputStream file) throws IOException
+	  {
+		 return new  ConstantMethodref(file.readUnsignedShort(), file.readUnsignedShort());
+		  }
+
+	 static ConstantInterfaceMethodref ConstantInterfaceMethodref(DataInputStream file) throws IOException
+	  {
+		 return new  ConstantInterfaceMethodref(file.readUnsignedShort(), file.readUnsignedShort());
+		  }
+	 
+	 static ConstantString ConstantString(DataInputStream file) throws IOException
+	  {
+		 return new ConstantString((int)file.readUnsignedShort());
+	  }
+
+	 
+	 static ConstantInteger ConstantInteger(DataInputStream file) throws IOException
+	  {
+		 return new ConstantInteger(file.readInt());
+	  }
+
+	 static ConstantFloat ConstantFloat(DataInputStream file) throws IOException
+	  {
+		 return new ConstantFloat(file.readFloat());
+	  }
+
+	 static ConstantLong ConstantLong(DataInputStream file) throws IOException
+	  {
+		 return new ConstantLong(file.readLong());
+	  }
+
+	 static ConstantDouble ConstantDouble(DataInputStream file) throws IOException
+	  {
+		 return new ConstantDouble(file.readDouble());
+	  }
+	 
+	 static ConstantNameAndType ConstantNameAndType(DataInputStream file) throws IOException
+	  {
+		return new  ConstantNameAndType((int)file.readUnsignedShort(), (int)file.readUnsignedShort());
+	  }
+
+	 static ConstantUtf8 ConstantUtf8(DataInputStream file) throws IOException
+	  {
+	    return new ConstantUtf8(file.readUTF());
+	  }
 
 	 /**
 	   * Read one constant from the given file, the type depends on a tag byte.
@@ -101,21 +101,21 @@ public class SecondConstantPool implements BCAttribute {
 	  static final Constant readConstant(DataInputStream file)
 	    throws IOException, ClassFormatException
 	  {
-		System.out.println("Attributes//SecondConstantPool niezaimplementowany"); //XXX
 	    byte b = file.readByte(); // Read tag byte
+	    System.out.println("Constant: " + b);
 
 	    switch(b) {
-//	    case Constants.CONSTANT_Class:              return ConstantClass(file);
-//	    case Constants.CONSTANT_Fieldref:           return ConstantFieldref(file);
-//	    case Constants.CONSTANT_Methodref:          return ConstantMethodref(file);
-//	    case Constants.CONSTANT_InterfaceMethodref: return ConstantInterfaceMethodref(file);
-//	    case Constants.CONSTANT_String:             return ConstantString(file);
-//	    case Constants.CONSTANT_Integer:            return ConstantInteger(file);
-//	    case Constants.CONSTANT_Float:              return ConstantFloat(file);
-//	    case Constants.CONSTANT_Long:               return ConstantLong(file);
-//	    case Constants.CONSTANT_Double:             return ConstantDouble(file);
-//	    case Constants.CONSTANT_NameAndType:        return  ConstantNameAndType(file);
-//	    case Constants.CONSTANT_Utf8:               return ConstantUtf8(file);
+	    case Constants.CONSTANT_Class:              return ConstantClass(file);
+	    case Constants.CONSTANT_Fieldref:           return ConstantFieldref(file);
+	    case Constants.CONSTANT_Methodref:          return ConstantMethodref(file);
+	    case Constants.CONSTANT_InterfaceMethodref: return ConstantInterfaceMethodref(file);
+	    case Constants.CONSTANT_String:             return ConstantString(file);
+	    case Constants.CONSTANT_Integer:            return ConstantInteger(file);
+	    case Constants.CONSTANT_Float:              return ConstantFloat(file);
+	    case Constants.CONSTANT_Long:               return ConstantLong(file);
+	    case Constants.CONSTANT_Double:             return ConstantDouble(file);
+	    case Constants.CONSTANT_NameAndType:        return  ConstantNameAndType(file);
+	    case Constants.CONSTANT_Utf8:               return ConstantUtf8(file);
 	    default:
 	      throw new ClassFormatException("Invalid byte tag in constant pool: " + b);
 	    }
