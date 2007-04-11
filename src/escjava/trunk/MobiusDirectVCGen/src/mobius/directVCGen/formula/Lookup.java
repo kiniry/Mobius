cfdc7b4c6b72c33fa1ab6be7fb9b382e8f294669
hermann <hermann@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-11 14:26:23 +0000

    Initial Add of Lookup.java - contains spec lookup functions

---
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/Lookup.java
@@ -0,0 +1,48 @@
+package mobius.directVCGen.formula;
+
+import java.util.Vector;
+
+import escjava.sortedProver.Lifter.Term;
+import javafe.ast.ClassDecl;
+import javafe.ast.MethodDecl;
+
+public class Lookup {
+
+	/**
+	 * Returns the FOL Term representation of the precondition of method m.
+	 * @param m the method of interest
+	 */
+	public static Term precondition(MethodDecl m){
+		return null;
+	}
+
+	/**
+	 * Returns the FOL Term representation of the normal postcondition of method m.
+	 * @param m the method of interest
+	 */
+	public static Term normal_postcondition(MethodDecl m){
+		return null;
+	}
+
+	/**
+	 * Returns a vector of   FOL Term representations of the exceptional postconditions of method m.
+	 * The exceptional postcondition will always look like this: Sort => Term
+	 * @param m the method of interest
+	 */
+	public static Vector<Term> exceptional_postconditions(MethodDecl m){
+		return null;
+	}
+
+//	/**
+//	 * Returns all annotations for method m
+//	 * @param m the method of interest
+//	 */
+//	public static ??? method_annotation(MethodDecl m){
+//		return null;
+//	}
+	
+	public static Term invariant(ClassDecl c){
+		return null;
+	}
+	
+}
