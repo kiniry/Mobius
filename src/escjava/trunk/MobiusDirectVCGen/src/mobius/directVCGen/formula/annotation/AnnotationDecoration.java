ab824d1e638a49f5afe60276855b39752ca830ca
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-12 15:26:52 +0000

    small errors corrected...

--- src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/annotation/AnnotationDecoration.java
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/annotation/AnnotationDecoration.java
@@ -11,19 +11,53 @@ public class AnnotationDecoration extends ASTDecoration {
 	public AnnotationDecoration() {
 		super("annotationDecorations");
 	}
-	
+	static final int pre = 0;
+	static final int post = 1;
+
 	@SuppressWarnings("unchecked")
-	public Vector<AAnnotation> get(ASTNode n) {
-		return (Vector<AAnnotation>)super.get(n);
+	public Vector<AAnnotation> getAnnotPre(ASTNode n) {
+		Vector<Vector<AAnnotation>> v = getAnnot(n);
+		if(v == null)
+			return null;
+		else 
+			return v.get(pre);
 	}
 	
-	public void set(ASTNode n, Vector<AAnnotation> v) {
-		super.set(n, v);
+	@SuppressWarnings("unchecked")
+	public Vector<AAnnotation> getAnnotPost(ASTNode n) {
+		Vector<Vector<AAnnotation>> v = getAnnot(n);
+		if(v == null)
+			return null;
+		else 
+			return v.get(post);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public Vector<Vector<AAnnotation>> getAnnot(ASTNode n) {
+		Vector<Vector<AAnnotation>> v = (Vector<Vector<AAnnotation>>)super.get(n);
+		return v;
+	}
+	
+	
+	public void setAnnotPre(ASTNode n, Vector<AAnnotation> v) {
+		Vector<Vector<AAnnotation>> res = getAnnot(n);
+		if(res == null) {
+			super.set(n, res = new Vector<Vector<AAnnotation>>(2));
+		}
+		res.set(pre, v);
+	}
+	public void setAnnotPost(ASTNode n, Vector<AAnnotation> v) {
+		Vector<Vector<AAnnotation>> res = getAnnot(n);
+		if(res == null) {
+			super.set(n, res = new Vector<Vector<AAnnotation>>(2));
+		}
+		res.set(post, v);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public AAnnotation getLast(ASTNode n) {
-		Vector<AAnnotation> v =  (Vector<AAnnotation>)super.get(n);
+	public AAnnotation getLastPre(ASTNode n) {
+		Vector<AAnnotation> v =  (Vector<AAnnotation>)getAnnotPre(n);
 		if (v == null || v.size() == 0)
 			return null;
 		return v.lastElement();
