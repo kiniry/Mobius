8f373c0d564d47514d758a5abd05b588d0aecfca
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-12 11:53:25 +0000

    new version of the VCEntry file

--- src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/vcgen/intern/VCEntry.java
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/vcgen/intern/VCEntry.java
@@ -1,8 +1,11 @@
 package mobius.directVCGen.vcgen.intern;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javafe.ast.Identifier;
+import javafe.ast.Type;
 import javafe.ast.TypeDecl;
 import escjava.sortedProver.Lifter.QuantVariableRef;
 import escjava.sortedProver.Lifter.Term;
@@ -12,7 +15,7 @@ public class VCEntry {
 	public Post post;
 	
 	/** the excp post condition... */
-	public final HashMap<TypeDecl, Post> excpost = new HashMap<TypeDecl, Post>();
+	public final List<ExcpPost> excpost = new ArrayList<ExcpPost>();
 	
 	public final HashMap<Identifier, Post> lbrpost = new HashMap<Identifier, Post>(); 
 	public Post brpost;
@@ -23,6 +26,29 @@ public class VCEntry {
 		
 	}
 	
+
+	
+	public VCEntry(VCEntry ve) {
+		post = ve.post;
+		brpost = ve.brpost;
+		contpost = ve.contpost;
+		excpost.addAll(ve.excpost);
+		lbrpost.putAll(ve.lbrpost);
+		lcontpost.putAll(ve.lcontpost);
+	}
+
+	public static class ExcpPost {
+		public final Type excp;
+		public final Post post;
+		public ExcpPost (Type excp, Post p) {
+			this.excp = excp;
+			this.post = p;
+		}
+		public String toString() {
+			return "( " + excp + ", " + post + ")";
+		}
+	}
+
 	public static class Post {
 		/** the temporary variable; used in the vcGen of expressions */
 		public final QuantVariableRef var;
