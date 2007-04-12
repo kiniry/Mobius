6d24f249c03131ca343389236a32c1834189d917
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-12 17:10:13 +0000

    added some value constants

--- src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/Num.java
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/Num.java
@@ -4,24 +4,33 @@ import escjava.sortedProver.Lifter.Term;
 
 public class Num {
 
-	public static Term value(Long long1) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Term value(Long l) {
+		
+		return Formula.lf.mkIntLiteral(l);
 	}
 
-	public static Term value(Integer integer) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Term value(Integer i) {
+		return Formula.lf.mkIntLiteral(i.longValue());
 	}
 
-	public static Term value(Byte byte1) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Term value(Byte b) {
+		return Formula.lf.mkIntLiteral(b.longValue());
 	}
 
-	public static Term value(Short short1) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Term value(Short s) {
+		return Formula.lf.mkIntLiteral(s.longValue());
+	}
+
+	public static Term value(Float f) {
+		return Formula.lf.mkRealLiteral(f.doubleValue());
+	}
+
+	public static Term value(Character c) {
+		return Formula.lf.mkIntLiteral(c.charValue());
+	}
+
+	public static Term value(Double d) {		
+		return Formula.lf.mkRealLiteral(d);
 	}
 
 }
