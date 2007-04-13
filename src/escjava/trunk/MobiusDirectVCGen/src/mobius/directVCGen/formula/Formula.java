582b7de0eb5c67ba4b6ef72ca26971b007321f17
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-13 08:23:37 +0000

    added some helper methods

--- src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/Formula.java
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/Formula.java
@@ -1,8 +1,11 @@
 package mobius.directVCGen.formula;
 
 import javafe.ast.Type;
+import javafe.ast.VarInit;
+import javafe.tc.FlowInsensitiveChecks;
 import escjava.sortedProver.Lifter;
 import escjava.sortedProver.Lifter.Term;
+import escjava.sortedProver.NodeBuilder.Sort;
 
 public class Formula {
 	static Lifter lf = new Lifter(null);
@@ -14,5 +17,8 @@ public class Formula {
 	public static Term translate(Type type) {
 		return lf.symbolRef(type.toString());
 	}
-	
+	public static Sort getSort(VarInit e) {
+		Type t = FlowInsensitiveChecks.getType(e);
+		return lf.typeToSort(t);
+	}
 }
