2a9ef26b8c907bc0562345619d701b1798b3c7ba
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-13 15:36:36 +0000

    commented the file

--- src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/annotation/AnnotationDecoration.java
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/annotation/AnnotationDecoration.java
@@ -9,21 +9,42 @@ import escjava.sortedProver.Lifter.Term;
 import javafe.ast.ASTDecoration;
 import javafe.ast.ASTNode;
 
+/**
+ * This class is made to decorate the AST give by Javafe with the annotations
+ * needed. 
+ * @see ASTDecoration javafe.ast.ASTNode
+ * @author J. Charles
+ */
 public class AnnotationDecoration extends ASTDecoration {
-
+	/** the current instance initialized of the annotation decorations */
 	public static final AnnotationDecoration inst = new AnnotationDecoration();
+	
+	/**
+	 * Constructor of the decoration helper.
+	 */
 	public AnnotationDecoration() {
 		super("annotationDecorations");
 	}
 	
+	/**
+	 * A data structure to stock the informations inside the AST.
+	 * @author J. Charles
+	 */
 	public static class Annotation {
+		/** the pre annotations of the decorated instruction */
 		protected final Vector<AAnnotation> pre = new Vector<AAnnotation>();
-
+		/** the post annotations of the decorated instruction */
 		protected final Vector<AAnnotation> post = new Vector<AAnnotation>();
-
+		/** the invariant associated with a while instruction */
 		protected Term inv = null;
 	}
 
+	/**
+	 * Retrieve the annotation preceding the instruction.
+	 * @param n the node to retrieve the annotation from
+	 * @return an annotation or <code>null</code> if the 
+	 * node has not been decorated
+	 */
 	public Vector<AAnnotation> getAnnotPre(ASTNode n) {
 		Annotation v = getAnnot(n);
 		if(v == null)
@@ -32,6 +53,12 @@ public class AnnotationDecoration extends ASTDecoration {
 			return v.pre;
 	}
 	
+	/**
+	 * Retrieve the annotation being after the given instruction.
+	 * @param n the node from which to fetch the annotation
+	 * @return an annotation or <code>null</code> if the node 
+	 * has not been decorated
+	 */
 	public Vector<AAnnotation> getAnnotPost(ASTNode n) {
 		Annotation v = getAnnot(n);
 		if(v == null)
@@ -40,14 +67,22 @@ public class AnnotationDecoration extends ASTDecoration {
 			return v.post;
 	}
 	
-	
+	/**
+	 * Retrieve the decoration of a given node
+	 * @param n the node from which to retrieve the decoration
+	 * @return the decoration object
+	 */
 	@SuppressWarnings("unchecked")
 	public Annotation getAnnot(ASTNode n) {
 		Annotation v = (Annotation) super.get(n);
 		return v;
 	}
 	
-	
+	/**
+	 * Sets the annotation preceding the node
+	 * @param n the node to decorate
+	 * @param v the annotation to set
+	 */
 	public void setAnnotPre(ASTNode n, Vector<AAnnotation> v) {
 		Annotation res = getAnnot(n);
 		if(res == null) {
@@ -56,6 +91,12 @@ public class AnnotationDecoration extends ASTDecoration {
 		res.pre.clear();
 		res.pre.addAll(v);
 	}
+	
+	/**
+	 * Sets the annotation which is after the node
+	 * @param n the node to decorate
+	 * @param v the annotation to set
+	 */
 	public void setAnnotPost(ASTNode n, Vector<AAnnotation> v) {
 		Annotation res = getAnnot(n);
 		if(res == null) {
@@ -65,6 +106,11 @@ public class AnnotationDecoration extends ASTDecoration {
 		res.post.addAll(v);
 	}
 	
+	/**
+	 * Sets the invariant associated with the given node
+	 * @param n the node to decorate
+	 * @param inv the invariant to set
+	 */
 	public void setInvariant(ASTNode n, Term inv) {
 		Annotation res = getAnnot(n);
 		if(res == null) {
@@ -73,7 +119,11 @@ public class AnnotationDecoration extends ASTDecoration {
 		res.inv = inv;
 	}
 	
-	
+	/**
+	 * Retrieve the invariant associated with the node
+	 * @param n the node to decorate
+	 * @return the invariant the node is decorated with
+	 */
 	@SuppressWarnings("unchecked")
 	public Term getInvariant(ASTNode n) {
 		// jgc: while no proper annotations are made - returns Logic.True...
