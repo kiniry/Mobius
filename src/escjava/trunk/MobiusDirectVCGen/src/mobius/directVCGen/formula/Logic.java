652a60ec3f2306606fda9aa837483b7ea37217fe
hermann <hermann@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-23 16:36:30 +0000

    Tidy up and added functionality for < > <= >= ...

--- src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/Logic.java
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/Logic.java
@@ -7,9 +7,54 @@ import escjava.sortedProver.Lifter.QuantTerm;
 import escjava.sortedProver.Lifter.QuantVariable;
 import escjava.sortedProver.Lifter.QuantVariableRef;
 import escjava.sortedProver.Lifter.Term;
+import escjava.sortedProver.NodeBuilder.PredSymbol;
 import escjava.sortedProver.NodeBuilder.Sort;
 
 public class Logic {
+	
+	private static Term logicBinaryOp(Term f1, Term f2, PredSymbol sym){
+		if((f1.getSort() != sort || f2.getSort() != sort))
+			throw new IllegalArgumentException("Bad type. Expecting predicates, " +
+					"found: " + f1.getSort() + " and " + f2.getSort());
+		return Formula.lf.mkFnTerm(sym, new Term[]{f1, f2});
+	}
+	
+	private static Term logicUnaryOp(Term f, PredSymbol sym){
+		if(f.getSort() != sort)
+			throw new IllegalArgumentException("Bad type. Expecting predicate, " +
+				"found: " + f.getSort());
+		return Formula.lf.mkFnTerm(sym, new Term []{f});
+	}
+	
+	private static Term numBinaryOp(Term l, Term r, int tag){
+		if(l.getSort() != r.getSort() &&
+				(!Num.isNum(l.getSort()) || !Num.isNum(r.getSort())))
+			throw new IllegalArgumentException("The sort of " + l + 
+					" is different from the sort of " + r + ".");
+		FnTerm t = null;
+		if (l.getSort() == Num.sortInt) {
+			if(r.getSort() == Num.sortReal) {
+				l = Num.intToReal(l);
+				t = Formula.lf.mkFnTerm(Formula.lf.symRealPred, new Term[] {l, r});
+			}
+			else {
+				t = Formula.lf.mkFnTerm(Formula.lf.symIntPred, new Term[] {l, r});
+			}
+			
+		}
+		else if (l.getSort() == Num.sortReal) {
+			if(r.getSort() == Num.sortInt) {
+				r = Num.intToReal(r);
+			}
+			t = Formula.lf.mkFnTerm(Formula.lf.symRealPred, new Term[] {l, r});
+		}
+		else {
+			throw new IllegalArgumentException("The sort " + l.getSort() + " is invalid!"); 
+		}
+		t.tag = tag;
+		return t;
+	}
+	
 	/** the sort to represent the predicates */
 	public static Sort sort = Formula.lf.sortPred;
 
@@ -28,6 +73,22 @@ public class Logic {
 	}
 
 	/**
+	 * Create a BoolProp object, a conversion from a boolean object
+	 * to a property object if necessary.
+	 * @param e the boolean object to convert
+	 * @return the BoolProp conversion object
+	 */
+	public static Term boolToProp(Term e) {	
+		if (e.getSort() == Logic.sort)
+			return Formula.lf.mkFnTerm(Formula.lf.symIsTrue, new Term[] {e});
+		else if (e.getSort() == Bool.sort)
+			return e;
+		else 
+			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
+					"found: " + e.getSort());
+	}
+	
+	/**
 	 * Create a And in the prop territory, from 2 booleans or
 	 * 2 properties. The 2 arguments should not have different types
 	 * or bad types (other than prop).
@@ -36,24 +97,27 @@ public class Logic {
 	 * @return a newly created and connector
 	 */
 	public static Term and(Term f1, Term f2) {
-		if((f1.getSort() != sort && f2.getSort() != sort))
-			throw new IllegalArgumentException("Bad type when creating and, " +
-					"found: " + f1.getSort() + " and " + f2.getSort());
-		return Formula.lf.mkFnTerm(Formula.lf.symAnd, new Term[]{f1, f2});
+		return logicBinaryOp(f1,f2, Formula.lf.symAnd);
 	}
 	
 	/**
-	 * Create a BoolProp object, a conversion from a boolean object
-	 * to a property object.
-	 * @param e the boolean object to convert
-	 * @return the BoolProp conversion object
+	 * Create an object representing an Or.
+	 * @param f1 the left parameter of the or
+	 * @param f2 the right parameter of the or
+	 * @return the newly created object
 	 */
-	public static Term boolToProp(Term e) {
-		if(e.getSort() != Bool.sort)
-			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
-				"found: " + e.getSort());
-		return Formula.lf.mkFnTerm(Formula.lf.symIsTrue, new Term[] {e});
-		
+	public static Term or(Term f1, Term f2) {
+		return logicBinaryOp(f1,f2, Formula.lf.symOr);
+	}
+	
+	
+	/**
+	 * Creates and returns the negation of a formula
+	 * @param f the formula to negate (of type prop)
+	 * @return return the new not construct
+	 */
+	public static Term not(Term f) {
+		return logicUnaryOp(f, Formula.lf.symNot);
 	}
 	
 	/**
@@ -79,7 +143,7 @@ public class Logic {
 		else if (l.getSort() == Num.sortInt) {
 			if(r.getSort() == Num.sortReal) {
 				l = Num.intToReal(l);
-				t = Formula.lf.mkFnTerm(Formula.lf.symRealBoolFn, new Term[] {l, r});
+				t = Formula.lf.mkFnTerm(Formula.lf.symRealPred, new Term[] {l, r});
 				t.tag = NodeBuilder.predEQ;
 			}
 			else {
@@ -107,13 +171,7 @@ public class Logic {
 	 * @return a nicely created implies
 	 */
 	public static Term implies(Term f1, Term f2) {
-		if(f1 == null || f2 == null)
-			throw new NullPointerException("Arguments were:" + f1 +", " +  f2);
-		if((f1.getSort() != f2.getSort() && f1.getSort() != sort))
-			throw new IllegalArgumentException("Bad type when creating the implies, " +
-					"found: " + f1.getSort() + " and " + f2.getSort());
-
-		return Formula.lf.mkFnTerm(Formula.lf.symImplies, new Term[]{f1, f2});
+		return logicBinaryOp(f1,f2, Formula.lf.symImplies);
 	}
 	
 	/**
@@ -123,42 +181,8 @@ public class Logic {
 	 * @return a nicely created fullimplies
 	 */
 	public static Term fullImplies(Term f1, Term f2) {
-		if(f1 == null || f2 == null)
-			throw new NullPointerException("Arguments were:" + f1 +", " +  f2);
-		if((f1.getSort() != f2.getSort() && f1.getSort() != sort))
-			throw new IllegalArgumentException("Bad type when creating the implies, " +
-					"found: " + f1.getSort() + " and " + f2.getSort());
-
-		return Formula.lf.mkFnTerm(Formula.lf.symIff, new Term[]{f1, f2});
+		return logicBinaryOp(f1,f2, Formula.lf.symIff);
 	} 
-	
-	
-	/**
-	 * Create an object representing an Or.
-	 * @param f1 the left parameter of the or
-	 * @param f2 the right parameter of the or
-	 * @return the newly created object
-	 */
-	public static Term or(Term f1, Term f2) {
-		if((f1.getSort() != f2.getSort() && f1.getSort() != Formula.lf.sortPred))
-			throw new IllegalArgumentException("Bad type when creating or, " +
-					"found: " + f1.getSort() + " and " + f2.getSort());
-
-		return Formula.lf.mkFnTerm(Formula.lf.symOr, new Term[]{f1, f2});
-	}
-	
-	
-	/**
-	 * Creates and returns the negation of a formula
-	 * @param f the formula to negate (of type prop)
-	 * @return return the new not construct
-	 */
-	public static Term not(Term f) {
-		if(f.getSort() != sort)
-			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
-				"found: " + f.getSort());
-		return Formula.lf.mkFnTerm(Formula.lf.symNot, new Term []{f});
-	}
 	
 	/**
 	 * Creates a universal binding for only one variable from the formula f.
@@ -169,7 +193,7 @@ public class Logic {
 	public static QuantTerm forall(QuantVariable v, Term f) {
 	
 		if(f.getSort() != sort)
-			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
+			throw new IllegalArgumentException("Bad type when creating forall, " +
 				"found: " + f.getSort());
 		
 		return Formula.lf.mkQuantTerm(true, new QuantVariable [] {v}, f, null, null);
@@ -184,7 +208,7 @@ public class Logic {
 	public static QuantTerm forall(QuantVariableRef v, Term f) {
 	
 		if(f.getSort() != sort)
-			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
+			throw new IllegalArgumentException("Bad type when creating forall, " +
 				"found: " + f.getSort());
 		
 		return Formula.lf.mkQuantTerm(true, new QuantVariable [] {v.qvar}, f, null, null);
@@ -199,7 +223,7 @@ public class Logic {
 	public static QuantTerm forall(QuantVariable[] v, Term f) {
 	
 		if(f.getSort() != sort)
-			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
+			throw new IllegalArgumentException("Bad type when creating forall, " +
 				"found: " + f.getSort());
 		
 		return Formula.lf.mkQuantTerm(true, v, f, null, null);
@@ -214,7 +238,7 @@ public class Logic {
 	public static QuantTerm exists(QuantVariable v, Term f) {
 	
 		if(f.getSort() != sort)
-			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
+			throw new IllegalArgumentException("Bad type when creating exists, " +
 				"found: " + f.getSort());
 		
 		return Formula.lf.mkQuantTerm(false, new QuantVariable [] {v}, f, null, null);
@@ -229,7 +253,7 @@ public class Logic {
 	public static QuantTerm exists(QuantVariable[] v, Term f) {
 	
 		if(f.getSort() != sort)
-			throw new IllegalArgumentException("Bad type when creating BoolProp, " +
+			throw new IllegalArgumentException("Bad type when creating exists, " +
 				"found: " + f.getSort());
 		
 		return Formula.lf.mkQuantTerm(false, v, f, null, null);
@@ -240,23 +264,63 @@ public class Logic {
 	}
 	
 	/**
-	 * Main for testing purpose.
-	 * @param args ignored
+	 * Returns a boolean le expression.
+	 * @param l The left element of the expression
+	 * @param r The right expression of the expression
+	 * @return The and expression a FnTerm with tag {@link NodeBuilder#predLE}
 	 */
-	public static void main(String [] args) {
-		QuantVariable [] vars = { Expression.var("v1", Logic.sort),
-								  Expression.var("v2", Bool.sort) };
-		
-		QuantVariableRef rv1 = Expression.refFromVar(vars[0]);
-		QuantVariableRef rv2 = Expression.refFromVar(vars[1]);
-		Term formula = 
-			 Logic.forall(vars, Logic.implies(rv1, rv2));
-		System.out.println(formula);
-		System.out.println(formula.subst(rv2,
-				Logic.implies(Logic.boolToProp(rv2), 
-						Logic.False())));
-		System.out.println(Logic.and(Logic.True(), Logic.False()));
+	public static Term le(Term l, Term r) {
+		return numBinaryOp(l,r,NodeBuilder.predLE);
 	}
+
+	/**
+	 * Returns a boolean lt expression.
+	 * @param l The left element of the expression
+	 * @param r The right expression of the expression
+	 * @return The and expression a FnTerm with tag {@link NodeBuilder#predLE}
+	 */
+	public static Term lt(Term l, Term r) {		
+		return numBinaryOp(l,r,NodeBuilder.predLT);
+	}
+
+	/**
+	 * Returns a boolean ge expression.
+	 * @param l The left element of the expression
+	 * @param r The right expression of the expression
+	 * @return The and expression a FnTerm with tag {@link NodeBuilder#predLE}
+	 */
+	public static Term ge(Term l, Term r) {		
+		return numBinaryOp(l,r,NodeBuilder.predGE);
+	}
+	
+	/**
+	 * Returns a boolean gt expression.
+	 * @param l The left element of the expression
+	 * @param r The right expression of the expression
+	 * @return The and expression a FnTerm with tag {@link NodeBuilder#predLE}
+	 */
+	public static Term gt(Term l, Term r) {		
+		return numBinaryOp(l,r,NodeBuilder.predGT);
+	}
+	
+//	/**
+//	 * Main for testing purpose.
+//	 * @param args ignored
+//	 */
+//	public static void main(String [] args) {
+//		QuantVariable [] vars = { Expression.var("v1", Logic.sort),
+//								  Expression.var("v2", Bool.sort) };
+//		
+//		QuantVariableRef rv1 = Expression.refFromVar(vars[0]);
+//		QuantVariableRef rv2 = Expression.refFromVar(vars[1]);
+//		Term formula = 
+//			 Logic.forall(vars, Logic.implies(rv1, rv2));
+//		System.out.println(formula);
+//		System.out.println(formula.subst(rv2,
+//				Logic.implies(Logic.boolToProp(rv2), 
+//						Logic.False())));
+//		System.out.println(Logic.and(Logic.True(), Logic.False()));
+//	}
 
 	public static Term equalsZero(Term t) {
 		Term res = null;
@@ -271,6 +335,7 @@ public class Logic {
 		}
 		return res;
 	}
+	
 	public static Term equalsNull(Term t) {
 		Term res = null;
 		if (t.getSort() == Ref.sort) {
@@ -280,25 +345,5 @@ public class Logic {
 			throw new IllegalArgumentException("The sort " + t.getSort() + " is invalid!"); 
 		}
 		return res;
-	}
-
-	public static Object ge(Term term, Term term2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static Object gt(Term term, Term term2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static Object le(Term term, Term term2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public static Object lt(Term term, Term term2) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
