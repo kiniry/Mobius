52cf58deb500a2cad941572368e2cd77ab71a95b
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-26 15:52:10 +0000

    functional tests for the boogie concrete grammar --- the microsoft boogie benchmark suite

--- src/freeboogie/trunk/FreeBoogie/src/freeboogie/parser/Main.java
+++ src/freeboogie/trunk/FreeBoogie/src/freeboogie/parser/Main.java
@@ -2,11 +2,14 @@
 
 package freeboogie.parser;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
+
+import freeboogie.util.Err;
 
 /**
  * Generate a pretty print of the AST constructed during parsing.
@@ -18,12 +21,19 @@ public class Main {
 
   /**
    * @param args
+   * @throws IOException 
    * @throws RecognitionException 
    */
   public static void main(String[] args) throws IOException, RecognitionException {
-    FbLexer lexer = new FbLexer(new ANTLRInputStream(System.in));
-    CommonTokenStream tokens = new CommonTokenStream(lexer);
-    FbParser parser = new FbParser(tokens);
-    parser.program();
+    for (int i = 0; i < args.length; ++i) {
+      try {
+        FbLexer lexer = new FbLexer(new ANTLRFileStream(args[i]));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        FbParser parser = new FbParser(tokens);
+        parser.program();
+      } catch (FileNotFoundException e) {
+        Err.error("I couldn't read from " + args[i] + ". Nevermind.");
+      }
+    }
   }
 }
