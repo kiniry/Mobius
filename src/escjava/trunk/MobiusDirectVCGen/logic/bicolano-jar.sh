4e6a2909b91aa5a80c3fab2026e29780aa3b4753
everest <everest@c6399e9c-662f-4285-9817-23cccad57800>
2007-04-27 14:45:05 +0000

    better script

--- src/escjava/trunk/MobiusDirectVCGen/logic/bicolano-jar.sh
+++ src/escjava/trunk/MobiusDirectVCGen/logic/bicolano-jar.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd Formalisation; make; cd ..
-jar cvf bicolano.jar `find ./ -name *.vo` defs_types.v
+jar cvf bicolano.jar `find ./Formalisation/Bicolano -name "*.v"` \
+                     `find ./Formalisation/Bicolano -name "*.vo"` \
+                     `find ./Formalisation/Library -name "*.v"` \
+                     `find ./Formalisation/Library -name "*.vo"`  defs_types.v
