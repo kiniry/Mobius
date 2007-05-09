728c716a2cc19a6048b179364c3da93935dd6fdf
jkiniry <jkiniry@c6399e9c-662f-4285-9817-23cccad57800>
2007-05-09 23:47:38 +0000

    added maps and unmaps signatures

--- src/escjava/trunk/ESCTools/specs-chalin-kiniry/src/Map.java
+++ src/escjava/trunk/ESCTools/specs-chalin-kiniry/src/Map.java
@@ -4,5 +4,12 @@ package escjava.model_classes;
 // immutable!
 public final class Map
 {
+  Map maps(Domain domainElement, Range rangeElement) {
+    //@ assert false;
+  }
+
+  Map unmap(Domain domainElement) {
+    //@ assert false;
+  }
 }
 
