3d5c8c77f7d1d7fe418196b58ff1de7d73aa27cc
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2007-05-17 15:58:55 +0000

    Adding support for Java 1.5 bytecode, but not yet for Java 1.5 sourcecode(1) javafe.reader.BinReader now calls javafe.reader.BCELReader(2) The makefiles have been updated to include bcel-5.2.jar in the classpath

--- src/escjava/trunk/ESCTools/Javafe/java/javafe/reader/BinReader.java
+++ src/escjava/trunk/ESCTools/Javafe/java/javafe/reader/BinReader.java
@@ -16,6 +16,8 @@ import javafe.genericfile.*;
 /**
  * A BinReader is a Reader that reads in CompilationUnits from binary
  * files (.class files).
+ * 
+ * @deprecated BCELReader is needed for Java 1.5 bytecode
  *
  * <p> BinReaders do not cache the results of their reading and always
  * return spec files.
@@ -43,8 +45,9 @@ public class BinReader extends Reader
      * Attempt to read and parse a CompilationUnit from *binary*
      * target.   Any errors encountered are reported via
      * javafe.util.ErrorSet.  Null is returned iff an error was
-     * encountered.<p>
+     * encountered.<p>  
      *
+     * @deprecated BCELReader.read is needed for Java 1.5 bytecode
      *
      * We always return a spec file.  (avoidSpec is ignored)<p>
      *
@@ -55,34 +58,10 @@ public class BinReader extends Reader
      */
     public CompilationUnit read(/*@non_null*/GenericFile target, boolean avoidSpec) {
 	javafe.util.Info.out("[loading " + target.getHumanName() + "]");
-	try {
-	    ASTClassFileParser parser = new ASTClassFileParser(target,false);
-		// if the 2nd argument above is false, omit all bodies.
-		// For some reason, despite the comment above, bodies are
-		// sometimes included.
-	    TypeDeclVec types =
-		TypeDeclVec.make(new TypeDecl[] { parser.typeDecl });
-            TypeDeclElemVec emptyTypeDeclElemVec = TypeDeclElemVec.make();
-	    ImportDeclVec emptyImportDeclVec = ImportDeclVec.make();
-	    CompilationUnit result = 
-		CompilationUnit.make(parser.classPackage,
-				     null,
-				     emptyImportDeclVec,
-				     types,
-				     parser.classLocation,
-				     emptyTypeDeclElemVec); 
-	    return result;
-	}
-	catch (IOException e)
-	{
-	  javafe.util.ErrorSet.error("I/O error: " + e.getMessage());
-	}
-	catch (ClassFormatError e)
-	{
-	  javafe.util.ErrorSet.error("Class format error: " + e.getMessage());
-	}
-
-	return null;
+	
+	// Use BCEL to parse Java 1.5 bytecode
+	BCELReader bcelReader = new BCELReader();
+	return bcelReader.read (target, avoidSpec);
     }
 
     /***************************************************
