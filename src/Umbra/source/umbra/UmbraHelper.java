96cffa7bf7dbde9f85e582e41868521df3b3f809
alx <alx@c6399e9c-662f-4285-9817-23cccad57800>
2007-06-11 12:53:11 +0000

    + Cleaning in the Umbra package

--- src/Umbra/source/umbra/UmbraHelper.java
+++ src/Umbra/source/umbra/UmbraHelper.java
@@ -15,6 +15,7 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
 import org.eclipse.ui.part.FileEditorInput;
 
+
 /**
  * This is just container for common operations used in the
  * application.
@@ -24,161 +25,196 @@ import org.eclipse.ui.part.FileEditorInput;
  */
 public class UmbraHelper {
 
-	/**
-	 * The file extension for the Java files.
-	 */
-	static public final String JAVA_EXTENSION       = ".java";
-	/**
-	 * The file extension for the Java class files.
-	 */
-	static public final String CLASS_EXTENSION      = ".class";
-	/**
-	 * The file extension for the files with the bytecode representation.
-	 */
-	static public final String BYTECODE_EXTENSION   = ".btc";
-	/**
-	 * The file extension for the history files.
-	 */
-	static public final String BYTECODE_HISTORY_EXTENSION   = ".bt";
-	/**
-	 * The extension for BoogiePL files.
-	 */	
-	static public final String BOOGIEPL_EXTENSION = ".bpl";
+    /**
+     * A string to indicate a point in a string template where the
+     * data to instantiate the template should be substituted.
+     */
+    public final static String SUBSTITUTE = "{1}";
 
-	
-	/**
-	 * This method replaces the last occurrence of the <code>oldSuffix</code>
-	 * with the <code>newSuffix</code> in <code>string</code>. It serves to 
-	 * exchange the file sufficies. In case <code>oldSuffix</code> does not
-	 * occur in <code>string</code> it returns <code>string</code>.
-	 * 
-	 * @param string string to replace the suffix from
-	 * @param oldSuffix the suffix to replace
-	 * @param newSuffix the new suffix
-	 * @return the string with replaced suffix
-	 */
-	public static String replaceLast(String string, String oldSuffix, 
-			                         String newSuffix) {
-		if (string.endsWith(oldSuffix)) {
-			// Return string with new suffix
-			return string.substring(0, string.lastIndexOf(oldSuffix)).
-			                                  concat(newSuffix);
-		} else {
-			// Given suffix does not occur
-			return string;
-		}
-	}
-	
-	/**
-	 * This is a convenience method to obtain the classpath
-	 * separator relevant to the current operating system. 
-	 *  
-	 * @return the string that separates the classpath entries 
-	 */
-	public static String getClassPathSeparator() {
-		return System.getProperty("path.separator");
-	}
+    
+    /* *********************************************************************
+     * FILE EXTENSIONS
+     */
+    
+    /**
+     * The file extension for the Java files.
+     */
+    static public final String JAVA_EXTENSION       = ".java";
+    
+    /**
+     * The file extension for the Java class files.
+     */
+    static public final String CLASS_EXTENSION      = ".class";
+    
+    /**
+     * The file extension for the files with the bytecode representation.
+     */
+    static public final String BYTECODE_EXTENSION   = ".btc";
+    
+    /**
+     * The file extension for the history files.
+     */
+    static public final String BYTECODE_HISTORY_EXTENSION   = ".bt";
+    
+    /**
+     * The extension for BoogiePL files.
+     */    
+    static public final String BOOGIEPL_EXTENSION = ".bpl";
 
-	/**
-	 * This is a convenience method to obtain the separator 
-	 * that separates subsequent direcotry and file entries
-	 * in a path to a resource. This value is relevant to the current 
-	 * operating system. 
-	 *  
-	 * @return the string that separates the file path entries 
-	 */
-	public static String getFileSeparator() {
-		return System.getProperty("file.separator");
-	}
-	
-	/**
-	 * This method strips off all the whitespace characters
-	 * in the given string
-	 * 
-	 * @param the string to strip the whitespace from
-	 * @result the string with the whitespace stripped off
-	 */
-	public static String stripAllWhitespace(String l) {
-		String s;
-		s = "";
-		int ii = 0;
-		int jj = l.length();
-		for (ii = 0; ii < jj; ii++)
-			if (!(Character.isWhitespace(l.charAt(ii)))) {
-				s += l.charAt(ii);
-			}
-		return s;	
-	}
+    
+    /* *********************************************************************
+     * GUI MESSAGES
+     */
 
-	/**
-	 * This method gives the proper classfile file for a given
-	 * Java file.
-	 * 
-	 * XXX Isn't there an eclipse method to do this task?
-	 * 
-	 * @param filel Java source code file for which we try to find the
-	 *              class file
-	 * @param editor in which the .java file is edited
-	 * @return 
-	 * @throws JavaModelException in case the project in which the editor
-	 * 
-	 */
-	public static IFile getClassFileName(IFile filel, 
-			                              CompilationUnitEditor editor) 
-	                                  throws JavaModelException {
-		IProject project = ((FileEditorInput)editor.
-				getEditorInput()).getFile().getProject();
-		IJavaProject jproject = JavaCore.create(project);
-		IPath outputloc = jproject.getOutputLocation();
-		String newloc = outputloc.append(filel.getFullPath().
-				          removeFirstSegments(1)).toPortableString();
-		String fname = replaceLast(newloc, 
-				JAVA_EXTENSION,
-				CLASS_EXTENSION); 
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IFile file = workspace.getRoot().
-		               getFile(Path.fromPortableString(fname));
-		return file;
-	}
+    /**
+     * A string used as a header in the message panes launched in connection
+     * with the Java source code action to disassemble code (class 
+     * {@ref DisasBCEL}).
+     */
+    public final static String DISAS_MESSAGE_TITLE =
+        "Disassemble Bytecode";
 
-	/**
-	 * TODO
-	 */
-	public final static String SUBSTITUTE = "{1}";
-	/**
-	 * TODO
-	 */
-	public final static String DISAS_MESSAGE_TITLE =
-	    "Disassemble Bytecode";
-	/**
-	 * TODO
-	 */
-	public final static String B2BPL_MESSAGE_TITLE =
-	    "Bytecode To BoogiePL";
-	/**
-	 * TODO
-	 * 
-	 */
-	public final static String DISAS_SAVE_BYTECODE_FIRST =
-	    "You must save the Bytecode before you can disassemble it.";
-	/**
-	 * TODO
-	 */
-	public final static String B2BPL_SAVE_BYTECODE_FIRST =
-	    "You must save the Bytecode before you can translate it into BoogiePL.";
-	/**
-	 * TODO
-	 */
-	public final static String INVALID_EXTENSION =
-	    "This is not a \"" + SUBSTITUTE + "\" file.";
-	/**
-	 * TODO
-	 */
-	public final static String BYTECODE_EDITOR_CLASS =
-	    "umbra.BytecodeEditor";
-	/**
-	 * TODO
-	 */
-	public final static String BOOGIEPL_EDITOR_CLASS =
-	    "umbra.BoogiePLEditor";
+    /**
+     * A string used as a header in the message panes launched in connection
+     * with the bytecode action to translate the bytecode to BoogiePL (class 
+     * {@ref BytecodeToBoogiePLAction}).
+     */
+    public final static String B2BPL_MESSAGE_TITLE =
+        "Bytecode To BoogiePL";
+    
+    /**
+     * The message which requires the user to save the bytecode before it
+     * is disassembled.
+     */
+    public final static String DISAS_SAVE_BYTECODE_FIRST =
+        "You must save the bytecode before you can disassemble it.";
+    
+    /**
+     * The message which requires the user to save the bytecode before it is
+     * translated to BoogiePL.
+     */
+    public final static String B2BPL_SAVE_BYTECODE_FIRST =
+        "You must save the bytecode before you can translate it into BoogiePL.";
+    
+    /**
+     * A template message that warns about wrong file type. 
+     */
+    public final static String INVALID_EXTENSION =
+        "This is not a \"" + SUBSTITUTE + "\" file.";
+    
+    
+    /* *********************************************************************
+     * ECLIPSE TEXTUAL IDENTIFIERS
+     */
+    
+    /**
+     * The text editor extension identifier which identifies the Umbra 
+     * bytecode editor.
+     */
+    public final static String BYTECODE_EDITOR_CLASS =
+        "umbra.BytecodeEditor";
+    
+    /**
+     * The text editor extension identifier which identifies the BoogiePL 
+     * editor.
+     */
+    public final static String BOOGIEPL_EDITOR_CLASS =
+        "umbra.BoogiePLEditor";
+        
+    
+    /**
+     * This method replaces the last occurrence of the <code>oldSuffix</code>
+     * with the <code>newSuffix</code> in <code>string</code>. It serves to 
+     * exchange the file sufficies. In case <code>oldSuffix</code> does not
+     * occur in <code>string</code> it returns <code>string</code>.
+     * 
+     * @param string string to replace the suffix from
+     * @param oldSuffix the suffix to replace
+     * @param newSuffix the new suffix
+     * @return the string with replaced suffix
+     */
+    public static String replaceLast(final String string, 
+                                     final String oldSuffix, 
+                                     final String newSuffix) {
+        if (string.endsWith(oldSuffix)) {
+            // Return string with new suffix
+            return string.substring(0, string.lastIndexOf(oldSuffix)).
+                                              concat(newSuffix);
+        } else {
+            // Given suffix does not occur
+            return string;
+        }
+    }
+    
+    /**
+     * This is a convenience method to obtain the classpath
+     * separator relevant to the current operating system. 
+     *  
+     * @return the string that separates the classpath entries 
+     */
+    public static String getClassPathSeparator() {
+        return System.getProperty("path.separator");
+    }
+
+    /**
+     * This is a convenience method to obtain the separator 
+     * that separates subsequent direcotry and file entries
+     * in a path to a resource. This value is relevant to the current 
+     * operating system. 
+     *  
+     * @return the string that separates the file path entries 
+     */
+    public static String getFileSeparator() {
+        return System.getProperty("file.separator");
+    }
+    
+    /**
+     * This method strips off all the whitespace characters
+     * in the given string.
+     * 
+     * @param l the string to strip the whitespace from
+     * @return the string with the whitespace stripped off
+     */
+    public static String stripAllWhitespace(final String l) {
+        String s;
+        s = "";
+        int ii = 0;
+        final int jj = l.length();
+        for (ii = 0; ii < jj; ii++)
+            if (!(Character.isWhitespace(l.charAt(ii)))) {
+                s += l.charAt(ii);
+            }
+        return s;    
+    }
+
+    /**
+     * This method gives the proper classfile file for a given
+     * Java file.
+     * 
+     * XXX Isn't there an eclipse method to do this task?
+     * 
+     * @param filel Java source code file for which we try to find the
+     *              class file
+     * @param editor in which the .java file is edited
+     * @return the IFile for the corresponding .class file
+     * @throws JavaModelException in case the project in which the editor
+     * 
+     */
+    public static IFile getClassFileName(final IFile filel, 
+                                         final CompilationUnitEditor editor) 
+                                throws JavaModelException {
+        final IProject project = ((FileEditorInput)editor.
+                getEditorInput()).getFile().getProject();
+        final IJavaProject jproject = JavaCore.create(project);
+        final IPath outputloc = jproject.getOutputLocation();
+        final String newloc = outputloc.append(filel.getFullPath().
+                          removeFirstSegments(1)).toPortableString();
+        final String fname = replaceLast(newloc, 
+                JAVA_EXTENSION,
+                CLASS_EXTENSION); 
+        final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        final IFile file = workspace.getRoot().
+                       getFile(Path.fromPortableString(fname));
+        return file;
+    }    
 }
--- src/Umbra/source/umbra/UmbraPlugin.java
+++ src/Umbra/source/umbra/UmbraPlugin.java
@@ -1,7 +1,7 @@
 package umbra;
 
-import org.eclipse.ui.plugin.*;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -9,48 +9,58 @@ import org.osgi.framework.BundleContext;
  */
 public class UmbraPlugin extends AbstractUIPlugin {
 
-	/**
-	 * The shared instance of the plugin.
-	 */
-	private static UmbraPlugin plugin;
-	
-	/**
-	 * The constructor which shares the instance.
-	 */
-	public UmbraPlugin() {
-		plugin = this;
-	}
+    /**
+     * The shared instance of the plugin.
+     */
+    private static UmbraPlugin plugin;
+    
+    /**
+     * The constructor which shares the instance.
+     */
+    public UmbraPlugin() {
+        plugin = this;
+    }
 
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+    /**
+     * This method is called upon plug-in activation.
+     * 
+     * @param context the context from which the bundle for this plug-in is
+     * extracted
+     * @throws Exception if this method fails to shut down this plug-in
+     */
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+    }
 
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
+    /**
+     * This method is called when the plug-in is stopped.
+     * 
+     * @param context the context from which the bundle for this plug-in is
+     * extracted
+     * @throws Exception if this method fails to shut down this plug-in
+     */
+    public void stop(final BundleContext context) throws Exception {
+        super.stop(context);
+        plugin = null;
+    }
 
-	/**
-	 * Returns the shared instance.
-	 */
-	public static UmbraPlugin getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance.
+     * 
+     * @return the only instance of the Umbra plugin in the system.
+     */
+    public static UmbraPlugin getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin("umbra", path);
-	}
+    /**
+     * Returns an image descriptor for the image file at the given
+     * plug-in relative path.
+     *
+     * @param path the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(final String path) {
+        return AbstractUIPlugin.imageDescriptorFromPlugin("umbra", path);
+    }
 }
