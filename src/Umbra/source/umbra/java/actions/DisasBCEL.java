3725d22711b9ec4f1e18e443bd9710f2ef020762
alx <alx@c6399e9c-662f-4285-9817-23cccad57800>
2007-06-12 13:08:17 +0000

    + UCD checkstyle applied to DisasBCEL

--- src/Umbra/source/umbra/java/actions/DisasBCEL.java
+++ src/Umbra/source/umbra/java/actions/DisasBCEL.java
@@ -14,9 +14,11 @@ import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
 
@@ -29,7 +31,8 @@ import umbra.editor.Composition;
  * Its execution causes generating new related bytecode file
  * in a new editor window.
  *
- * @author BYTECODE team
+ * @author BYTECODE team (contact alx@mimuw.edu.pl)
+ * @version a-01
  */
 
 public class DisasBCEL implements IEditorActionDelegate {
@@ -38,71 +41,99 @@ public class DisasBCEL implements IEditorActionDelegate {
    * The editor of a Java file for which the bytecode file is
    * generated.
    */
-  private CompilationUnitEditor editor;
+  private CompilationUnitEditor my_editor;
 
   /**
    * Finds JavaClass structure related to the current Java
    * source. Generates new bytecode from it and displays
    * it in a new bytecode editor window.
    *
-   * @param see the IActionDelegate.run(IAction)
+   * @param an_action see the IActionDelegate.run(IAction)
+   * @see IActionDelegate#run(IAction)
    */
-  public void run(IAction action) {
-    IPath active = ((FileEditorInput)editor.
-        getEditorInput()).getFile().getFullPath();
-    if (editor.isSaveOnCloseNeeded()) {
-      MessageDialog.openWarning(editor.getSite().getShell(),
+  public final void run(final IAction an_action) {
+    final IPath active = ((FileEditorInput)my_editor.getEditorInput()).
+                                                     getFile().getFullPath();
+    if (my_editor.isSaveOnCloseNeeded()) {
+      MessageDialog.openWarning(my_editor.getSite().getShell(),
                     UmbraHelper.DISAS_MESSAGE_TITLE,
                     UmbraHelper.DISAS_SAVE_BYTECODE_FIRST);
       return;
     }
-    int lind = active.toOSString().lastIndexOf(UmbraHelper.JAVA_EXTENSION);
-    if (lind == -1) {
-      MessageDialog.openInformation(editor.getSite().getShell(),
+    if (active.toPortableString().
+        lastIndexOf(UmbraHelper.JAVA_EXTENSION) == -1) {
+      MessageDialog.openInformation(my_editor.getSite().getShell(),
                       UmbraHelper.DISAS_MESSAGE_TITLE,
                       UmbraHelper.INVALID_EXTENSION.
                       replaceAll(UmbraHelper.SUBSTITUTE,
                            UmbraHelper.JAVA_EXTENSION));
-    } else {
-      try {
-        IFile filel = ((FileEditorInput)editor.getEditorInput()).
-                             getFile();
-        IFile file = UmbraHelper.getClassFileName(filel, editor);
-        FileEditorInput input = new FileEditorInput(file);
-        IWorkbenchPage page = editor.getEditorSite().getPage();
-        BytecodeEditor bcEditor = (BytecodeEditor)page.openEditor(input,
+      return;
+    }
+    final IFile filel = ((FileEditorInput)my_editor.getEditorInput()).getFile();
+    final IWorkbenchPage page = my_editor.getEditorSite().getPage();
+    try {
+      final IFile file = UmbraHelper.getClassFileName(filel, my_editor);
+      FileEditorInput input = new FileEditorInput(file);
+      final BytecodeEditor bc_editor = (BytecodeEditor)page.openEditor(input,
                            UmbraHelper.BYTECODE_EDITOR_CLASS, true);
-        bcEditor.refreshBytecode(active, null, null);
-        input = new FileEditorInput(file);
-        JavaClass jc = bcEditor.getJavaClass();
-        Composition.startDisas();
-        page.closeEditor(bcEditor, true);
-        bcEditor = (BytecodeEditor)page.openEditor(input,
-                          UmbraHelper.BYTECODE_EDITOR_CLASS, true);
-        page.bringToTop(bcEditor);
-        bcEditor.setRelation((AbstractDecoratedTextEditor)editor, jc);
-        Composition.stopDisas();
-      } catch (CoreException e) {
-        e.printStackTrace();
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+      bc_editor.refreshBytecode(active, null, null);
+      input = new FileEditorInput(file);
+      final JavaClass jc = bc_editor.getJavaClass();
+      page.closeEditor(bc_editor, true);
+      openEditorAndDisassemble(page, input, jc);
+    } catch (CoreException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
   /**
-   * Currently, does nothing.
+   * This method opens previously closed editor so that the content of the
+   * editor is coloured with the current colouring style.
+   *
+   * @param a_page a workbench page in which the new editor is to be reopend
+   * @param an_input an input wchich will be presented in the editor
+   * @param a_jclass a BCEL Java class structure to be associated with the
+   *                 editor
+   * @throws PartInitException if the editor could not be created or initialized
    */
-  public void selectionChanged(IAction action, ISelection selection) {
+  private void openEditorAndDisassemble(final IWorkbenchPage a_page,
+                                        final FileEditorInput an_input,
+                                        final JavaClass a_jclass)
+    throws PartInitException {
+    Composition.startDisas();
+    final BytecodeEditor a_beditor = (BytecodeEditor)a_page.openEditor(an_input,
+                        UmbraHelper.BYTECODE_EDITOR_CLASS, true);
+    a_page.bringToTop(a_beditor);
+    a_beditor.setRelation((AbstractDecoratedTextEditor)my_editor, a_jclass);
+    Composition.stopDisas();
+  }
+
+  /**
+   * Currently, does nothing.
+   *
+   * @param an_action see {@ref IActionDelegate#selectionChanged(IAction,
+   *                   ISelection)}
+   * @param a_selection see {@ref IActionDelegate#selectionChanged(IAction,
+   *                       ISelection)}
+   */
+  public void selectionChanged(final IAction an_action,
+                               final ISelection a_selection) {
   }
 
   /**
    * It sets the editor with the Java source code.
+   *
+   * @param an_action see {@ref IEditorActionDelegate#setActiveEditor(IAction,
+   *                    IEditorPart)}
+   * @param a_target_editor the new editor to be active for the action
    */
-  public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-    editor = (CompilationUnitEditor)targetEditor;
+  public final void setActiveEditor(final IAction an_action,
+                                    final IEditorPart a_target_editor) {
+    my_editor = (CompilationUnitEditor)a_target_editor;
   }
 
 }
