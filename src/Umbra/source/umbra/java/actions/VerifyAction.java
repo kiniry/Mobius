bccc1f7be74e856f2b66f39502f373ba2602048d
alx <alx@c6399e9c-662f-4285-9817-23cccad57800>
2007-06-13 13:04:15 +0000

    + VerifyAction updated to conform to UCD Checkstyle

--- src/Umbra/source/umbra/java/actions/VerifyAction.java
+++ src/Umbra/source/umbra/java/actions/VerifyAction.java
@@ -8,21 +8,35 @@ import org.eclipse.ui.IEditorPart;
 /**
  * The action is used to convert Java Bytecode into BoogiePL.
  *
- * @author Samuel Willimann
+ * @author Samuel Willimann (contact through h@inf.ethz.ch)
+ * @version a-01
  *
  */
 public class VerifyAction implements IEditorActionDelegate {
 
   /**
-   * TODO
+   * The method sets the internal Java source code editor attribute.
+   * Currently it does nothing.
+   *
+   * TODO probably the content should be filled in.
+   *
+   * @param an_action the action which triggered the change of the editor
+   * @param a_java_editor the new Java source code editor to be associated with
+   * the action
    */
-  public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+  public void setActiveEditor(final IAction an_action,
+                              final IEditorPart a_java_editor) {
   }
 
   /**
-   * TODO
+   * This method handles the action of the verification action.
+   * Currently it does nothing.
+   *
+   * TODO probably the content should be filled in.
+   *
+   * @param an_action the action that triggered the operation
    */
-  public void run(IAction action) {
+  public final void run(final IAction an_action) {
 
     // TODO convert Bytecode to BoogiePL
 
@@ -33,10 +47,16 @@ public class VerifyAction implements IEditorActionDelegate {
   }
 
   /**
-   * TODO
+   * Currently, does nothing.
+   *
+   * TODO probably the content should be filled in.
+   *
+   * @param an_action see {@ref IActionDelegate#selectionChanged(IAction,
+   *                       ISelection)}
+   * @param a_selection see {@ref IActionDelegate#selectionChanged(IAction,
+   *                       ISelection)}
    */
-  public void selectionChanged(IAction action, ISelection selection) {
-    // TODO Auto-generated method stub
-
+  public void selectionChanged(final IAction an_action,
+                               final ISelection a_selection) {
   }
 }
