9c0b0e692f73b7ec0ac8336d53038d928c03b779
leavens <leavens@c6399e9c-662f-4285-9817-23cccad57800>
2007-06-25 22:41:12 +0000

    More work on this.  Adding an exercise to specify Bag just before the first demo of the tools.

---
+++ src/escjava/trunk/ESCTools/slides/CAV2007Tutorial/examples/BagOfInt.java
@@ -0,0 +1,36 @@
+//@ refines "BagOfInt.jml";
+class BagOfInt {
+
+  private /*@ spec_public @*/ int[] a;
+  private /*@ spec_public @*/ int n;
+
+  public BagOfInt(int[] input) {
+    n = input.length;
+    a = new int[n];
+    System.arraycopy(input, 0, a, 0, n);
+  }
+
+  public /*@ pure @*/ int occurrences(int i) {
+    int count = 0;
+    for (int j = 0; j < n; j++) {
+      if (a[j] == i) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public int extractMin() {
+    int m = Integer.MAX_VALUE;
+    int mindex = 0;
+    for (int i = 1; i <= n; i++) {
+      if (a[i] < m) {
+        mindex = i;
+        m = a[i];
+      }
+    }
+    n--;
+    a[mindex] = a[n];
+    return m;
+  }
+}
