077a2a4742f09b95f90e066ef162f8c8eb57d110
leavens <leavens@c6399e9c-662f-4285-9817-23cccad57800>
2007-06-25 06:06:10 +0000

    More work on this

---
+++ src/escjava/trunk/ESCTools/slides/CAV2007Tutorial/examples/UnboundedStack.java
@@ -0,0 +1,31 @@
+public class BoundedStack {
+
+  /*@ public model JMLObjectSequence theStack;
+    @ public initially theStack != null
+    @                  && theStack.isEmpty();
+    @*/
+
+  //@ public invariant theStack != null;
+
+  /*@ public normal_behavior
+    @   requires !theStack.isEmpty();
+    @   assignable theStack;
+    @   ensures theStack.equals(
+    @              \old(theStack.trailer()));
+    @*/
+  public abstract void pop( );
+
+  /*@ public normal_behavior
+    @   assignable theStack;
+    @   ensures theStack.equals(
+    @              \old(theStack.insertFront(x)));
+    @*/
+  public abstract void push(Object x);
+
+  /*@ public normal_behavior
+    @   requires !theStack.isEmpty();
+    @   assignable \nothing;
+    @   ensures \result == theStack.first();
+    @*/
+  public /*@ pure @*/ abstract Object top( );
+}
