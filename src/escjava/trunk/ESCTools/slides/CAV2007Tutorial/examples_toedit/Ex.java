23413a45a6762fbb5df5af5145ce63eb1d455d91
leavens <leavens@c6399e9c-662f-4285-9817-23cccad57800>
2007-07-02 11:03:33 +0000

    Slight improvements

--- src/escjava/trunk/ESCTools/slides/CAV2007Tutorial/examples_toedit/Ex.java
+++ src/escjava/trunk/ESCTools/slides/CAV2007Tutorial/examples_toedit/Ex.java
@@ -1,7 +1,7 @@
 public class Ex {
-  public void m(Object o) {
-    if (!(o instanceof String)) {
-      throw new ClassCastException();
+    public void m(Object o) {
+        if (!(o instanceof String)) {
+            throw new ClassCastException();
+        }
     }
-  }
 }
