63979fcaa738f2aade2aee547844342bd7f04ec8
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2007-07-05 09:49:25 +0000

    added the necessary comments

--- src/bico/src/mobius/bico/visitors/ArrayInstructionVisitor.java
+++ src/bico/src/mobius/bico/visitors/ArrayInstructionVisitor.java
@@ -21,10 +21,13 @@ import org.apache.bcel.generic.LASTORE;
 import org.apache.bcel.generic.SALOAD;
 import org.apache.bcel.generic.SASTORE;
 
-public class ArrayInstructionVisitor extends EmptyVisitor {
+/**
+ * Translate the Array instructions to bicolano friendly versions.
+ * @author J. Charles (julien.charles@inria.fr)
+ */
+public final class ArrayInstructionVisitor extends EmptyVisitor {
   /** the result string. */
   private String fRes;
-  
 
   /**
    * Does nothing.
@@ -33,82 +36,151 @@ public class ArrayInstructionVisitor extends EmptyVisitor {
     fRes = "";
   }
   
-  
+  /**
+   * Set the result to <code>Vaload Aarray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitAALOAD(final AALOAD obj) {
     fRes = "Vaload Aarray";
   }
   
+  /**
+   * Set the result to <code>Vastore Aarray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitAASTORE(final AASTORE obj) {
     fRes = "Vastore Aarray";
   }
   
+  /**
+   * Set the result to <code>Vaload Barray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitBALOAD(final BALOAD obj) {
     fRes = "Vaload Barray";
   }
   
+  /**
+   * Set the result to <code>Vastore Barray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitBASTORE(final BASTORE obj) {
     fRes = "Vastore Barray";
   }
   
+  
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitCALOAD(final CALOAD obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitCASTORE(final CASTORE obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
+  
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitDALOAD(final DALOAD obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
+  
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitDASTORE(final DASTORE obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
+  
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitFALOAD(final FALOAD obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
+  
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitFASTORE(final FASTORE obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
+  /**
+   * Set the result to <code>Vaload Iarray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitIALOAD(final IALOAD obj) {
     fRes = "Vaload Iarray";
   }
   
+  /**
+   * Set the result to <code>Vastore Iarray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitIASTORE(final IASTORE obj) {
     fRes = "Vastore Iarray";
   }
+  
+
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitLALOAD(final LALOAD obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
+  /**
+   * Unhandled instruction.
+   * @param obj ignored
+   */
   @Override
   public void visitLASTORE(final LASTORE obj) {
     fRes = Util.unhandled("ArrayInstruction", obj);
   }
   
-  
+  /**
+   * Set the result to <code>Vaload Sarray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitSALOAD(final SALOAD obj) {
     fRes = "Vaload Sarray";
   }
   
+  /**
+   * Set the result to <code>Vastore Sarray</code>.
+   * @param obj ignored
+   */
   @Override
   public void visitSASTORE(final SASTORE obj) {
     fRes = "Vastore Sarray";
