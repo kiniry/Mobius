42216c7f5bf3466aa6dfcdec5ba264d39ae1a228
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2007-07-05 09:58:04 +0000

    added the necessary comments

--- src/bico/src/mobius/bico/visitors/BranchInstructionVisitor.java
+++ src/bico/src/mobius/bico/visitors/BranchInstructionVisitor.java
@@ -24,8 +24,11 @@ import org.apache.bcel.generic.IF_ICMPNE;
 import org.apache.bcel.generic.JsrInstruction;
 import org.apache.bcel.generic.Select;
 
-
-public class BranchInstructionVisitor extends EmptyVisitor {
+/**
+ * Translate the Branch instructions to bicolano friendly versions.
+ * @author J. Charles (julien.charles@inria.fr)
+ */
+public final class BranchInstructionVisitor extends EmptyVisitor {
   /** the result string. */
   private String fRes;
 
@@ -36,99 +39,194 @@ public class BranchInstructionVisitor extends EmptyVisitor {
     fRes = "";
   }
   
+  /**
+   * Set the result to <code>Goto index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitGotoInstruction(final GotoInstruction ins) {
     final String index = Util.printZ(ins.getIndex());
     fRes = "Goto " + index;
   }
 
+  
+  /**
+   * Set the result to <code>If0 EqInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFEQ(final IFEQ ins) {
     fRes = "If0 EqInt " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>If0 GeInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFGE(final IFGE ins) {
     fRes = "If0 GeInt " + Util.printZ(ins.getIndex());
   }
 
+  /**
+   * Set the result to <code>If0 GtInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFGT(final IFGT ins) {
     fRes = "If0 GtInt " + Util.printZ(ins.getIndex());
   }
 
+  /**
+   * Set the result to <code>If0 LeInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFLE(final IFLE ins) {
     fRes = "If0 LeInt " + Util.printZ(ins.getIndex());
   }
 
+  /**
+   * Set the result to <code>If0 LtInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFLT(final IFLT ins) {
     fRes = "If0 LtInt " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>If0 NeInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFNE(final IFNE ins) {
     fRes = "If0 NeInt " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>Ifnull NeRef index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFNONNULL(final IFNONNULL ins) {
     fRes = "Ifnull NeRef " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>Ifnull EqRef index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIFNULL(final IFNULL ins) {
     fRes = "Ifnull EqRef " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>Ifacmp EqRef index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ACMPEQ(final IF_ACMPEQ ins) {
     fRes = "If_acmp EqRef " + Util.printZ(ins.getIndex());
 
   }
   
+  /**
+   * Set the result to <code>Ifacmp NeRef index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ACMPNE(final IF_ACMPNE ins) {
     fRes = "If_acmp NeRef " + Util.printZ(ins.getIndex());
   }
 
+  
+  /**
+   * Set the result to <code>Ificmp EqInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ICMPEQ(final IF_ICMPEQ ins) {
     fRes = "If_icmp EqInt " + Util.printZ(ins.getIndex());
   }
-
+  
+  /**
+   * Set the result to <code>Ificmp GeInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ICMPGE(final IF_ICMPGE ins) {
     fRes = "If_icmp GeInt " + Util.printZ(ins.getIndex());
   }
   
+  
+  /**
+   * Set the result to <code>Ificmp GtInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ICMPGT(final IF_ICMPGT ins) {
     fRes = "If_icmp GtInt " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>Ificmp LeInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ICMPLE(final IF_ICMPLE ins) {
     fRes = "If_icmp LeInt " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>Ificmp LtInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ICMPLT(final IF_ICMPLT ins) {
     fRes = "If_icmp LtInt " + Util.printZ(ins.getIndex());
   }
   
+  /**
+   * Set the result to <code>Ificmp NeInt index</code>.
+   * @param ins the instruction from where to get the index
+   * of the jump
+   */
   @Override
   public void visitIF_ICMPNE(final IF_ICMPNE ins) {
     fRes = "If_icmp NeInt " + Util.printZ(ins.getIndex());
   }
-
-
+  
+  /**
+   * Unhandled instruction.
+   * @param ins ignored
+   */
   @Override
   public void visitJsrInstruction(final JsrInstruction ins) {
     fRes = Util.unhandled("JsrInstruction", ins);
   }
   
+  /**
+   * Unhandled instruction.
+   * @param ins ignored
+   */
   @Override
   public void visitSelect(final Select ins) {
     fRes = Util.unimplemented("Select", ins);
