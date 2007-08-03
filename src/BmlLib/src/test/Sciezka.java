619eb33862fcba7dafe48e79052ca3cd7241f70d
chrzaszc <chrzaszc@c6399e9c-662f-4285-9817-23cccad57800>
2007-08-03 09:43:36 +0000

    + Added class for local path management

---
+++ src/BmlLib/src/test/Sciezka.java
@@ -0,0 +1,6 @@
+package test;
+
+public class Sciezka {
+	//public static String path = "/home/chrzaszcz/Mobius/Testy/BmlLib/src/";
+	//public static String path = "D:\\dane\\workspace\\BmlLib\\src\\";
+}
