dde8c2e32e1deb35b18af6a60265df2cf687c596
claudia <claudia@c6399e9c-662f-4285-9817-23cccad57800>
2007-09-06 16:00:48 +0000

    cbr: inv new with heap buildInv(SMap map, SValue val, SAny type)

--- src/escjava/trunk/ESCTools/Escjava/java/escjava/sortedProver/auflia/AufliaNodeBuilder.java
+++ src/escjava/trunk/ESCTools/Escjava/java/escjava/sortedProver/auflia/AufliaNodeBuilder.java
@@ -567,7 +567,7 @@ public class AufliaNodeBuilder extends EscNodeBuilder
 	public SPred buildAssignCompat(SMap map, SValue val, SAny type) {
 		throw new UnsupportedOperationException();
 	}
-  public SPred buildInv(SValue val, SAny type) {
+  public SPred buildInv(SMap map, SValue val, SAny type) {
     throw new UnsupportedOperationException();
   }
   public SPred buildIsAlive(SMap map, SRef ref) {
