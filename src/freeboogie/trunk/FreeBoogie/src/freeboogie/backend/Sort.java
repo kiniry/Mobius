a9d34dd68a601505727dd3912af4931c1bd869fc
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2007-10-10 14:44:15 +0000

    a skeleton of the prover backend

--- src/freeboogie/trunk/FreeBoogie/src/freeboogie/backend/Sort.java
+++ src/freeboogie/trunk/FreeBoogie/src/freeboogie/backend/Sort.java
@@ -9,5 +9,33 @@ package freeboogie.backend;
  * @author reviewed by TODO
  */
 public enum Sort {
+  /** supertype for all non-predicates */ ANY(null),
+  /** a predicate */ PRED(null),
+  /** a value */ VALUE(ANY),
+  /** a boolean value (NOT a predicate) */ BOOL(VALUE),
+  /** an integer */ INT(VALUE),
+  /** a real number */ REAL(VALUE),
+  /** a reference */ REF(VALUE);
+  
+  /** The supersort of {@code this}. */
+  public final Sort superSort;
 
+  /**
+   * Constructs a sort, with a given super sort.
+   * @param superSort the super sort
+   */
+  Sort(Sort superSort) {
+    this.superSort = superSort;
+  }
+  
+  /**
+   * Tests whether {@code this} is a subsort of {@code other}.
+   * @param other the potential supersort
+   * @return whether {@code this <: other}
+   */
+  public boolean isSubsortOf(Sort other) {
+    if (this == other) return true;
+    if (superSort == null) return false;
+    return superSort.isSubsortOf(other);
+  }
 }
