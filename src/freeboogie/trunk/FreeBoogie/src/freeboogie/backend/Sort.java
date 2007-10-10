7d82e80c5c4c1a7342b5ed5f56fcece22794ac81
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2007-10-10 10:28:02 +0000

    started backend

---
+++ src/freeboogie/trunk/FreeBoogie/src/freeboogie/backend/Sort.java
@@ -0,0 +1,13 @@
+package freeboogie.backend;
+
+/**
+ * The sorts supported by the backend. There is not much 
+ * variation between the sorts supported by different provers
+ * so we stick to a conservative set of sorts.
+ *
+ * @author rgrig 
+ * @author reviewed by TODO
+ */
+public enum Sort {
+
+}
