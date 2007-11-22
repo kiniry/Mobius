7590cb468ee74c52511b4186540804aa3ea6bc9e
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2007-11-22 14:45:22 +0000

    added javadoc

--- src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/coq/UnsupportedNodeBuilder.java
+++ src/escjava/trunk/MobiusDirectVCGen/src/mobius/directVCGen/formula/coq/UnsupportedNodeBuilder.java
@@ -2,51 +2,100 @@ package mobius.directVCGen.formula.coq;
 
 import escjava.sortedProver.EscNodeBuilder;
 
+/**
+ * A node builder that contains all the unsupported operation
+ * of the Coq node builder.
+ * @author J. Charles (julien.charles@inria.fr)
+ */
 public abstract class UnsupportedNodeBuilder extends EscNodeBuilder  {
 
+  /**
+   * Unsupported operation.
+   * @param arg1 ignored
+   * @param arg2 ignored
+   * @return throws an exception
+   */
   @Override
   public SPred buildXor(final SPred arg1, final SPred arg2) {
     throw new UnsupportedOperationException();
   }
 
+  /**
+   * Unsupported operation.
+   * @param f ignored
+   * @return throws an exception
+   */
   @Override
   public SReal buildReal(final double f) {
     throw new UnsupportedOperationException("Translation of reals is not supported yet!");
   }
 
-
+  /**
+   * Unsupported operation.
+   * @param tag ignored
+   * @param arg1 ignored
+   * @param arg2 ignored
+   * @return throws an exception
+   */
   @Override
-  public SBool buildRealBoolFun(final int realPredTag, final SReal arg1, final SReal arg2) {
+  public SBool buildRealBoolFun(final int tag, final SReal arg1, final SReal arg2) {
     throw new UnsupportedOperationException("Translation of reals is not supported yet!");
   }
 
-
+  /**
+   * Unsupported operation.
+   * @param tag ignored
+   * @param arg1 ignored
+   * @param arg2 ignored
+   * @return throws an exception
+   */
   @Override
-  public SReal buildRealFun(final int realFunTag, final SReal arg1, final SReal arg2) {
+  public SReal buildRealFun(final int tag, final SReal arg1, final SReal arg2) {
     throw new UnsupportedOperationException("Translation of reals is not supported yet!");
   }
 
-
+  /**
+   * Unsupported operation.
+   * @param tag ignored
+   * @param arg ignored
+   * @return throws an exception
+   */
   @Override
-  public SReal buildRealFun(final int realFunTag, final SReal arg1) {
+  public SReal buildRealFun(final int tag, final SReal arg) {
     throw new UnsupportedOperationException("Translation of reals is not supported yet!");
   }
 
- 
+  /**
+   * Unsupported operation.
+   * @param tag ignored
+   * @param arg1 ignored
+   * @param arg2 ignored
+   * @return throws an exception
+   */
   @Override
-  public SPred buildRealPred(final int realPredTag, final SReal arg1, final SReal arg2) {
+  public SPred buildRealPred(final int tag, final SReal arg1, final SReal arg2) {
     throw new UnsupportedOperationException("Translation of reals is not supported yet!");
   }
 
+  /**
+   * Unsupported operation.
+   * @param c ignored
+   * @return throws an exception
+   */
   @Override
   public SAny buildConstantRef(final FnSymbol c) {
     throw new UnsupportedOperationException();
   }
   
 
-
+  /**
+   * Unsupported operation.
+   * @param tag ignored
+   * @param arg ignored
+   * @return throws an exception
+   */
   @Override
-  public SInt buildIntFun(final int intFunTag, final SInt arg1) {
+  public SInt buildIntFun(final int tag, final SInt arg) {
     throw new UnsupportedOperationException();
   }
 }
