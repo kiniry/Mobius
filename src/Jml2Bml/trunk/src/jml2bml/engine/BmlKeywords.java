1bb00e4d8c26cdafe62104219b3464b19beaab1d
fulara <fulara@c6399e9c-662f-4285-9817-23cccad57800>
2008-01-06 15:04:49 +0000

    some checkstyle bugfixes & refactoring

--- src/Jml2Bml/trunk/src/jml2bml/engine/BmlKeywords.java
+++
@@ -1,6 +0,0 @@
-package jml2bml.engine;
-
-public class BmlKeywords {
-	public final static String NON_NULL = "non_null";
-	public final static String ASSERT = "assert";
-}
---
+++ src/Jml2Bml/trunk/src/jml2bml/engine/KeywordTranslator.java
@@ -0,0 +1,7 @@
+package jml2bml.engine;
+
+public class KeywordTranslator {
+  public static String translate(String jmlKeyword){
+    return "==>";
+  }
+}
