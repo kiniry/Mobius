47e2900c122e488b0f5e5bfd0942768cab396383
fintanfairmichael <fintanfairmichael@c6399e9c-662f-4285-9817-23cccad57800>
2008-02-17 22:23:52 +0000

    Minor changes.

--- src/bon/bonc/src/ie/ucd/bon/parser/ParseProblemExplanations.java
+++ src/bon/bonc/src/ie/ucd/bon/parser/ParseProblemExplanations.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2007, Fintan Fairmichael, University College Dublin under the BSD licence.
+ * See LICENCE.TXT for details.
+ */
 package ie.ucd.bon.parser;
 
 import ie.ucd.bon.Main;
