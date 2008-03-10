cdf05acce477943ca7198360a6c65ebe7977532f
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2008-03-10 15:48:31 +0000

    id util

---
+++ src/freeboogie/trunk/FreeBoogie/src/freeboogie/util/Id.java
@@ -0,0 +1,16 @@
+package freeboogie.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Id {
+  static private Map<String, Integer> counter =
+    new HashMap<String, Integer>();
+
+  static private String get(String categ) {
+    Integer x = counter.get(categ);
+    if (x == null) x = -1;
+    counter.put(categ, ++x);
+    return "tmp_" + categ + "_" + x;
+  }
+}
