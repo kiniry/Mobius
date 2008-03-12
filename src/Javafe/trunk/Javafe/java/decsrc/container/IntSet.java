560dd3c986eb70e41d870b9e015958df1c1312c9
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-03-12 13:31:50 +0000

    Rename variables called 'peer' to avoid confusion with 'peer' keyword

--- src/Javafe/trunk/Javafe/java/decsrc/container/IntSet.java
+++ src/Javafe/trunk/Javafe/java/decsrc/container/IntSet.java
@@ -80,12 +80,12 @@ public abstract class IntSet {
 
     /** post(this) = union(pre(this), pre(peer)).
 	Returns true iff post(this) != pre(this). */
-    public boolean union(IntSet peer) {
+    public boolean union(IntSet intSetPeer) {
 	boolean changed = false;
-	if (this != peer) {
+	if (this != intSetPeer) {
 	    int iter = -1;
-	    while ((iter = peer.next(iter)) >= 0) {
-		changed |= insert(peer.get(iter));
+	    while ((iter = intSetPeer.next(iter)) >= 0) {
+		changed |= insert(intSetPeer.get(iter));
 	    }
 	}
 	return changed;
