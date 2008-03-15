f4a83aa9362de37431c4927c17a4cceac726e60e
fulara <fulara@c6399e9c-662f-4285-9817-23cccad57800>
2008-03-15 21:06:09 +0000

    The List example

---
+++ src/Jml2Bml/trunk/src/main/List.java
@@ -0,0 +1,26 @@
+package main;
+public  class List {
+
+         private Object[] list;
+
+         /*@  requires list != null;
+               @ ensures \result ==(\exists int i;
+           @ 0 <= i && i < list.length &&
+           @ \old(list[i]) == obj1 && list[i] == obj2);
+           @*/
+         public boolean replace(Object obj1,Object obj2) {
+           /*@
+             @ loop_invariant i <= list.length && i >=0 && (\forall int k;0 <= k && k < i ==>
+             @     list[k] != obj1);
+             @*/
+           for (int i = 5;
+                i < list.length; 
+                i++ ){
+             if ( list[i] == obj1) {
+               list[i] = obj2;
+               return true;
+             }
+           }
+           return false;
+         }
+}
