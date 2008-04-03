b4efac55bd752fc13e1ffd8115988ca0f63ae3f4
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-03 12:47:23 +0000

    #543: Updated POM for ESC/Java2

--- src/escjava/trunk/ESCTools/maven/sampleScript.sh
+++ src/escjava/trunk/ESCTools/maven/sampleScript.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/dcochran/apache-maven-2.0.8
+export M2_HOME=/usr/local/apache-maven/apache-maven-2.0.8
 export M2=$M2_HOME/bin
 export MAVEN_OPTS=-Xms256m
-export JAVA_HOME=/usr
+export JAVA_HOME=/usr/local/java
 export PATH=$M2:$JAVA_HOME/bin:$PATH
 mvn --version 
 mvn $*
