ff2e7535d6a86af310871be39a05f087738c6e1b
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-10 14:34:57 +0000

    #543: Move list of servers into a configuration file

--- src/escjava/trunk/ESCTools/src/test/java/DistributedTest.java
+++ src/escjava/trunk/ESCTools/src/test/java/DistributedTest.java
@@ -1,41 +1,46 @@
+import java.util.Iterator;
+
 import junit.framework.*;
 import junitutils.*;
 
 public class DistributedTest extends junitutils.TestFilesTestSuite {
 
-    static public junit.framework.Test suite() {
+	static public junit.framework.Test suite() {
 
 	String hostName = "";
-try
-{
-	java.net.InetAddress localMachine =
-java.net.InetAddress.getLocalHost();	
-	hostName = localMachine.getHostName();
-}
-catch(java.net.UnknownHostException uhe)
-{
-	uhe.getStackTrace();
-	hostName = "localhost";
-}    
-    	String defaultSettings = "-nowarn Deadlock -verboseTrace -testMode -Loop5";
-    	String listOfFiles = "src/test/list.txt";
-    	String testSuiteName = "Distributed-Test-" + hostName;
-
-	String[] serverNames = {"arrow.ucd.ie","object.ucd.ie"};
-	int numberOfServers = serverNames.length;
-	int serverIndex = -1; // run no tests unless a match is found
-
-	// Match server name and take every nth test
-	for (int i=0; i < serverNames.length; i++)
+	try
 	{
-		if (hostName.equals(serverNames[i])) {
-			System.out.println("Test Server: " + hostName);
-			serverIndex = i;
-			break;
-	
-		}
+		java.net.InetAddress localMachine =
+			java.net.InetAddress.getLocalHost();	
+		hostName = localMachine.getHostName();
 	}
+	catch(java.net.UnknownHostException uhe)
+	{
+		throw new RuntimeException(uhe.toString());
+	}    
 	
+	String defaultSettings = "-nowarn Deadlock -verboseTrace -testMode -Loop5";
+	String listOfFiles = "src/test/list.txt";
+	String listOfServers = "src/test/servers.txt";
+	String testSuiteName = "Distributed-Test-" + hostName;
+	int numberOfServers = 0;
+	int serverIndex = -1; // run no tests unless a server is found 
+    	
+    	try { 
+    	    Iterator i = new LineIterator(listOfServers);
+    	    while (i.hasNext()) {
+    	    	
+    	    	String s = (String)i.next();
+    	    	if (s.equalsIgnoreCase(hostName)) {
+    	    		serverIndex = numberOfServers;
+    	    	}
+    	    	numberOfServers++;
+    	    }
+    	} catch (java.io.IOException e) {
+    	    throw new RuntimeException(e.toString());
+    	}
+
+	 
 
         junit.framework.TestSuite suite =
 	    new AbstractDistributedTestSuite(
