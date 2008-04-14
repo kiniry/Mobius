a5ed79e80f20a32a270a11bb1de4558393dd14e4
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-14 11:31:03 +0000

    #543: Adding distribution script

---
+++ src/escjava/trunk/ESCTools/mvn.sh
@@ -0,0 +1,7 @@
+# Distribution script for Maven builds
+
+ssh dcochran@arrow.ucd.ie mvn $*
+ssh dcochran@object.ucd.ie mvn $*
+ssh dcochran@morphism.ucd.ie mvn $*
+ssh dcochran@category.ucd.ie mvn $*
+ssh dcochran@voting.ucd.ie mvn $*
