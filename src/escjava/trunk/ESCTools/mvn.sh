34f9dd08933bf94993c2b407d51caf069bb05dbf
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-15 12:15:27 +0000

    #543: Remove local shell script for running Maven

--- src/escjava/trunk/ESCTools/mvn.sh
+++
@@ -1,9 +0,0 @@
-# Distribution script for Maven builds
-
-echo $USER $HOSTNAME $PWD
-export MAVEN="cd ESCJava2; ./maven.local"
-export REMOTE_TEST="sudo -u dcochran ssh -f"
-echo $REMOTE_TEST server $MAVEN
-$REMOTE_TEST dcochran@arrow.ucd.ie $MAVEN $* 
-$REMOTE_TEST dcochran@object.ucd.ie $MAVEN $* 
-$REMOTE_TEST dermotcochran@voting.ucd.ie $MAVEN $* 
