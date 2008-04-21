bb8f2ad405d447047d98a4b26d34ef8db74cbe11
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-21 12:18:06 +0000

    #543: Resolve mvn pathname for arrow and object

---
+++ src/test/run_local.sh
@@ -0,0 +1,8 @@
+# move to working directory, get latest and setup for maven tests
+export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/maven/apache-maven-2.0.8
+echo $HOSTNAME
+cd ESCTools
+svn cleanup
+svn update
+make pre-maven
+$M2_HOME/bin/mvn $*
