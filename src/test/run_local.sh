1c2d24eb950b74cf364f66b5b3057cea7f532db2
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-22 11:23:39 +0000

    #543: Generate Makefile.local as required

--- src/test/run_local.sh
+++ src/test/run_local.sh
@@ -2,6 +2,7 @@
 export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/maven/apache-maven-2.0.8
 echo $HOSTNAME
 cd $HOSTNAME/ESCTools
+echo "export ESCTOOLS_ROOT=" $PWD > Makefile.local
 svn cleanup
 svn update
 make pre-maven
