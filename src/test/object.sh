a6db06a5aa3941cb44afb62182572f30fa68e5e9
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-22 16:36:19 +0000

    #543: Set JAVA_HOME on Object

--- src/test/object.sh
+++ src/test/object.sh
@@ -3,7 +3,7 @@
 # move to working directory, get latest and setup for maven tests
 export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/maven/apache-maven-2.0.8
 export MAVEN_OPTS="-Xmx1740m -Xms512m"
-export JAVA_HOME=/usr
+export JAVA_HOME=/usr/local/java/jdk1.5.0_13/
 echo $HOSTNAME
 cd object/ESCTools
 echo "export ESCTOOLS_ROOT="$PWD > Makefile.local
