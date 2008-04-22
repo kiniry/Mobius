6162682d24117b554e729216d8a9470d7804ed1a
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-22 10:54:42 +0000

    #543: Restored file header from original file and updated copyright notice.

--- src/escjava/trunk/ESCTools/src/test/java/AbstractDistributedTestSuite.java
+++ src/escjava/trunk/ESCTools/src/test/java/AbstractDistributedTestSuite.java
@@ -1,16 +1,25 @@
 /*
- * Copyright (C) 2000-2001 Iowa State University This file is part of mjc, the
- * MultiJava Compiler. This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version. This program is distributed in the
- * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
- * the GNU General Public License for more details. You should have received a
- * copy of the GNU General Public License along with this program; if not, write
- * to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA $Id: TestFilesTestSuite.java 71450 2008-04-10 10:31:38Z
- * dcochran $ Author: David R. Cok
+ * Copyright (C) 2000-2001 Iowa State University
+ * Modified 2008 Systems Research Group, University College Dublin
+ *
+ * This file is part of mjc, the MultiJava Compiler.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Id: TestFilesTestSuite.java 71450 2008-04-10 10:31:38Z dcochran $
+ * Author: David R. Cok
  */
 package junitutils;
 
