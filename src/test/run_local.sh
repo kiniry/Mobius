de775a91066fd6ba6e818c67d05642619510ee8c
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-22 11:09:12 +0000

    #543: Arrow and Object servers need to maintain their own build files idependantly of each other.  This is done by using a sub-folder based on $HOSTNAME.

--- src/test/run_local.sh
+++ src/test/run_local.sh
@@ -1,7 +1,7 @@
 # move to working directory, get latest and setup for maven tests
 export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/maven/apache-maven-2.0.8
 echo $HOSTNAME
-cd ESCTools
+cd $HOSTNAME/ESCTools
 svn cleanup
 svn update
 make pre-maven
