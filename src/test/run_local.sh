59605af9f0be40b9883fc5ff97e6282c6dc60181
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-22 08:31:17 +0000

    #543: Don't override default memory settings for now

--- src/test/run_local.sh
+++ src/test/run_local.sh
@@ -1,6 +1,5 @@
 # move to working directory, get latest and setup for maven tests
 export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/maven/apache-maven-2.0.8
-export MAVEN_OPTS=-Xms512m -Xmx1024m
 echo $HOSTNAME
 cd ESCTools
 svn cleanup
--- src/test/run_test.sh
+++ src/test/run_test.sh
@@ -1,6 +1,5 @@
 # move to working directory, get latest and setup for maven tests
 echo $HOSTNAME
-export MAVEN_OPTS=-Xms512m -Xmx1024m
 cd ESCTools
 svn cleanup
 svn update
