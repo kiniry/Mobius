4581f872f0c736ffbd117039608f963e909ce17c
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-22 15:52:09 +0000

    #543: Increase heap size

--- src/test/run_test.sh
+++ src/test/run_test.sh
@@ -1,6 +1,6 @@
 # move to working directory, get latest and setup for maven tests
 echo $HOSTNAME
-export MAVEN_OPTS="-Xmx1024 -Xms512"
+export MAVEN_OPTS="-Xmx1740 -Xms512"
 cd ESCTools
 svn cleanup
 svn update
