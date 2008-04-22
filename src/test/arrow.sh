a590fca94473f5fb32728d63604dc97af128c36e
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-22 15:58:43 +0000

    #543: Update memory settings (use 'm' suffix)

--- src/test/arrow.sh
+++ src/test/arrow.sh
@@ -2,7 +2,7 @@
 #
 # move to working directory, get latest and setup for maven tests
 export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/maven/apache-maven-2.0.8
-export MAVEN_OPTS="-Xmx1740 -Xms512"
+export MAVEN_OPTS="-Xmx1740m -Xms512m"
 echo $HOSTNAME
 cd arrow/ESCTools
 echo "export ESCTOOLS_ROOT="$PWD > Makefile.local
--- src/test/run_test.sh
+++ src/test/run_test.sh
@@ -1,6 +1,6 @@
 # move to working directory, get latest and setup for maven tests
 echo $HOSTNAME
-export MAVEN_OPTS="-Xmx1740 -Xms512"
+export MAVEN_OPTS="-Xmx1740m -Xms512m"
 cd ESCTools
 svn cleanup
 svn update
