674c97c5b61a1b1f9c4077ba2088236e5ce3c4e8
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-25 12:20:37 +0000

    #543: Added name of original author

--- src/test/hardlink.sh
+++ src/test/hardlink.sh
@@ -1,10 +1,14 @@
-Run it from the top-level directly that you want to
-copy-by-linking, after setting the CHROOT value to the full pathname of
-the directory you want to create the copy in. This script will only link
-regular files, not symlinks, device files etc., but you should be able
-to modify it to do that fairly easily if you need to do that.
-
 #!/bin/sh
+
+# Author: Paul Howarth
+# http://forums.fedoraforum.org/showthread.php?t=42825 
+ 
+# Run it from the top-level directly that you want to
+# copy-by-linking, after setting the CHROOT value to the full pathname of
+# the directory you want to create the copy in. This script will only link
+# regular files, not symlinks, device files etc., but you should be able
+# to modify it to do that fairly easily if you need to do that.
+
 
 # TEMPLATE directory is root of tree you want to replicate.
 # It should *not* be an absolute pathname.
