734a83d917dde37627bbb482b558e6efd1870921
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-04-28 14:44:37 +0000

    #543: Use Arrow as the primary Maven test server, but also run tests on Voting as the Continuum server

--- src/test/arrow.sh
+++ src/test/arrow.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 #
-# move to local hard link of the working directory
+# Setup hardlinks to the working directory with latest checkout
 export M2_HOME=/Network/Servers/kind.ucd.ie/Volumes/Home/maven/apache-maven-2.0.8
 export MAVEN_OPTS="-Xmx1740m -Xms512m"
 echo $HOSTNAME
-cd arrow/working-directory
+cd arrow/ESCTOOLS
+svn up
+echo "export ESCTOOLS_ROOT="$WORKDIR > Makefile.local
+make pre-maven
+nice ../../test/hardlink.sh ~maven/object/working-directory
+nice ../../test/hardlink.sh ~maven/category/working-directory
 $M2_HOME/bin/mvn $*
