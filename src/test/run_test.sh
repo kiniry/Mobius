4a68ecb8e763827f47f6a75265b50ee47e9084fc
dcochran <dcochran@c6399e9c-662f-4285-9817-23cccad57800>
2008-05-20 15:08:36 +0000

    Most of these scripts are not needed when we are using Hudson instead of Continuum.

--- src/test/run_test.sh
+++
@@ -1,8 +0,0 @@
-# move to working directory, get latest and setup for maven tests
-echo $HOSTNAME
-export MAVEN_OPTS="-Xmx1740m -Xms512m"
-cd ESCTools
-svn cleanup
-svn update
-make pre-maven
-mvn $*
