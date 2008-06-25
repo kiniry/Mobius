c3e51f8ffe11d282b23ef9366052c89d564d31c2
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2008-06-25 14:12:50 +0000

    A new simple test file

---
+++ src/escjava/trunk/MobiusDirectVCGen/samples/Test.java
@@ -0,0 +1,13 @@
+public class Test{
+  public  int k ;
+
+  public Test() {}
+
+  /*@
+    @ ensures (\old(k) + 1 == k)  && (\forall Test o; o != this ==> o.k == \old(o.k));
+    @*/
+  public void inc() {
+    k = k + 1;
+  }
+
+}
