6c48ae05d06db10f00007f5243e2c0f066d102f9
chalin <chalin@c6399e9c-662f-4285-9817-23cccad57800>
2008-07-16 06:13:20 +0000

    Test for added support for -nonNullByDefault for method return types and parameters.

---
+++ src/escjava/trunk/ESCTools/Escjava/test/escjava/test/test20B/A.java
@@ -0,0 +1,33 @@
+public class A extends B {
+	public A() {
+	}
+
+	public Object m_result1() {
+		return null; // warning: NonNullResult
+	}
+
+	public Object m_result2(/*@nullable*/Object o) {
+		return o; // warning: NonNullResult
+	}
+
+	public Object m_result3() {
+		return "";
+	}
+
+	public /*@nullable*/Object m_result4() { // warning: nullable ignored
+		return "";
+	}
+
+	public int m_result_ok() {
+		return 0;
+	}
+
+	public Object m_param_result1(Object o) {
+		return o;
+	}
+
+	public void m_param1(int i) {
+		i++;
+	}
+
+}
