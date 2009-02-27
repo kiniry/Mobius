b89f0cf00f9676860cc3268ec9026641ba30295a
sw237122 <sw237122@c6399e9c-662f-4285-9817-23cccad57800>
2009-02-27 08:09:17 +0000

    added class used as a factory of loggers

---
+++ src/Umbra/trunk/source/umbra/logging/LoggerFactory.java
@@ -0,0 +1,61 @@
+/*
+ * @title       "Umbra"
+ * @description "An editor for the Java bytecode and BML specifications"
+ * @copyright   "(c) 2006-2008 University of Warsaw"
+ * @license     "All rights reserved. This program and the accompanying
+ *               materials are made available under the terms of the LGPL
+ *               licence see LICENCE.txt file"
+ */
+package umbra.logging;
+
+import java.util.logging.Logger;
+
+/**
+ * @version a-01
+ *
+ */
+public class LoggerFactory {
+
+  private enum Mode {DEVELOPMENT, DEBUG, PRODUCTION};
+  
+  private static Mode mode = Mode.DEVELOPMENT;
+  private static LoggerFactoryImpl loggerFactoryImpl = getRightImpl();
+
+  private static LoggerFactoryImpl getRightImpl() {
+    switch (mode) {
+      case DEVELOPMENT: 
+        return new LoggerFactoryDevelopmentImpl();
+      case DEBUG: 
+        return new LoggerFactoryDevelopmentImpl();
+      case PRODUCTION: 
+        return new LoggerFactoryDevelopmentImpl();
+    }
+    return null;
+  }
+  
+  /**
+   * @param newMode
+   */
+  public static void setMode(final Mode newMode) {
+    if (newMode != mode) {
+      mode = newMode;
+      loggerFactoryImpl = getRightImpl();
+    }
+  }
+  
+  /**
+   * @return
+   */
+  public static Mode getMode() {
+    return mode;
+  }
+  
+  /**
+   * @param loggerName
+   * @return
+   */
+  public static Logger getClassLogger(final Class<?> c) {
+    return loggerFactoryImpl.getClassLogger(c);
+  }
+  
+}
