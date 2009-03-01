935ef68994410f5fa73488bdc852e241a2e29cc1
sw237122 <sw237122@c6399e9c-662f-4285-9817-23cccad57800>
2009-03-01 00:53:48 +0000

    concrete verification factory creating set of objects that uses console for i/o.

---
+++ src/Umbra/trunk/source/umbra/verifier/ConsoleVerificationFactory.java
@@ -0,0 +1,33 @@
+/*
+ * @title       "Umbra"
+ * @description "An editor for the Java bytecode and BML specifications"
+ * @copyright   "(c) 2006-2008 University of Warsaw"
+ * @license     "All rights reserved. This program and the accompanying
+ *               materials are made available under the terms of the LGPL
+ *               licence see LICENCE.txt file"
+ */
+package umbra.verifier;
+
+/**
+ * @author Szymon Wrzyszcz (sw237122@students.mimuw.edu.pl)
+ * @version a-01
+ *
+ */
+public class ConsoleVerificationFactory extends VerificationFactory {
+
+  /**
+   * 
+   */
+  @Override
+  public ResultPresenter getResultPresenter(BytecodeVerifier verifier) {
+    return new ConsoleResultPresenter(verifier);
+  }
+
+  /**
+   * 
+   */
+  @Override
+  public SaveConfirmer getSaveConfirmer(ResultPresenter resultPresenter) {
+    return new ConsoleSaveConfirmer(resultPresenter); 
+  }
+}
