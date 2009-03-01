d92cd6f4703fd85709a72d961d6f334f7c7cdd44
sw237122 <sw237122@c6399e9c-662f-4285-9817-23cccad57800>
2009-03-01 00:49:28 +0000

    abstract class used to ask user for save confirmation

---
+++ src/Umbra/trunk/source/umbra/verifier/SaveConfirmer.java
@@ -0,0 +1,33 @@
+/*
+ * @title       "Umbra"
+ * @description "An editor for the Java bytecode and BML specifications"
+ * @copyright   "(c) 2006-2008 University of Warsaw"
+ * @license     "All rights reserved. This program and the accompanying
+ *               materials are made available under the terms of the LGPL
+ *               licence see LICENCE.txt file"
+ */
+package umbra.verifier;
+
+/**
+ * @author Szymon Wrzyszcz (sw237122@students.mimuw.edu.pl)
+ * @version a-01
+ *
+ */
+public abstract class SaveConfirmer {
+
+  protected ResultPresenter resultPresenter;
+  
+  /**
+   * @param resultPresenter
+   */
+  public SaveConfirmer(ResultPresenter resultPresenter) {
+    this.resultPresenter = resultPresenter;
+  }
+  
+  /**
+   * in case there are verification problems asks user if she wants to save anyway
+   * @return true if user wants to save, false otherwise
+   */
+  public abstract boolean confirm();
+  
+}
