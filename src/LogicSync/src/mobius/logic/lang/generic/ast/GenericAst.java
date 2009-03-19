ba4b07c8a888fab384fc69b6bd85b300cf2acc8c
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-03-19 13:34:31 +0000

    preparing for generation

---
+++ src/LogicSync/src/mobius/logic/lang/generic/ast/GenericAst.java
@@ -0,0 +1,32 @@
+package mobius.logic.lang.generic.ast;
+
+import mobius.logic.lang.ast.Ast;
+import mobius.logic.lang.coq.ast.Evaluator;
+
+public abstract class GenericAst extends Ast {
+  private GenericAst next;
+  private GenericAst last;
+  
+  public GenericAst() {
+    next = null;
+    last = this;
+  }
+  
+  /**
+   * Dispatches to {@code e.eval} based on the static type of the node
+   * and the dynamic type of {@code e}.
+   * @param <R> the type of the result computed by the evaluator
+   * @param e the evaluator
+   * @return the result computed by the evaluator
+   */
+  abstract public <R> R eval(Evaluator<R> e);
+  
+  public void add(GenericAst next) {
+    last.next = next;
+    last = next;
+  }
+  public GenericAst getNext() {
+    return next;
+  }  
+
+}
