854e0f08ae3b7cf63b1ebbf26b72b200979c2bc6
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-03-26 11:14:54 +0000

    did a lot of cleanup. Now supports global env/ local env

--- src/freeboogie/trunk/FreeBoogie/src/freeboogie/vcgen/Passificator.java
+++ src/freeboogie/trunk/FreeBoogie/src/freeboogie/vcgen/Passificator.java
@@ -23,6 +23,7 @@ import freeboogie.ast.Implementation;
 import freeboogie.ast.Signature;
 import freeboogie.ast.Transformer;
 import freeboogie.ast.TupleType;
+import freeboogie.ast.Type;
 import freeboogie.ast.VariableDecl;
 import freeboogie.astutil.PrettyPrinter;
 import freeboogie.tc.SimpleGraph;
@@ -36,7 +37,6 @@ import freeboogie.util.Err;
  * Avoiding Exponential Explosion: Generating Compact Verification Conditions
  * C. Flanagan and J. B. Saxe
  * 
- * TODO: fix the global env stuff
  * 
  * @author J. Charles (julien.charles@gmail.com)
  */
@@ -45,7 +45,8 @@ public class Passificator extends Transformer {
   /** the current instance of the type checker. */
   private final TcInterface fTypeChecker;
 
-  private final Environment fGlobalVarsCnt = new Environment();
+  /** the main global environment. */
+  private final Environment fEnv = new Environment();
 
   
   /**
@@ -56,13 +57,26 @@ public class Passificator extends Transformer {
     fTypeChecker = tc;
   }
 
-  
+  /**
+   * Process the AST, and returns the modified version.
+   * @param ast the ast to look at.
+   * @return a valid modified ast
+   */
   public Declaration process(final Declaration ast) {
     Declaration passifiedAst = (Declaration)ast.eval(this);
     passifiedAst = addVariableDeclarations(passifiedAst);
     verifyAst(passifiedAst);
     return fTypeChecker.getAST();
   }
+  
+  @Override
+  public Ast eval(VariableDecl decl, String name, 
+                  Type type, Identifiers typeVars, Declaration tail) {
+    fEnv.addGlobal(decl);
+    Declaration newTail = tail != null?(Declaration)tail.eval(this) : null; 
+    return VariableDecl.mk(name, type, typeVars, newTail);
+  }
+  
   
   /**
    * Treats method by methods.
@@ -77,11 +91,13 @@ public class Passificator extends Transformer {
     }
     else {
 
-      BodyPassifier bp = BodyPassifier.passify(fTypeChecker, currentFG, oldBody, (VariableDecl)sig.getResults());
+      BodyPassifier bp = BodyPassifier.passify(fTypeChecker, currentFG, fEnv, 
+                                               oldBody, (VariableDecl)sig.getResults());
       sig = Signature.mk(sig.getName(), sig.getArgs(),
                          bp.getResult(),
                          sig.getTypeVars(), sig.loc());
       body = bp.getBody();
+      fEnv.updateGlobalWith(bp.getEnvironment());
 
     }
 
@@ -101,15 +117,9 @@ public class Passificator extends Transformer {
    * @return the AST with the added declarations
    */
   private Declaration addVariableDeclarations(Declaration ast) {
-    for (Map.Entry<VariableDecl, Integer> e : fGlobalVarsCnt.entrySet()) {
+    for (Map.Entry<VariableDecl, Integer> e : fEnv.getGlobalSet()) {
       for (int i = 1; i <= e.getValue(); ++i) {
-        Identifiers ntv = e.getKey().getTypeVars();
-        if (ntv != null) ntv = ntv.clone();
-        ast = VariableDecl.mk(
-          e.getKey().getName()+"$$"+i, 
-          TypeUtils.stripDep(e.getKey().getType()).clone(), 
-          ntv, 
-          ast);
+        ast = ABasicPassifier.mkDecl(e.getKey(), i, ast); 
       }
     }
     return ast;
@@ -132,17 +142,24 @@ public class Passificator extends Transformer {
     return true;
   }
   
-  private static class BodyPassifier extends Transformer {
+  
+  /**
+   * 
+   * TODO: description
+   *
+   * @author J. Charles (julien.charles@inria.fr)
+   * @author reviewed by TODO
+   */
+  private static class BodyPassifier extends ABasicPassifier {
     /** the list of local variables declarations. */
     private Declaration newLocals = null;
     /** the current counter associated with each local variable. */
-    private final Environment fLocalVarsCnt = new Environment();
+    private final Environment fEnv = new Environment();
     private final Map<Block, Environment> startBlockStatus = 
       new HashMap<Block, Environment> ();
     private final Map<Block, Environment> endBlockStatus = 
       new HashMap<Block, Environment> ();
     private final VariableDecl fResults;
-    private final TcInterface fTypeChecker;
     private final SimpleGraph<Block> fFlowGraph;
     
     private Body fBody;
@@ -150,23 +167,30 @@ public class Passificator extends Transformer {
     /**
      * @param typeChecker  
      * @param flowGraph 
+     * @param globalEnv 
      * @param results */
     public BodyPassifier(final TcInterface typeChecker, 
                          final SimpleGraph<Block> flowGraph, 
-                         final VariableDecl results) {
-      fTypeChecker = typeChecker;
+                         Environment globalEnv, final VariableDecl results) {
+      super(typeChecker);
       fResults = results;
       fFlowGraph = flowGraph;
+      fEnv.putAll(globalEnv);
     }
     
+    public Environment getEnvironment() {
+      return fEnv;
+    }
+
     public Body getBody() {
       return fBody;
     }
 
     public static BodyPassifier passify(TcInterface fTypeChecker, 
                                         SimpleGraph<Block> flowGraph, 
+                                        Environment globalEnv, 
                                         Body body, VariableDecl results) {
-      BodyPassifier bp = new BodyPassifier(fTypeChecker, flowGraph, results);
+      BodyPassifier bp = new BodyPassifier(fTypeChecker, flowGraph, globalEnv, results);
       body.eval(bp);
       return bp;
     }
@@ -177,7 +201,7 @@ public class Passificator extends Transformer {
     }
     
     private void newLocals() {
-      for (Entry<VariableDecl, Integer> decl: fLocalVarsCnt.entrySet()) {
+      for (Entry<VariableDecl, Integer> decl: fEnv.getLocalSet()) {
         int last = decl.getValue();
         VariableDecl old = decl.getKey();
         for (int i = 1; i <= last; ++i) {
@@ -188,8 +212,8 @@ public class Passificator extends Transformer {
     
     private VariableDecl newResults(VariableDecl old) {
       if (old == null) return null;
-      int last = fLocalVarsCnt.get(old);
-      fLocalVarsCnt.remove(old);
+      int last = fEnv.get(old);
+      fEnv.remove(old);
       for (int i = 0; i < last; ++i) {
         newLocals = mkDecl(old, i, newLocals);
       }
@@ -216,10 +240,10 @@ public class Passificator extends Transformer {
       Set<Block> blist = fFlowGraph.from(block);
       Environment env = merge(blist);
       if (env == null) {
-        env = fLocalVarsCnt.clone();
+        env = fEnv.clone();
       }
       startBlockStatus.put(block, env.clone());      
-      Command newCmd = cmd == null? null : (Command) cmd.eval(new CommandEvaluator(env));
+      Command newCmd = cmd == null? null : (Command) cmd.eval(new CommandEvaluator(getTypeChecker(), env));
       endBlockStatus.put(block, env);     
       Block newTail = tail == null? null : (Block) tail.eval(this);
       Identifiers newSucc = succ;
@@ -229,7 +253,7 @@ public class Passificator extends Transformer {
       if (succList.size() == 1) {
         Block next = succList.iterator().next();
         Environment nextEnv = startBlockStatus.get(next);
-        for (Entry<VariableDecl, Integer> entry: nextEnv.entrySet()) {
+        for (Entry<VariableDecl, Integer> entry: nextEnv.getAllSet()) {
           VariableDecl decl = entry.getKey();
           int currIdx = env.get(decl);
           int oldIdx = entry.getValue();
@@ -245,7 +269,7 @@ public class Passificator extends Transformer {
         }
       }
       
-      updateWith(env); 
+      fEnv.updateWith(env); 
       return Block.mk(name, newCmd, newSucc, newTail, block.loc());
     }
 
@@ -266,8 +290,10 @@ public class Passificator extends Transformer {
         return res;
       }
       
-      for (VariableDecl decl: res.keySet()) {
-        int currInc= res.get(decl);
+      for (Entry<VariableDecl, Integer> entry: res.getAllSet()) {
+        VariableDecl decl = entry.getKey();
+        int currInc = entry.getValue();
+        boolean change = false;
         for (Block b: blist) {
           Integer incarnation = endBlockStatus.get(b).get(decl);
           int inc = incarnation == null ? 0 : incarnation;
@@ -275,26 +301,17 @@ public class Passificator extends Transformer {
             if (inc > currInc) {
               currInc= incarnation;
             }
+            change = true;
           }
         }
-        res.put(decl, currInc + 1);
+        if (change) {
+          res.put(decl, currInc + 1);
+        }
       }
       
       return res;
     }
-    
-    private void updateWith(Environment env) {
 
-      for (VariableDecl decl: env.keySet()) {
-        Integer currInc = fLocalVarsCnt.get(decl);
-        int curr = currInc != null? currInc : 0;
-        Integer incarnation = env.get(decl);
-        if (incarnation > curr) {
-              curr = incarnation;
-              fLocalVarsCnt.put(decl, curr);
-        }
-      }
-    }
     
     /**
      * Returns the new variable representing the result that
@@ -304,127 +321,266 @@ public class Passificator extends Transformer {
     public VariableDecl getResult() {
       return fNewResults;
     }
+  }
+
+  private static class CommandEvaluator extends ABasicPassifier {
+    Environment env;
+    int belowOld = 0;
+    
+    public CommandEvaluator(TcInterface tc, Environment env) {
+      super (tc);
+      this.env = env;
+    }
+
+    /**
+     * Returns a fresh identifier out of an old one.
+     * @param var the old id
+     * @return an id which was not used before.
+     */
+    public AtomId fresh(AtomId var) {
+      
+      VariableDecl decl = getDeclaration(var);
+      Integer i = env.get(decl);
+      int curr = i == null? 0 : i;
+      curr++;
+      env.put(decl, curr);
+      return mkVar(var, curr);
+    }
+    
+    
+    @Override
+    public Expr eval(AtomOld atomOld, Expr e) {
+      ++belowOld;
+      e = (Expr)e.eval(this);
+      --belowOld;
+      return e;
+    }
+    
+    @Override
+    public AssertAssumeCmd eval(final AssignmentCmd assignmentCmd, 
+                                final AtomId var, final Expr rhs) {
+      AssertAssumeCmd result = null;
+      Expr value = (Expr) rhs.eval(this);
+      result = mkAssumeEQ(fresh(var), value);
+      return result;
+    }
 
 
+    /**
+     * Returns a fresh identifier out of an old one.
+     * @param var the old id
+     * @return an id which was not used before.
+     */
+    public AtomId get(AtomId var) {
+      VariableDecl decl = getDeclaration(var);
+      int i = env.get(decl);
+      if (belowOld > 0) {
+        i = 0;
+      }
+      return mkVar(var, i);
+    }
+    
+    @Override
+    public AtomId eval(AtomId atomId, String id, TupleType types) {
+      return get(atomId);
+    }
+  }
+  /**
+   * 
+   * TODO: description
+   *
+   * @author J. Charles (julien.charles@inria.fr)
+   * @author reviewed by TODO
+   */
+  public static class Environment {
+    /** global variable list. */
+    private final Map<VariableDecl, Integer> global = 
+      new LinkedHashMap<VariableDecl, Integer>() ;
+    /** local variable list. */
+    private final Map<VariableDecl, Integer> local = 
+      new LinkedHashMap<VariableDecl, Integer>();
+    /** for efficiency. */
+    private final Map<VariableDecl, Integer> all = 
+      new LinkedHashMap<VariableDecl, Integer>();
+   
+    /** */
+    public Environment() {
+     // nothing 
+    }
+    
+    /**
+     * Adds a variable to the global variables set.
+     * @param decl the variable to add
+     */
+    public void addGlobal(VariableDecl decl) {
+      global.put(decl, 0);
+      all.put(decl, 0);
+    }
+    
+    /**
+     * Put a variable in the environment with its corresponding index.
+     * @param decl de variable declaration
+     * @param i the index
+     */
+    public void put(VariableDecl decl, int i) {
+      if (global.containsKey(decl)) {
+        global.put(decl, i);
+      }
+      else {
+        local.put(decl, i);
+      }
+      all.put(decl, i);
+    }
 
+    /**
+     * Returns the global set of variables.
+     * @return a set of variables and their indexes
+     */
+    public Set<Entry<VariableDecl, Integer>> getGlobalSet() {
+      return global.entrySet();
+    }
+    
+    /**
+     * Returns the local set of variables.
+     * @return a set of variables and their indexes
+     */
+    public Set<Entry<VariableDecl, Integer>> getLocalSet() {
+      return local.entrySet();
+    }
+    
+    /**
+     * Returns all the collected variables so far.
+     * @return a set of variables and their indexes
+     */
+    public Set<Entry<VariableDecl, Integer>> getAllSet() {
+      return all.entrySet();
+    }
+    
+    /**
+     * Removes a variable from the environment.
+     * @param old the variable to remove
+     */
+    public void remove(VariableDecl old) {
+      global.remove(old);
+      local.remove(old);
+      all.remove(old);
+    }
+
+    public void putAll(Environment env) {
+      global.putAll(env.global);
+      local.putAll(env.local);
+      all.putAll(env.all);
+    }
+
+    private Environment(Environment env) {
+      putAll(env);
+    }
+
+    /**
+     * Returns the index corresponding to the given variable
+     * @param key the variable to get the index from
+     * @return an index > to 0
+     */
+    public int get(VariableDecl key) {
+      Integer i = all.get(key);
+      return i == null? 0 : i;
+    }
+    
+    
+    @Override
+    public Environment clone() {
+      return new Environment(this);
+    }
+    
+    /**
+     * Update with another environment.
+     * ie. if variable index is greater in the other 
+     * environment, it replaces the current variable index.
+     * @param env the environment to update with.
+     */
+    public void updateWith(Environment env) {
+      update(env.global, global);
+      update(env.local, local);
+      update(env.all, all);
+    }
+    
+    /**
+     * Updates only the global environment with the given values.
+     * @param env the environment to update with
+     */
+    public void updateGlobalWith(Environment env) {
+      update(env.global, global);
+      all.putAll(global);
+    }
+    
+    private static void update(Map<VariableDecl, Integer> src, 
+                               Map<VariableDecl, Integer> target) {
+
+      for (VariableDecl decl: src.keySet()) {
+        Integer currInc = target.get(decl);
+        int curr = currInc != null? currInc : 0;
+        Integer incarnation = src.get(decl);
+        if (incarnation > curr) {
+              curr = incarnation;
+              target.put(decl, curr);
+        }
+      }
+    }
+  }
+  
+
+  
+  public static abstract class ABasicPassifier extends Transformer {
+
+    private final TcInterface fTypeChecker;
+    public ABasicPassifier (TcInterface tc) {
+      fTypeChecker = tc;
+    }
     /**
      * Returns the variable declaration corresponding to the given id.
      * @param id the id to check for
      * @return a valid variable declaration
      */
     VariableDecl getDeclaration(AtomId id) {
-      return (VariableDecl) fTypeChecker.getST().ids.def(id);
+      return (VariableDecl) getTypeChecker().getST().ids.def(id);
+    }
+    public TcInterface getTypeChecker() {
+      return fTypeChecker;
+    }  
+    
+    public static AssertAssumeCmd mkAssumeEQ(Expr left, Expr right) {
+      return AssertAssumeCmd.mk(AssertAssumeCmd.CmdType.ASSUME, null,
+        BinaryOp.mk(BinaryOp.Op.EQ, left, right));
     }
     
-    private class CommandEvaluator extends Transformer {
-      Environment env;
-      int belowOld = 0;
-      
-      public CommandEvaluator(Environment env) {
-         this.env = env;
+    public static AtomId mkVar(VariableDecl decl, int idx) {
+      String name = decl.getName();
+      if (idx != 0) {
+        name += "$$" + idx;
       }
-
-      /**
-       * Returns a fresh identifier out of an old one.
-       * @param var the old id
-       * @return an id which was not used before.
-       */
-      public AtomId fresh(AtomId var) {
-        
-        VariableDecl decl = getDeclaration(var);
-        Integer i = env.get(decl);
-        int curr = i == null? 0 : i;
-        curr++;
-        env.put(decl, curr);
-        return mkVar(var, curr);
-      }
-      
-      
-      @Override
-      public Expr eval(AtomOld atomOld, Expr e) {
-        ++belowOld;
-        e = (Expr)e.eval(this);
-        --belowOld;
-        return e;
-      }
-      
-      @Override
-      public AssertAssumeCmd eval(final AssignmentCmd assignmentCmd, 
-                                  final AtomId var, final Expr rhs) {
-        AssertAssumeCmd result = null;
-        Expr value = (Expr) rhs.eval(this);
-        result = mkAssumeEQ(fresh(var), value);
-        return result;
-      }
-
-
-      /**
-       * Returns a fresh identifier out of an old one.
-       * @param var the old id
-       * @return an id which was not used before.
-       */
-      public AtomId get(AtomId var) {
-        VariableDecl decl = getDeclaration(var);
-        int i = env.get(decl);
-        if (belowOld > 0) {
-          i = 0;
-        }
-        return mkVar(var, i);
-      }
-      
-      @Override
-      public AtomId eval(AtomId atomId, String id, TupleType types) {
-        return get(atomId);
-      }
-    }
-  }
-
-  private static class Environment extends  LinkedHashMap<VariableDecl, Integer> { 
-    public Environment() {
-     // nothing 
-    }
-    public int get(VariableDecl key) {
-      Integer i = super.get(key);
-      return i == null? 0 : i;
+      return AtomId.mk(name, null);
     }
     
-    @Override
-    public Environment clone() {
-      return (Environment) super.clone();
+    public static AtomId mkVar(AtomId id, int idx) {
+      String name = id.getId();
+      if (idx != 0) {
+        name += "$$" + idx;
+      }
+      return  AtomId.mk(name, id.getTypes(), id.loc());
     }
-  }
-  
-  
-  public static AssertAssumeCmd mkAssumeEQ(Expr left, Expr right) {
-    return AssertAssumeCmd.mk(AssertAssumeCmd.CmdType.ASSUME, null,
-      BinaryOp.mk(BinaryOp.Op.EQ, left, right));
-  }
-  
-  public static AtomId mkVar(VariableDecl decl, int idx) {
-    String name = decl.getName();
-    if (idx != 0) {
-      name += "$$" + idx;
+    
+    public static VariableDecl mkDecl(VariableDecl old, int idx, Declaration next) {
+      String name = old.getName();
+      if (idx != 0) {
+        name += "$$" + idx;
+      }
+      return VariableDecl.mk(
+        name,
+        TypeUtils.stripDep(old.getType()).clone(),
+        old.getTypeVars() == null? null :old.getTypeVars().clone(),
+        next);
     }
-    return AtomId.mk(name, null);
+   
   }
-  
-  public static AtomId mkVar(AtomId id, int idx) {
-    String name = id.getId();
-    if (idx != 0) {
-      name += "$$" + idx;
-    }
-    return  AtomId.mk(name, id.getTypes(), id.loc());
-  }
-  
-  public static VariableDecl mkDecl(VariableDecl old, int idx, Declaration next) {
-    String name = old.getName();
-    if (idx != 0) {
-      name += "$$" + idx;
-    }
-    return VariableDecl.mk(
-      name,
-      TypeUtils.stripDep(old.getType()).clone(),
-      old.getTypeVars() == null? null :old.getTypeVars().clone(),
-      next);
-  }
+
 }
