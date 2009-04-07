dc8b6cb6aff138ff945dfcc1fcb73fb109d3ceb6
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2009-04-07 16:32:34 +0000

    style stuff

--- src/LogicSync/src/mobius/logic/lang/boogie/BoogieLanguage.java
+++ src/LogicSync/src/mobius/logic/lang/boogie/BoogieLanguage.java
@@ -1,9 +1,8 @@
 package mobius.logic.lang.boogie;
 
 import java.io.File;
-import java.util.List;
 
-import mobius.logic.lang.ALanguage;
+import mobius.logic.lang.ABasicLanguage;
 import mobius.logic.lang.generic.ast.GenericAst;
 
 /** 
@@ -11,36 +10,26 @@ import mobius.logic.lang.generic.ast.GenericAst;
  *
  * @author rgrig@
  */
-public class BoogieLanguage extends ALanguage {
-  @Override public boolean isLanguageFile(File f) {
-    assert false : "todo";
-    return false;
+public class BoogieLanguage extends ABasicLanguage {
+  /** {@inheritDoc} */
+  @Override public boolean isLanguageFile(final File f) {
+    return f.getName().endsWith(".bpl");
   }
 
-  @Override public void addInput(File in) {
-    assert false : "todo";
-  }
+  /** Does nothing. */
+  @Override public void prepare() { }
 
-  @Override public void addMerge(File merge) {
-    assert false : "todo";
-  }
-
-  @Override public void addGenerate(File gen) {
+  /** {@inheritDoc} */
+  @Override public void generateFrom(final GenericAst ast) {
     assert false : "todo";
   }
 
-  @Override public void prepare() {
+  /** {@inheritDoc} */
+  @Override public void mergeWith(final GenericAst ast) {
     assert false : "todo";
   }
 
-  @Override public void generateFrom(GenericAst ast) {
-    assert false : "todo";
-  }
-
-  @Override public void mergeWith(GenericAst ast) {
-    assert false : "todo";
-  }
-
+  /** {@inheritDoc} */
   @Override public GenericAst extractGenericAst() {
     assert false : "todo";
     return null;
