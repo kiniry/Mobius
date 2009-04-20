60978d5855e8ba8e1843571fe6c6f29e734c14ad
sw237122 <sw237122@c6399e9c-662f-4285-9817-23cccad57800>
2009-04-20 20:59:39 +0000

    added graphical presenter for verification results

---
+++ src/Umbra/trunk/source/umbra/verifier/SWTResultPresenter.java
@@ -0,0 +1,70 @@
+/*
+ * @title       "Umbra"
+ * @description "An editor for the Java bytecode and BML specifications"
+ * @copyright   "(c) 2006-2008 University of Warsaw"
+ * @license     "All rights reserved. This program and the accompanying
+ *               materials are made available under the terms of the LGPL
+ *               licence see LICENCE.txt file"
+ */
+package umbra.verifier;
+
+/**
+ * @author Szymon Wrzyszcz
+ * @version a-01
+ *
+ */
+public class SWTResultPresenter extends ResultPresenter {
+
+  /**
+   * @param verifier
+   */
+  public SWTResultPresenter(BytecodeVerifier verifier) {
+    super(verifier);
+  }
+  
+  /* (non-Javadoc)
+   * @see umbra.verifier.ResultPresenter#presentAll()
+   */
+  @Override
+  public void presentAll() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  /* (non-Javadoc)
+   * @see umbra.verifier.ResultPresenter#presentPass1()
+   */
+  @Override
+  public void presentPass1() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  /* (non-Javadoc)
+   * @see umbra.verifier.ResultPresenter#presentPass2()
+   */
+  @Override
+  public void presentPass2() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  /* (non-Javadoc)
+   * @see umbra.verifier.ResultPresenter#presentPass3a()
+   */
+  @Override
+  public void presentPass3a() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  /* (non-Javadoc)
+   * @see umbra.verifier.ResultPresenter#presentPass3b()
+   */
+  @Override
+  public void presentPass3b() {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
