beb3a5734c3269335163557797d4c754e23d0184
piotr.swigon <piotr.swigon@c6399e9c-662f-4285-9817-23cccad57800>
2009-04-20 21:55:15 +0000

    added a_shell param and changed constructor

--- src/Umbra/trunk/source/umbra/verifier/SWTVerificationFactory.java
+++ src/Umbra/trunk/source/umbra/verifier/SWTVerificationFactory.java
@@ -8,26 +8,36 @@
  */
 package umbra.verifier;
 
+import org.eclipse.swt.widgets.Shell;
+
 /**
  * This is a factory for graphical save confirmer.
  * 
  * @author Tomasz Olejniczak (to236111@students.mimuw.edu.pl)
+ * @author Szymon Wrzyszcz (sw237122@students.mimuw.edu.pl)
  * @version a-01
  *
  */
 public class SWTVerificationFactory extends VerificationFactory {
 
+  private Shell a_shell;
+  
+  /**
+   * @param a_shell
+   */
+  public SWTVerificationFactory(Shell a_shell) {
+    this.a_shell = a_shell;
+  }
+  
   /**
    * Creates new result presenter for given verifier.
-   * 
-   * TODO (to236111): change to graphical presenter
    * 
    * @return presenter for results of verification
    * @param  verifier
    */
   @Override
   public ResultPresenter getResultPresenter(BytecodeVerifier verifier) {
-    return new ConsoleResultPresenter(verifier);
+    return new SWTResultPresenter(verifier, a_shell);
   }
 
   /**
@@ -39,6 +49,6 @@ public class SWTVerificationFactory extends VerificationFactory {
    */
   @Override
   public SaveConfirmer getSaveConfirmer(ResultPresenter resultPresenter) {
-    return new SWTSaveConfirmer(resultPresenter); 
+    return new SWTSaveConfirmer(resultPresenter, a_shell); 
   }
 }
