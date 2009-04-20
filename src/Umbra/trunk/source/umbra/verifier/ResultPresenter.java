729a41117d9e396355826af76413eca855ab89c9
piotr.swigon <piotr.swigon@c6399e9c-662f-4285-9817-23cccad57800>
2009-04-20 23:01:37 +0000

    added some auxiliary methods from console presenter

--- src/Umbra/trunk/source/umbra/verifier/ResultPresenter.java
+++ src/Umbra/trunk/source/umbra/verifier/ResultPresenter.java
@@ -8,6 +8,10 @@
  */
 package umbra.verifier;
 
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.Type;
+import org.apache.bcel.verifier.VerificationResult;
+
 /**
  * @author Szymon Wrzyszcz (sw237122@students.mimuw.edu.pl)
  * @version a-01
@@ -48,5 +52,42 @@ public abstract class ResultPresenter {
    * presents result of whole verification
    */
   public abstract void presentAll();
+  
+  protected String getInfo(VerificationResult result) {
+    if (result.getStatus() == VerificationResult.VERIFIED_OK) {
+      return "OK";
+    } else if (result.getStatus() == VerificationResult.VERIFIED_REJECTED) {
+      return result.getMessage();
+    } else {
+      return "?";
+    }
+  }
+  
+  protected String line(int n, char c) {
+    StringBuilder sb = new StringBuilder();
+    for (int i=0; i < n; i++) {
+      sb.append(c);
+    }
+    return sb.toString();
+  }
+  
+  protected String presentMethod(Method m) {
+    Type ret = m.getReturnType();
+    String name = m.getName();
+    Type[] args = m.getArgumentTypes();
+    StringBuilder sb = new StringBuilder();
+    sb.append(ret.toString());
+    sb.append(" ");
+    sb.append(name);
+    sb.append("(");
+    for (int i = 0; i < args.length; i++) {
+      sb.append(args[i].toString());
+      if (i < args.length - 1) {
+        sb.append(", ");
+      }
+    }
+    sb.append(")");
+    return sb.toString();
+  }
   
 }
