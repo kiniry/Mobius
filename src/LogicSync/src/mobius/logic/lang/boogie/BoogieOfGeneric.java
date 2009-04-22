738b70220d1e62dcf2ee8653d7f563b064f3affc
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2009-04-22 16:41:03 +0000

    started boogie of generic

--- src/LogicSync/src/mobius/logic/lang/boogie/BoogieOfGeneric.java
+++ src/LogicSync/src/mobius/logic/lang/boogie/BoogieOfGeneric.java
@@ -1,8 +1,15 @@
 package mobius.logic.lang.boogie;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 import freeboogie.ast.Ast;
+import freeboogie.ast.BinaryOp;
+import freeboogie.ast.PrimitiveType;
+import freeboogie.ast.UnaryOp;
 import mobius.logic.lang.generic.ast.ACleanEvaluator;
 import mobius.logic.lang.generic.ast.Atom;
 import mobius.logic.lang.generic.ast.GenericAst;
@@ -13,6 +20,41 @@ import mobius.logic.lang.generic.ast.Term;
  * @author rgrig@
  */
 public class BoogieOfGeneric extends ACleanEvaluator<Ast> {
+  /** 
+   * Maps special ids in the generic language to the corresponding
+   * Boogie binary operator.
+   */
+  private static Map<String, BinaryOp.Op> binaryOpFromName;
+
+  /** 
+   * Maps special ids in the generic language to the corresponding
+   * Boogie unary operator.
+   */
+  private static Map<String, UnaryOp.Op> unaryOpFromName;
+
+  /**
+   * Maps special ids in the generic language to the corresponding
+   * Boogie primitive types.
+   */
+  private static Map<String, PrimitiveType.Ptype> typeFromName;
+
+  /** 
+   * These are not translated to Boogie at all, usually because 
+   * they are implicit there.
+   */
+  private static Set<String> ignoredIds;
+
+  /** These ids are translated into casts. */
+  private static Set<String> castIds;
+
+  static {
+    binaryOpFromName = new HashMap<String, BinaryOp.Op>();
+    unaryOpFromName = new HashMap<String, UnaryOp.Op>();
+    typeFromName = new HashMap<String, PrimitiveType.Ptype>();
+    ignoredIds = new HashSet<String>();
+    castIds = new HashSet<String>();
+  }
+
   /** {@inheritDoc} */
   @Override
   public Ast evalApplication(final Term next, final Term first) {
@@ -51,6 +93,10 @@ public class BoogieOfGeneric extends ACleanEvaluator<Ast> {
   /** {@inheritDoc} */
   @Override
   public Ast evalFormula(final String id, final Term term) {
+    // if special, don't process
+    // ask typechecker if it's a function
+      // translate to function
+      // translate to axiom
     assert false : "not implemented";
     return null;
   }
---
+++ src/LogicSync/test/boogie1.bpl
@@ -0,0 +1,34 @@
+/*
+ negb <-> !
+ integralLE <-> <
+ Zeq_bool -> ==
+ = -> ==
+ \/ -> ||   (*)
+ bor -> ||
+
+ forall <-> forall
+
+ (S_to_Z x) <-> cast(x, Z)
+ (Z_to_S x) <-> cast(x, S)
+ (S_to_bool x) <-> cast(x, bool)
+
+ Z <-> int
+ Prop -> bool
+ bool -> bool
+
+ ignore:
+    Z_to_S_elim   
+    T
+ */
+
+type S;
+
+function select(S, S) returns (S); 
+  // ask TypeChecker.checkType(); make it public if it isn't
+function store(S, S, S) returns (S); 
+function select(S, Z, S) returns (S); 
+
+axiom arrayLenghtAx :
+  (forall a : S :: 0 < cast(arrayLength(a),Z));
+axiom N F;
+
