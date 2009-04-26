a3b387d695ee8232f0661a0fb6356f421384b551
kjk <kjk@c6399e9c-662f-4285-9817-23cccad57800>
2009-04-26 12:29:35 +0000

    Updated JMLCompile to match new Jml2Bml plugin api

--- src/Umbra/trunk/source/umbra/java/actions/JMLCompile.java
+++ src/Umbra/trunk/source/umbra/java/actions/JMLCompile.java
@@ -10,8 +10,10 @@ package umbra.java.actions;
 
 import java.io.IOException;
 
+import jml2bml.plugin.Jml2BmlPlugin;
+import jml2bml.plugin.NotTranslatedException;
+
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -22,7 +24,6 @@ import org.eclipse.ui.part.FileEditorInput;
 import umbra.lib.FileNames;
 import umbra.lib.GUIMessages;
 import annot.io.ReadAttributeException;
-import api.Jml2BmlAPI;
 
 /**
  * This class defines the action related to Java source editor.
@@ -35,6 +36,7 @@ import api.Jml2BmlAPI;
  * @author Jarosław Paszek (jp209217@students.mimuw.edu.pl)
  * @author Wojciech Was (ww209224@students.mimuw.edu.pl)
  * @author Aleksy Schubert (alx@mimuw.edu.pl)
+ * @author Krzysztof Jakubczyk (kjk@mimuw.edu.pl)
  * @version a-01
  */
 
@@ -65,7 +67,7 @@ public class JMLCompile extends DisasBCEL {
       return;
     }
     try {
-      compile(jFile, bFile);
+      Jml2BmlPlugin.getDefault().compile(jFile, bFile);
       openBCodeEditorForJavaFile(jFile);
     } catch (IOException e) {
       MessageDialog.openError(shell,
@@ -84,29 +86,10 @@ public class JMLCompile extends DisasBCEL {
       messageProblemsWithLoading(jFile.getLocation());
     } catch (ReadAttributeException e) {
       messageProblemsWithLoading(jFile.getLocation());
+    } catch (NotTranslatedException e) {
+      MessageDialog.openError(shell,
+                              GUIMessages.DISAS_MESSAGE_TITLE,
+                              GUIMessages.DISAS_CLASSFILEOUTPUT_PROBLEMS);
     }
   }
-
-  /**
-   * This method delegates the compilation of JML to the Jml2Bml compiler.
-   *
-   * @param a_jfile the file for the Java source code
-   * @param a_bfile the corresponding file for the class file
-   * @throws ClassNotFoundException in case the class for the given file
-   *   cannot be found
-   * @throws ReadAttributeException in case a BML attribute from the given class
-   *   file cannot be found
-   * @throws IOException in case the class file cannot be saved to the given
-   *   location
-   */
-  private void compile(final IFile a_jfile, final IFile a_bfile)
-    throws ClassNotFoundException, ReadAttributeException, IOException {
-    String bname = a_bfile.getName();
-    final IPath path = a_bfile.getLocation();
-    bname = bname.substring(0, bname.lastIndexOf("."));
-    final String bpath = path.removeLastSegments(1).toOSString();
-    final String sourceFile = a_jfile.getLocation().toOSString();
-    Jml2BmlAPI.compile(sourceFile, bpath, bname);
-  }
-
 }
