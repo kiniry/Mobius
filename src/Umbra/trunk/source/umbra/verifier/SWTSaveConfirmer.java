9660559392b223ad5ee2a9f4e96129c5f1ac9b1a
tolej <tolej@c6399e9c-662f-4285-9817-23cccad57800>
2009-05-01 23:22:53 +0000

    Umbra: minor code style fixes

--- src/Umbra/trunk/source/umbra/verifier/SWTSaveConfirmer.java
+++ src/Umbra/trunk/source/umbra/verifier/SWTSaveConfirmer.java
@@ -17,54 +17,65 @@ import org.eclipse.swt.widgets.Shell;
  * bytecode verification error occurs it creates dialog
  * window which asks user whether to proceed with saving the
  * file or not.
- * 
+ *
  * TODO (to236111) add window which presents verification results
- * 
+ *
  * @author Tomasz Olejniczak (to236111@students.mimuw.edu.pl)
  * @version a-01
  *
  */
 public class SWTSaveConfirmer extends SaveConfirmer {
-  
-  private BytecodeVerifier verifier;
-  private Shell a_shell;
-  
+
+  /**
+   * Verifier which does verification.
+   */
+  private BytecodeVerifier my_verifier;
+
+  /**
+   * Parent shell.
+   */
+  private Shell my_shell;
+
   /**
    * Creates graphical save confirmer used during saving a bytecode
    * file.
-   * 
-   * @param resultPresenter presenter with verification output
+   *
+   * @param a_result_presenter presenter with verification output
+   * @param a_shell parent shell
    */
-  public SWTSaveConfirmer(ResultPresenter resultPresenter, Shell a_shell) {
-    super(resultPresenter);
-    verifier = resultPresenter.verifier;
-    this.a_shell = a_shell;
+  public SWTSaveConfirmer(final ResultPresenter a_result_presenter,
+                          final Shell a_shell) {
+    super(a_result_presenter);
+    my_verifier = a_result_presenter.verifier;
+    this.my_shell = a_shell;
   }
- 
+
   /**
    * This method is called during saving a bytecode file. If a
    * bytecode verification error occurs it creates dialog
    * window which asks user whether to proceed with saving the
    * file or not
-   * 
+   *
    * TODO (to236111) add constants in GUIMessages for error message
    * and window title
-   * 
+   *
    * @return <code>true</code> if there weren't any verification
    * errors, user decision otheriwse
    */
   @Override
   public boolean confirm() {
-    if (!verifier.passed()) {
+    if (!my_verifier.passed()) {
       resultPresenter.presentAll();
-      MessageBox msgBox = new MessageBox(a_shell, SWT.ICON_QUESTION | SWT.YES | SWT.NO);
-      msgBox.setMessage("Errors occured during bytecode verification. Save anyway?");
+      final MessageBox msgBox =
+        new MessageBox(my_shell, SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+      msgBox.
+      setMessage("Errors occured during bytecode verification. Save anyway?");
       msgBox.setText("Verification errors");
-      int res = msgBox.open();
+      final int res = msgBox.open();
       return (res == SWT.YES);
     } else {
       return true;
     }
   }
-  
+
 }
