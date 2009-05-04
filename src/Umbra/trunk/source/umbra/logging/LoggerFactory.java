b9c66e8f95c921dd4b0d1dd41b2a85213bc2e04f
sw237122 <sw237122@c6399e9c-662f-4285-9817-23cccad57800>
2009-05-04 21:06:22 +0000

    style changed to agree with cs

--- src/Umbra/trunk/source/umbra/logging/LoggerFactory.java
+++ src/Umbra/trunk/source/umbra/logging/LoggerFactory.java
@@ -13,73 +13,104 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * @author szymon wrzyszcz (sw237122@students.mimuw.edu.pl)
  * @version a-01
  *
  */
-public class LoggerFactory {
+public final class LoggerFactory {
 
-  private enum Mode {DEVELOPMENT, DEBUG, PRODUCTION};
-  
-  private static Mode mode = Mode.DEVELOPMENT;
+  /**
+   *
+   * @author szymon wrzyszcz sw237122@students.mimuw.edu.pl)
+   * @version a-01
+   *
+   */
+  private enum Mode { DEVELOPMENT, DEBUG, PRODUCTION };
+
+  /**
+   *
+   */
+  private static Mode my_mode = Mode.DEVELOPMENT;
+
+  /**
+   *
+   */
   private static LoggerFactoryImpl loggerFactoryImpl = getRightImpl();
 
-  private static LoggerFactoryImpl getRightImpl() {
-    switch (mode) {
-      case DEVELOPMENT: 
-        return new LoggerFactoryDevelopmentImpl();
-      case DEBUG: 
-        return new LoggerFactoryDevelopmentImpl();
-      case PRODUCTION: 
-        return new LoggerFactoryDevelopmentImpl();
-    }
-    return null;
-  }
-  
   /**
-   * @param newMode
+   * forbids creating objects of the class.
    */
-  public static void setMode(final Mode newMode) {
-    if (newMode != mode) {
-      mode = newMode;
+  private LoggerFactory() {
+  }
+
+  /**
+   *
+   * @return impl
+   */
+  private static LoggerFactoryImpl getRightImpl() {
+    LoggerFactoryImpl impl = null;
+    switch (my_mode) {
+      case DEVELOPMENT:
+        impl = new LoggerFactoryDevelopmentImpl();
+        break;
+      case DEBUG:
+        impl = new LoggerFactoryDevelopmentImpl();
+        break;
+      case PRODUCTION:
+        impl = new LoggerFactoryDevelopmentImpl();
+        break;
+      default:
+        break;
+    }
+    return impl;
+  }
+
+  /**
+   * @param a_new_mode new mode
+   */
+  public static void setMode(final Mode a_new_mode) {
+    if (a_new_mode != my_mode) {
+      my_mode = a_new_mode;
       loggerFactoryImpl = getRightImpl();
     }
   }
-  
+
   /**
    * @return mode the factory is currently in
    */
   public static Mode getMode() {
-    return mode;
+    return my_mode;
   }
-  
+
   /**
-   * @param c class we create logger for
+   * @param a_class class we create logger for
    * @return new instance of logger with formatters and handlers set by
    * currently used factory implementation
    */
-  public static Logger getClassLogger(final Class<?> c) {
-    return loggerFactoryImpl.getClassLogger(c);
+  public static Logger getClassLogger(final Class < ? > a_class) {
+    return loggerFactoryImpl.getClassLogger(a_class);
   }
-  
+
   /**
-   * @param name
+   * @param a_name name of the logger
    * @return new logger
    */
-  public static Logger getDefaultLogger(String name) {
-    return loggerFactoryImpl.getDefaultLogger(name);
+  public static Logger getDefaultLogger(final String a_name) {
+    return loggerFactoryImpl.getDefaultLogger(a_name);
   }
-  
+
   /**
-   * @param name
+   * @param a_name name of the logger
    * @return new logger
    */
-  public static Logger getMockUpLogger(String name) {
-    Logger logger = loggerFactoryImpl.getDefaultLogger("mockup[" + name + "]");
+  public static Logger getMockUpLogger(final String a_name) {
+    final Logger logger = loggerFactoryImpl.getDefaultLogger(
+              "[" + a_name + "]");
     logger.setLevel(Level.ALL);
     for (Handler h : logger.getHandlers()) {
       h.setLevel(Level.ALL);
     }
     return logger;
   }
-  
+
 }
