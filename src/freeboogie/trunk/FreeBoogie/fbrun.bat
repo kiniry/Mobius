49e085df7cb9bca2540cb6a12aa2d56a38ea6f1d
rgrig <rgrig@c6399e9c-662f-4285-9817-23cccad57800>
2009-06-23 21:03:02 +0000

    stop supporting bat on windows (use jar instead)

--- src/freeboogie/trunk/FreeBoogie/fbrun.bat
+++
@@ -1,4 +0,0 @@
-SET ROOT=.
-SET CP=%ROOT%/classes;%ROOT%/lib/antlr-2.7.7.jar;%ROOT%/lib/antlr-3.0.1.jar;%ROOT%/lib/stringtemplate-3.1b1.jar
-java -Xms64m -Xmx64m -ea -cp "%CP%" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
