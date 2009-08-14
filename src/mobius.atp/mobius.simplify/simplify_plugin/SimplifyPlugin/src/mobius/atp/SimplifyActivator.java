af61e2e36aae04a617457758bb296de7879efbb3
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-08-14 10:37:12 +0000

    added properly an activator

---
+++ src/mobius.atp/mobius.simplify/simplify_plugin/SimplifyPlugin/src/mobius/atp/SimplifyActivator.java
@@ -0,0 +1,51 @@
+package mobius.atp;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author J. Charles (julien.charles@inria.fr)
+ */
+public class SimplifyActivator extends AbstractUIPlugin {
+
+  /** The plug-in ID. */
+  public static final String PLUGIN_ID = "mobius.directvcgen.ui";
+
+  /** The shared instance. */
+  private static SimplifyActivator plugin;
+
+  
+  
+  /**
+   * The constructor.
+   */
+  public SimplifyActivator() {
+    plugin = this;
+  }
+
+  /** {@inheritDoc} */
+  public void start(final BundleContext context) throws Exception {
+    super.start(context);
+
+  }
+
+  /** {@inheritDoc} */
+  public void stop(final BundleContext context) throws Exception {
+    super.stop(context);
+    
+  }
+
+  /**
+   * Returns the shared instance.
+   *
+   * @return the shared instance
+   */
+  public static SimplifyActivator getDefault() {
+    return plugin;
+  }
+  
+
+}
+
