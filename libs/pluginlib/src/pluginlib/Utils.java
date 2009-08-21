0fc83cde584f043f85f0962555e32a7d9e9e82bb
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-08-21 13:39:52 +0000

    added the prover path

--- libs/pluginlib/src/pluginlib/Utils.java
+++ libs/pluginlib/src/pluginlib/Utils.java
@@ -891,4 +891,19 @@ public class Utils {
 		int z = p.waitFor();
 		return z;
 	}
+	
+  public static class ProverPath {
+    private final String name;
+    private final String path;
+    public ProverPath(String name, String path) {
+      this.name = name;
+      this.path = path;
+    }
+    public String getName() {
+      return name;
+    }
+    public String getPath() {
+      return path;
+    }
+  }
 }
