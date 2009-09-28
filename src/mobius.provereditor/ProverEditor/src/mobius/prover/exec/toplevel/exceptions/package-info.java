b40732481d6ecf0f9c6cd556bcf64cc54352bfe1
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 12:28:07 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/exec/toplevel/exceptions/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides some exception classes for the top level. 
+ * The choice that has been made for the prover exception is 
+ * to have lots of specific exception classes instead of 
+ * a single on with different message kinds.
+ * @see prover.exec.AProverException
+ */
+package mobius.prover.exec.toplevel.exceptions;
+
+
