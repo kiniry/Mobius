89f0cb9060375982a7242fdc2e1e3b72d53cc2b5
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 12:30:31 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/exec/toplevel/stream/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides the various classes to handle the
+ * top level streams. All the classes are based
+ * on the Java's streams.
+ * Mostly it is in order to be sure to have proper
+ * asynchronous readings.
+ * @see java.io.InputStream
+ * @see java.io.OutputStream
+ */
+package mobius.prover.exec.toplevel.stream;
+
