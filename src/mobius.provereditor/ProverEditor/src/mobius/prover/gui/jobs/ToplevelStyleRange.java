248ef0e0568265e06e57bf551dc94ca1b38e4a92
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 11:22:00 +0000

    added the package info file

--- src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/jobs/ToplevelStyleRange.java
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/jobs/ToplevelStyleRange.java
@@ -6,11 +6,19 @@ import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.graphics.Color;
 
+/**
+ * A style range with a white background.
+ * @author J. Charles (julien.charles@gmail.com)
+ */
 public class ToplevelStyleRange extends StyleRange implements IColorConstants {
-
-  public ToplevelStyleRange(int i, int len, Color col) {
+  /**
+   * Creates a top level style range, a style range with a white background.
+   * @param i the starting point of the range
+   * @param len the length of the range
+   * @param col the foreground color
+   */
+  public ToplevelStyleRange(final int i, final int len, final Color col) {
     super(i, len, col, WHITE);
     font = JFaceResources.getTextFont();
   }
-
 }
---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/jobs/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides some facilities to implement Jobs. 
+ * These Jobs are mostly here to append some text
+ * to the current proof view with the right color.
+ * @see org.eclipse.ui.progress.UIJob
+ */
+package mobius.prover.gui.jobs;
+
