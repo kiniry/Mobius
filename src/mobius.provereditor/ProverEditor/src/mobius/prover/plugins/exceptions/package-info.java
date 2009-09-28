b8d3f50db5fa4e1a373c2d1c40b85bb42d871b4b
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 10:06:19 +0000

    added some other package info files

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/plugins/exceptions/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Exceptions that shall be thrown by the plugins 
+ * can be found here. 
+ * Plugins should use directly the exceptions in this 
+ * package, or subclass them for more specific purpose.
+ */
+package mobius.prover.plugins.exceptions;
+
---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/plugins/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Start here if you want to add a prover in ProverEditor.
+ * All the plugins specific things are in this package. 
+ * When a prover adds an extension to the extension point
+ * <code>prover.editor</code>, 2 classes have to be implemented
+ * inherited from this package.
+ */
+package mobius.prover.plugins;
+
