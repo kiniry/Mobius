7a31cf5d6264db3f4679b43d322b0525357e1516
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 12:32:26 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/exec/toplevel/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the top level. The top level is the way to
+ * interact with a prover top level. It is generic and
+ * it is parametized by a prover specific translator.
+ * @see prover.plugins.IProverTopLevel
+ * @see prover.plugins.AProverTranslator
+ * @see prover.Prover
+ */
+package mobius.prover.exec.toplevel;
+
