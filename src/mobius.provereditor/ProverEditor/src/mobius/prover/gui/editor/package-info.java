72c94b9ab123e2dedca78e600d4e47295bfd5260
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 11:16:55 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/editor/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides all the facilities to highlight a prover language file.
+ * The main class in this package is the 
+ * {@link prover.gui.editor.ProverEditor} class,
+ * but there are also lots of helper classes buried under, which could
+ * be useful in another context, or help define the scanners used to do
+ * the highlightings.
+ * @see org.eclipse.ui.editors.text.TextEditor
+ */
+package mobius.prover.gui.editor;
+
