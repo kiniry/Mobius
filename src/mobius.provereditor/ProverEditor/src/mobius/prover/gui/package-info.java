a6b62d9e9a14522aa7cc26dbc59506ab40f73e6a
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 12:24:20 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the editor, the viewpart and most of the gui
+ * elements to be used for the prover editor. 
+ * It provides also several facilities and templates
+ * to use with foreign plugins. This package 
+ * is in a way glued to the <code>prover.exec</code> package.
+ * @see org.eclipse.ui.part.ViewPart
+ */
+package mobius.prover.gui;
+
