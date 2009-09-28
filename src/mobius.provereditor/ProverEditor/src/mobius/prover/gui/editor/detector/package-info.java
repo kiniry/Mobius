8d2a9c5ca7bfe525ea5fe24a7e4b0a806e3b4846
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 11:09:59 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/editor/detector/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Basic implementations for some word detectors.
+ * These implementations were first used in Coq context, 
+ * but they are alike to be used with some other provers.
+ * @see org.eclipse.jface.text.rules.IWordDetector
+ */
+package mobius.prover.gui.editor.detector;
+
