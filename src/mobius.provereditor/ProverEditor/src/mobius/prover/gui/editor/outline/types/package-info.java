abd6e0d5412e42dedb441549aa63f5d4a81387fc
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 11:14:20 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/editor/outline/types/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides all the facilities to build the nodes 
+ * of the tree of the outline.
+ * @see org.eclipse.ui.views.contentoutline
+ */
+package mobius.prover.gui.editor.outline.types;
+
