15cdb71afdb32ee40f25d6d333400b4f1007ef14
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 11:23:30 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/popup/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the popup menu elements.
+ * It contains the action for launching the Ide as well as
+ * the action to compile a file.
+ * @see org.eclipse.jface.action.IAction
+ */
+package mobius.prover.gui.popup;
+
