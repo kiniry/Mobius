5861f4908ed7ffc40bce9d9ac55b528b5fbd4b95
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 12:22:26 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/gui/preference/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the preference pages for the ProverEditor. For each prover a 
+ * {@link prover.gui.preference.ProverPreferencePage} is automatically added 
+ * to the main preference page ({@link prover.gui.preference.PreferencePage}).
+ * @see org.eclipse.ui.IWorkbenchPreferencePage
+ */
+package mobius.prover.gui.preference;
+
