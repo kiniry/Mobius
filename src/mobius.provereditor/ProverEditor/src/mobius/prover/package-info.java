1b20d2bd4d34c01fbc84e214c7f82beb3a2dc7c2
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 10:00:55 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The main package to edit files from provers in eclipse.
+ * It contains 3 subpackages: 
+ * <li> <code>prover.exec</code> to handle the basic interactions 
+ * with the provers 
+ * <li> <code>prover.gui</code> to glue the plugin within Eclipse's ide.
+ * <li> <code>prover.plugins</code> to glue the extension point and help
+ * in adding new prover plugins.
+ * @see org.eclipse.ui.plugin.AbstractUIPlugin
+ */
+package mobius.prover;
+
