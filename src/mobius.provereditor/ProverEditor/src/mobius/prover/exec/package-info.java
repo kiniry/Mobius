bf31cb7c5665d9f486c012394e77b9a677e4019c
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-09-28 12:33:41 +0000

    added the package info file

---
+++ src/mobius.provereditor/ProverEditor/src/mobius/prover/exec/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The package managing all the direct interactions with the prover.
+ * The main part is the top level ({@link prover.exec.toplevel.TopLevel})
+ * which implements the {@link prover.exec.ITopLevel} interface
+ * to have communication with the prover plugins.
+ * @see prover.plugins.IProverTopLevel
+ * @see prover.exec.ITopLevel
+ */
+package mobius.prover.exec;
+
