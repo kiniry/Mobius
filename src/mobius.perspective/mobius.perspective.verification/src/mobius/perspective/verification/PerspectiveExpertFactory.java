f0c14891b7929fc55840b4a09879013c01d7d24b
jcharles <jcharles@c6399e9c-662f-4285-9817-23cccad57800>
2009-12-16 15:53:28 +0000

    merged with expert

---
+++ src/mobius.perspective/mobius.perspective.verification/src/mobius/perspective/verification/PerspectiveExpertFactory.java
@@ -0,0 +1,35 @@
+package mobius.perspective.verification;
+
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.console.IConsoleConstants;
+
+public class PerspectiveExpertFactory implements IPerspectiveFactory {
+  /** represents the number two third (2/3). */
+  private static final float twoThird = 0.66f;
+  /** {@inheritDoc} */
+  public void createInitialLayout(final IPageLayout layout) {
+    // Get the editor area.
+    final String editorArea = layout.getEditorArea();
+    
+    // Top left: Resource Navigator view and Bookmarks view placeholder
+    final IFolderLayout left = layout.createFolder("topLeft", IPageLayout.LEFT, 
+                                                   0.25f, editorArea);
+    left.addView(JavaUI.ID_PACKAGES);
+    left.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+    
+
+    // Bottom right: Task List view
+    layout.addView(IConsoleConstants.ID_CONSOLE_VIEW, IPageLayout.BOTTOM, 
+                   twoThird, editorArea);
+      
+    // Bottom left: Outline view and Property Sheet view
+    final IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT, 
+                                                    0.65f, editorArea);
+    right.addView(IPageLayout.ID_OUTLINE);
+    right.addView(IPageLayout.ID_PROP_SHEET);
+  }
+
+}
