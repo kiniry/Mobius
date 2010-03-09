01d5b57026767e396e2ddb86bc64511283fb6cd8
fintanfairmichael <fintanfairmichael@c6399e9c-662f-4285-9817-23cccad57800>
2010-03-09 16:59:45 +0000

    .

--- src/bon/bonc/update-site/upload.sh
+++ src/bon/bonc/update-site/upload.sh
@@ -5,6 +5,10 @@ path="/Volumes/Data/web/kindsoft/html/products/opensource/BONc/update"
 
 read -p "Enter username on kind:" user
 
+#Remove existing content first?
 scp site.xml logs.zip content.jar artifacts.jar ${user}@${server}:${path}/
-scp features/* ${user}@${server}:${path}/features/
-scp plugins/* ${user}@${server}:${path}/plugins/
+scp -r features ${user}@${server}:${path}/
+scp -r plugins ${user}@${server}:${path}/
+scp -r web ${user}@${server}:${path}/
+
+echo "Done."
